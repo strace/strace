94fe3a0be1e43a6bcc68c2a364349df91c736ad8
Nate Sammons <nate@users.sourceforge.net>
1999-04-05 22:33:54 +0000

    Remove (apparently?) unused definition of struct kernel_stat for LINUXSPARC.

--- file.c
+++ file.c
@@ -453,30 +453,6 @@ int addr;
 }
 #endif
 
-#ifdef LINUXSPARC
-/* Ugly, bug GLIBC makes this necessary. */
-struct kernel_stat
-{
-    unsigned short int st_dev;
-    unsigned long int st_ino;
-    unsigned short int st_mode;
-    short int st_nlink;
-    unsigned short int st_uid;
-    unsigned short int st_gid;
-    unsigned short int st_rdev;
-    long int st_size;
-    long int st_atime;
-    unsigned long int __unused1;
-    long int st_mtime;
-    unsigned long int __unused2;
-    long int st_ctime;
-    unsigned long int __unused3;
-    long int st_blksize;
-    long int st_blocks;
-    unsigned long int __unused4;
-    unsigned long int __unused5;
-};
-#endif
 static void
 printstat(tcp, addr)
 struct tcb *tcp;
