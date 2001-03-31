29f0d05cee0805346e1f3372fc0d67e5a3c3d761
Wichert Akkerman <wichert@deephackmode.org>
2001-03-31 16:14:55 +0000

    add dir variable to set location of kernel includes

--- linux/ioctlent.sh
+++ linux/ioctlent.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
+dir="/tmp/include"
 files="linux/* asm/* scsi/*"
 
 # Build the list of all ioctls
 regexp='^[[:space:]]*#[[:space:]]*define[[:space:]]\+[A-Z][A-Z0-9_]*[[:space:]]\+_\(IO\|IOW\|IOR\|IOWR\)\>'
-grep $regexp $files 2>/dev/null | \
+(cd $dir ; grep $regexp $files 2>/dev/null ) | \
 	sed -ne 's/^\(.*\):[[:space:]]*#[[:space:]]*define[[:space:]]*\([A-Z0-9_]*\)[[:space:]]*_I.*(\([^[,]*\),\([^,)]*\).*/	{ "\1",	"\2",	_IOC(_IOC_NONE,\3,\4,0)	},/p' \
 	> ioctls.h
 
@@ -15,7 +16,8 @@ bases=$(sed -ne 's/.*_IOC_NONE,\([A-Z][A-Z0-9_]\+\),.*/\1/p' ioctls.h | uniq | s
 for base in $bases ; do
 	echo "Looking for $base"
 	regexp="^[[:space:]]*#[[:space:]]*define[[:space:]]\+$base"
-	grep -h $regexp 2>/dev/null $files | grep -v '\<_IO' >> ioctldefs.h
+	(cd $dir ; grep -h $regexp 2>/dev/null $files ) | \
+		grep -v '\<_IO' >> ioctldefs.h
 done
 
 
