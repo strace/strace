7daaace2640597ade2b96e6f2ccfdfaa311343b1
Roland McGrath <roland@redhat.com>
2002-12-30 09:33:20 +0000

    2002-12-30  Roland McGrath  <roland@redhat.com>
    	* Makefile.am [LINUX && X86_64]: Remove cruft.
    	* linux/x86_64/errnoent1.h: New file.
    	* linux/x86_64/ioctlent1.h: New file.
    	* linux/x86_64/signalent1.h: New file.
    	* linux/x86_64/syscallent1.h: New file.
    	* linux/x86_64/i386-headers.diff: File removed.
    	* linux/x86_64/makeheaders.sh: File removed.
    	* linux/x86_64/Makefile.in: File removed.

--- linux/x86_64/makeheaders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in ../*.h; do
-	NM=`basename $i .h`1.h
-	/bin/cp -vf $i $NM
-done
-
-patch -p0 < i386-headers.diff
---
+++ linux/x86_64/syscallent1.h
@@ -0,0 +1,6 @@
+/* Our second set comes from the i386 files.
+   Only a couple of calls we cannot support without the i386 headers.  */
+
+#define sys_oldstat printargs
+#define sys_oldfstat printargs
+#include "../syscallent.h"
