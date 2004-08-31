92f1d6d4e4326564dcfc0b3cfbe6a4a9047795f8
Roland McGrath <roland@redhat.com>
2004-08-31 07:03:20 +0000

    2004-07-12  Dmitry V. Levin  <ldv@altlinux.org>
    	* signal.c [LINUX] (parse_sigset_t): Fix hex strings parser.
    	Fixes RH#128091.

--- signal.c
+++ signal.c
@@ -793,9 +793,9 @@ parse_sigset_t (const char *str, sigset_t *set)
 		if (*p >= '0' && *p <= '9')
 			digit = *p - '0';
 		else if (*p >= 'a' && *p <= 'f')
-			digit = *p - 'a';
+			digit = *p - 'a' + 10;
 		else if (*p >= 'A' && *p <= 'F')
-			digit = *p - 'A';
+			digit = *p - 'A' + 10;
 		else
 			break;
 		if (digit & 1)
