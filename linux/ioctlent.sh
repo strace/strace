7166bb0cead2a8390ccd81c55205344f536680aa
Roland McGrath <roland@redhat.com>
2004-10-06 22:31:38 +0000

    2004-09-13  Dmitry V. Levin  <ldv@altlinux.org>
    	* linux/ioctlsort.c (main): Omit duplicate lines
    	(with same name and code) from output.
    	* linux/ioctlent.sh: Build the list of ioctls defined in
    	scsi/sg.h (0x22..), scsi/scsi.h and scsi/scsi_ioctl.h (0x53..),
    	as suggested by Peter Jones <pjones@redhat.com>
    	* linux/ioctlent.h: Regenerated.
    	Fixes RH#129808.

--- linux/ioctlent.sh
+++ linux/ioctlent.sh
@@ -58,9 +58,10 @@ s/^\(.*\):[[:space:]]*#[[:space:]]*define[[:space:]]*\([A-Z0-9_]*\)[[:space:]]*\
 }
 
 : > ioctls.h
+lookup_ioctls 22 scsi/sg.h
 lookup_ioctls 46 linux/fb.h
 lookup_ioctls 4B linux/kd.h
-lookup_ioctls 53 linux/cdrom.h
+lookup_ioctls 53 linux/cdrom.h scsi/scsi.h scsi/scsi_ioctl.h
 lookup_ioctls 54 $asm/ioctls.h
 lookup_ioctls 56 linux/vt.h
 lookup_ioctls '7[12]' linux/videotext.h
