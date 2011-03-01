e689e04c102d02514af429925ef3b00caaacd058
Mike Frysinger <vapier@gentoo.org>
2011-02-28 19:57:24 -0500

    tests: do not make missing /usr/bin/time a failure
    * tests/init.sh (framework_skip_): New function.
    (check_prog): Use it instead of framework_failure_.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- tests/init.sh
+++ tests/init.sh
@@ -6,11 +6,12 @@ warn_() { printf >&2 '%s\n' "$*"; }
 fail_() { warn_ "$ME_: failed test: $*"; exit 1; }
 skip_() { warn_ "$ME_: skipped test: $*"; exit 77; }
 framework_failure_() { warn_ "$ME_: framework failure: $*"; exit 99; }
+framework_skip_() { warn_ "$ME_: framework skip: $*"; exit 77; }
 
 check_prog()
 {
 	"$@" --version > /dev/null 2>&1 ||
-		framework_failure_ "$* is not available"
+		framework_skip_ "$* is not available"
 }
 
 check_strace()
