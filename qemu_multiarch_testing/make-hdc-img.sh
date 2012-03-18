6acf586aa8adbf1505ff2975c1b0c1cb4920bf66
Denys Vlasenko <vda.linux@googlemail.com>
2012-03-18 23:27:23 +0100

    qemu_multiarch_testing/: a directory with scripts for build testing
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

---
+++ qemu_multiarch_testing/make-hdc-img.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+HDCMEGS=128
+
+rm hdc.img 2>/dev/null
+umount -d hdc.img.dir 2>/dev/null
+rm -rf hdc.img.dir 2>/dev/null
+
+dd if=/dev/zero of=hdc.img bs=1024 seek=$((HDCMEGS*1024-1)) count=1 &&
+mke2fs -q -b 1024 -F -i 4096 hdc.img &&
+tune2fs -j -c 0 -i 0 hdc.img &&
+mkdir hdc.img.dir &&
+mount -o loop hdc.img hdc.img.dir &&
+cp -a hdc.dir/* hdc.img.dir &&
+umount -d hdc.img.dir &&
+rm -rf hdc.img.dir &&
+true
---
+++ qemu_multiarch_testing/parallel-build-hdc-img.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+build_in_dir()
+{
+	cd "$1" || exit 1
+	rm -f hdb.img 2>/dev/null
+	./native-build.sh ../hdc.img
+	rm -f hdb.img 2>/dev/null
+}
+
+started=false
+for dir; do
+	test -d "$dir" || continue
+	test -e "$dir/native-build.sh" || continue
+	echo "Starting: $dir"
+	build_in_dir "$dir" </dev/null >"$dir.log" 2>&1 &
+	started=true
+done
+
+$started || {
+	echo "Give me system-image-ARCH directories on command line"
+	exit 1
+}
+
+echo "Waiting to finish"
+wait
+echo "Done, check the logs"
