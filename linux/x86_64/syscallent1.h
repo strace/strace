e67e9f89ca8109ae31be07fbc1dd4d425c1f13da
Dmitry V. Levin <ldv@altlinux.org>
2013-05-01 15:36:13 +0000

    x86_64: decode __old_kernel_stat based syscalls
    * linux/x86_64/syscallent1.h: Remove sys_old*stat redirections.

--- linux/x86_64/syscallent1.h
+++ linux/x86_64/syscallent1.h
@@ -1,7 +1,3 @@
-/* Our second set comes from the i386 files.
-   Only a couple of calls we cannot support without the i386 headers.  */
+/* Our second set comes from the i386 files.  */
 
-#define sys_oldstat printargs
-#define sys_oldfstat printargs
-#define sys_oldlstat printargs
 #include "../i386/syscallent.h"
