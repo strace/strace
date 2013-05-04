921744d5e72022e68158efa7052f1d7ebe193cc1
Mike Frysinger <vapier@gentoo.org>
2013-05-04 01:42:54 -0400

    Fix building for mips n64 targets
    * util.c (printllval): Handle LINUX_MIPSN64 define.

--- util.c
+++ util.c
@@ -177,7 +177,8 @@ printxval(const struct xlat *xlat, int val, const char *dflt)
 int
 printllval(struct tcb *tcp, const char *format, int arg_no)
 {
-#if defined(X86_64) || defined(POWERPC64) || defined(TILE) || defined(AARCH64)
+#if defined(X86_64) || defined(POWERPC64) || defined(TILE) || defined(AARCH64) || \
+    defined(LINUX_MIPSN64)
 	if (current_personality == 0) {
 		/* Technically, format expects "long long",
 		 * but we supply "long". We expect that
