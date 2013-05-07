3883ab84388efe01962e4164911508af24d3f2f5
Mike Frysinger <vapier@gentoo.org>
2013-05-07 10:50:20 -0400

    mips: fix build regression
    The recent commit (2690fadc8b35190dddd29274a7405bac13adc469) shuffled
    the mips headers around causing it to check the ABI defines before it
    included the header which set those up.  Now all mips builds fail with:
    	In file included from count.c:36:0:
    	defs.h:48:4: error: #error Unsupported _MIPS_SIM
    
    * defs.h [MIPS]: Move sgidefs.h above _MIPS_SIM check.

--- defs.h
+++ defs.h
@@ -38,6 +38,7 @@
 #endif
 
 #ifdef MIPS
+# include <sgidefs.h>
 # if _MIPS_SIM == _MIPS_SIM_ABI64
 #  define LINUX_MIPSN64
 # elif _MIPS_SIM == _MIPS_SIM_NABI32
@@ -47,7 +48,6 @@
 # else
 #  error Unsupported _MIPS_SIM
 # endif
-# include <sgidefs.h>
 #endif
 
 #include <features.h>
