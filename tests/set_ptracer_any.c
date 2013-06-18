97c85088a286831aec3ecb41942946d52dc006d4
Dmitry V. Levin <ldv@altlinux.org>
2013-06-18 16:50:18 +0000

    tests: ensure that strace can detach from sleeping and stopped processes
    * tests/set_ptracer_any.c: New file.
    * tests/detach-sleeping: New test.
    * tests/detach-stopped: Likewise.
    * tests/Makefile.am (check_PROGRAMS): Add set_ptracer_any.
    (TESTS): Add detach-sleeping and detach-stopped.
    * tests/.gitignore: Add set_ptracer_any.

---
+++ tests/set_ptracer_any.c
@@ -0,0 +1,24 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#include <stdio.h>
+#include <unistd.h>
+#ifdef HAVE_PRCTL
+# include <sys/prctl.h>
+#endif
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		return 99;
+#if defined HAVE_PRCTL && defined PR_SET_PTRACER && defined PR_SET_PTRACER_ANY
+	(void) prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
+#endif
+	if (write(1, "\n", 1) != 1) {
+		perror("write");
+		return 99;
+	}
+	(void) execvp(argv[1], argv + 1);
+	perror(argv[1]);
+	return 99;
+}
