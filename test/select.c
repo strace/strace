c4b9214a0fe23f1b1196bdfb5addcba013855cb3
Denys Vlasenko <dvlasenk@redhat.com>
2013-11-05 12:44:55 +0100

    test/select.c: make comment more understandable.
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- test/select.c
+++ test/select.c
@@ -15,8 +15,8 @@ int main()
 	FD_SET(2, &rds);
 	/* Start with a nice simple select */
 	select(3, &rds, &rds, &rds, NULL);
-	/* Now the crash case that trinity found, -ve nfds
-	 * but with a pointer to a large chunk of valid memory
+	/* Now the crash case that trinity found, negative nfds
+	 * but with a pointer to a large chunk of valid memory.
 	 */
 	select(-1, (fd_set *)buffer, NULL, NULL, NULL);
 	return 0;
