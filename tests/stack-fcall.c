c588b205be19f84f757dec6f2e6ed2a157750f97
Dmitry V. Levin <ldv@altlinux.org>
2014-06-13 15:07:24 +0000

    tests: enhance -k test
    Add two more function calls to the stack.  Suggested by Masatake YAMATO.
    
    * tests/stack-fcall.c (f1): Rename to f3.
    (f1, f2): New functions.
    * tests/strace-k.test: Update.

--- tests/stack-fcall.c
+++ tests/stack-fcall.c
@@ -3,10 +3,22 @@
 
 /* Use "volatile" to avoid compiler optimization. */
 
-int f1(int i)
+int f3(int i)
 {
 	static pid_t (* volatile g)(void) = getpid;
 	return g() + i;
+}
+
+int f2(volatile int i)
+{
+	static int (* volatile g)(int) = f3;
+	return g(i) - i;
+}
+
+int f1(volatile int i)
+{
+	static int (* volatile g)(int) = f2;
+	return g(i) + i;
 }
 
 int f0(volatile int i)
