542ca1ef3deeed7dc646bd3ddbb149c55cb6f369
Dmitry V. Levin <ldv@altlinux.org>
2014-12-03 21:09:26 +0000

    Move umount2 parser to a separate file
    * umount.c: New file.
    * Makefile.am (strace_SOURCES): Add it.
    * system.c: Move sys_umount2 and related code to umount.c.

---
+++ umount.c
@@ -0,0 +1,18 @@
+#include "defs.h"
+
+#define MNT_FORCE	0x00000001	/* Attempt to forcibily umount */
+#define MNT_DETACH	0x00000002	/* Just detach from the tree */
+#define MNT_EXPIRE	0x00000004	/* Mark for expiry */
+
+#include "xlat/umount_flags.h"
+
+int
+sys_umount2(struct tcb *tcp)
+{
+	if (entering(tcp)) {
+		printstr(tcp, tcp->u_arg[0], -1);
+		tprints(", ");
+		printflags(umount_flags, tcp->u_arg[1], "MNT_???");
+	}
+	return 0;
+}
