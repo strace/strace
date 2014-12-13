825b524c8cf5acc1740176ed6f89c711b996770e
Dmitry V. Levin <ldv@altlinux.org>
2014-12-13 01:34:47 +0000

    Update SWAP_FLAG_* constants
    * swapon.c: Ensure that SWAP_FLAG_DISCARD_ONCE and
    SWAP_FLAG_DISCARD_PAGES are defined.
    * xlat/swap_flags.in: Add SWAP_FLAG_DISCARD_ONCE and
    SWAP_FLAG_DISCARD_PAGES.

--- swapon.c
+++ swapon.c
@@ -8,6 +8,12 @@
 #ifndef SWAP_FLAG_DISCARD
 # define SWAP_FLAG_DISCARD 0x10000
 #endif
+#ifndef SWAP_FLAG_DISCARD_ONCE
+# define SWAP_FLAG_DISCARD_ONCE 0x20000
+#endif
+#ifndef SWAP_FLAG_DISCARD_PAGES
+# define SWAP_FLAG_DISCARD_PAGES 0x40000
+#endif
 
 #include "xlat/swap_flags.h"
 
