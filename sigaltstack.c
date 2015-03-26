174bb2b36ec7b4c922331ff52681d79eac7add2e
Dmitry V. Levin <ldv@altlinux.org>
2015-03-26 13:03:25 +0000

    signal.c: move sigaltstack parser to a separate file
    * sigaltstack.c: New file.
    * Makefile.am (strace_SOURCES): Add it.
    * signal.c (print_stack_t, sys_sigaltstack): Move to sigaltstack.c.
    * xlat/sigaltstack_flags.in: Add default values.

---
+++ sigaltstack.c
@@ -0,0 +1,53 @@
+#include "defs.h"
+#include "xlat/sigaltstack_flags.h"
+
+static void
+print_stack_t(struct tcb *tcp, unsigned long addr)
+{
+	stack_t ss;
+	int r;
+
+	if (!addr) {
+		tprints("NULL");
+		return;
+	}
+
+#if SUPPORTED_PERSONALITIES > 1 && SIZEOF_LONG > 4
+	if (current_wordsize != sizeof(ss.ss_sp) && current_wordsize == 4) {
+		struct {
+			uint32_t ss_sp;
+			int32_t ss_flags;
+			uint32_t ss_size;
+		} ss32;
+		r = umove(tcp, addr, &ss32);
+		if (r >= 0) {
+			memset(&ss, 0, sizeof(ss));
+			ss.ss_sp = (void*)(unsigned long) ss32.ss_sp;
+			ss.ss_flags = ss32.ss_flags;
+			ss.ss_size = (unsigned long) ss32.ss_size;
+		}
+	} else
+#endif
+	{
+		r = umove(tcp, addr, &ss);
+	}
+	if (r < 0) {
+		tprintf("%#lx", addr);
+	} else {
+		tprintf("{ss_sp=%#lx, ss_flags=", (unsigned long) ss.ss_sp);
+		printflags(sigaltstack_flags, ss.ss_flags, "SS_???");
+		tprintf(", ss_size=%lu}", (unsigned long) ss.ss_size);
+	}
+}
+
+int
+sys_sigaltstack(struct tcb *tcp)
+{
+	if (entering(tcp)) {
+		print_stack_t(tcp, tcp->u_arg[0]);
+	} else {
+		tprints(", ");
+		print_stack_t(tcp, tcp->u_arg[1]);
+	}
+	return 0;
+}
