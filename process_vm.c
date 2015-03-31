87af1935fb17ce3253ff2a573815ac88e1b48c31
Dmitry V. Levin <ldv@altlinux.org>
2015-03-31 21:00:39 +0000

    Cleanup process_vm_writev syscall decoding
    * process_vm.c (sys_process_vm_writev): Do not check for syserror,
    it is not applicable on entering syscall.

--- process_vm.c
+++ process_vm.c
@@ -35,17 +35,11 @@ sys_process_vm_writev(struct tcb *tcp)
 		/* arg 1: pid */
 		tprintf("%ld, ", tcp->u_arg[0]);
 		/* arg 2: local iov */
-		if (syserror(tcp))
-			tprintf("%#lx", tcp->u_arg[1]);
-		else
-			tprint_iov(tcp, tcp->u_arg[2], tcp->u_arg[1], 1);
+		tprint_iov(tcp, tcp->u_arg[2], tcp->u_arg[1], 1);
 		/* arg 3: local iovcnt */
 		tprintf(", %lu, ", tcp->u_arg[2]);
 		/* arg 4: remote iov */
-		if (syserror(tcp))
-			tprintf("%#lx", tcp->u_arg[3]);
-		else
-			tprint_iov(tcp, tcp->u_arg[4], tcp->u_arg[3], 0);
+		tprint_iov(tcp, tcp->u_arg[4], tcp->u_arg[3], 0);
 		/* arg 5: remote iovcnt */
 		/* arg 6: flags */
 		tprintf(", %lu, %lu", tcp->u_arg[4], tcp->u_arg[5]);
