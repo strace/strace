f6026b6e657ce8619e54dfd081a354ec929ba0ce
Dmitry V. Levin <ldv@altlinux.org>
2015-07-15 00:33:20 +0000

    wait: move fallback definitions of wait options to xlat/
    * wait.c: Move definitions of __W* flags ...
    * xlat/wait4_options.in: ... here.

--- wait.c
+++ wait.c
@@ -2,16 +2,6 @@
 
 #include <sys/wait.h>
 
-#ifndef __WNOTHREAD
-# define __WNOTHREAD	0x20000000
-#endif
-#ifndef __WALL
-# define __WALL		0x40000000
-#endif
-#ifndef __WCLONE
-# define __WCLONE	0x80000000
-#endif
-
 #include "xlat/wait4_options.h"
 
 #if !defined WCOREFLAG && defined WCOREFLG
