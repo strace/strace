d27cccaeff704db6a2741a15bc51c5d89650cad9
Dmitry V. Levin <ldv@altlinux.org>
2015-07-15 09:02:17 +0000

    tests: add utime.test
    * tests/utime.c: New file.
    * tests/utime.test: New test.
    * tests/Makefile.am (check_PROGRAMS): Add utime.
    (TESTS): Add utime.test.
    * tests/.gitignore: Add utime.

---
+++ tests/utime.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <utime.h>
+#include <errno.h>
+#include <stdio.h>
+
+static void
+print_tm(struct tm *p)
+{
+	printf("%02d/%02d/%02d-%02d:%02d:%02d",
+	       p->tm_year + 1900, p->tm_mon + 1, p->tm_mday,
+	       p->tm_hour, p->tm_min, p->tm_sec);
+}
+
+int
+main(void)
+{
+	time_t t = time(NULL);
+	struct utimbuf u = { .actime = t, .modtime = t };
+	struct tm *p = localtime(&t);
+
+	printf("utime\\(\"utime\\\\nfilename\", \\[");
+	print_tm(p);
+	printf(", ");
+	print_tm(p);
+	printf("\\]\\) += -1 ENOENT .*\n");
+
+	return utime("utime\nfilename", &u) == -1 && errno == ENOENT ? 0 : 77;
+}
