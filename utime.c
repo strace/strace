8d51f439461ca720cf9cd5a731a7e09ac55887d0
Dmitry V. Levin <ldv@altlinux.org>
2015-07-15 08:29:56 +0000

    utime.c: make use of RVAL_DECODED
    * utime.c (sys_utime): Update for RVAL_DECODED.

--- utime.c
+++ utime.c
@@ -9,24 +9,23 @@ SYS_FUNC(utime)
 	} u;
 	unsigned wordsize;
 
-	if (entering(tcp)) {
-		printpath(tcp, tcp->u_arg[0]);
-		tprints(", ");
+	printpath(tcp, tcp->u_arg[0]);
+	tprints(", ");
 
-		wordsize = current_wordsize;
-		if (umoven_or_printaddr(tcp, tcp->u_arg[1], 2 * wordsize, &u))
-			;
-		else if (wordsize == sizeof u.utl[0]) {
-			tprintf("[%s,", sprinttime(u.utl[0]));
-			tprintf(" %s]", sprinttime(u.utl[1]));
-		}
-		else if (wordsize == sizeof u.uti[0]) {
-			tprintf("[%s,", sprinttime(u.uti[0]));
-			tprintf(" %s]", sprinttime(u.uti[1]));
-		}
-		else
-			tprintf("<decode error: unsupported wordsize %d>",
-				wordsize);
+	wordsize = current_wordsize;
+	if (umoven_or_printaddr(tcp, tcp->u_arg[1], 2 * wordsize, &u))
+		;
+	else if (wordsize == sizeof u.utl[0]) {
+		tprintf("[%s,", sprinttime(u.utl[0]));
+		tprintf(" %s]", sprinttime(u.utl[1]));
 	}
-	return 0;
+	else if (wordsize == sizeof u.uti[0]) {
+		tprintf("[%s,", sprinttime(u.uti[0]));
+		tprintf(" %s]", sprinttime(u.uti[1]));
+	}
+	else
+		tprintf("<decode error: unsupported wordsize %d>",
+			wordsize);
+
+	return RVAL_DECODED;
 }
