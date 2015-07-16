d6ed7948f77fb65fec67efd2685569fdb5910c74
Dmitry V. Levin <ldv@altlinux.org>
2015-07-16 23:15:46 +0000

    sync_file_range.c: make use of RVAL_DECODED
    * sync_file_range.c (sys_sync_file_range, sys_sync_file_range2): Update
    for RVAL_DECODED.

--- sync_file_range.c
+++ sync_file_range.c
@@ -6,26 +6,26 @@
 
 SYS_FUNC(sync_file_range)
 {
-	if (entering(tcp)) {
-		int argn;
-		printfd(tcp, tcp->u_arg[0]);
-		argn = printllval(tcp, ", %lld, ", 1);
-		argn = printllval(tcp, "%lld, ", argn);
-		printflags(sync_file_range_flags, tcp->u_arg[argn],
-		           "SYNC_FILE_RANGE_???");
-	}
-	return 0;
+	int argn;
+
+	printfd(tcp, tcp->u_arg[0]);
+	argn = printllval(tcp, ", %lld, ", 1);
+	argn = printllval(tcp, "%lld, ", argn);
+	printflags(sync_file_range_flags, tcp->u_arg[argn],
+		   "SYNC_FILE_RANGE_???");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(sync_file_range2)
 {
-	if (entering(tcp)) {
-		int argn;
-		printfd(tcp, tcp->u_arg[0]);
-		printflags(sync_file_range_flags, 1,
-		           "SYNC_FILE_RANGE_???");
-		argn = printllval(tcp, ", %lld, ", 2);
-		argn = printllval(tcp, "%lld, ", argn);
-	}
-	return 0;
+	int argn;
+
+	printfd(tcp, tcp->u_arg[0]);
+	printflags(sync_file_range_flags, 1,
+		   "SYNC_FILE_RANGE_???");
+	argn = printllval(tcp, ", %lld, ", 2);
+	argn = printllval(tcp, "%lld, ", argn);
+
+	return RVAL_DECODED;
 }
