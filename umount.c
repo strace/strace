c1f37f6e7ad44ce90bed1b86b22543c47847c7b3
Dmitry V. Levin <ldv@altlinux.org>
2015-07-16 21:13:27 +0000

    umount.c: make use of RVAL_DECODED
    * umount.c (sys_umount2): Update for RVAL_DECODED.

--- umount.c
+++ umount.c
@@ -8,10 +8,9 @@
 
 SYS_FUNC(umount2)
 {
-	if (entering(tcp)) {
-		printstr(tcp, tcp->u_arg[0], -1);
-		tprints(", ");
-		printflags(umount_flags, tcp->u_arg[1], "MNT_???");
-	}
-	return 0;
+	printstr(tcp, tcp->u_arg[0], -1);
+	tprints(", ");
+	printflags(umount_flags, tcp->u_arg[1], "MNT_???");
+
+	return RVAL_DECODED;
 }
