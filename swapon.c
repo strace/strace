9afb4d06c07dab4ad556e17b6b81eddb859a24eb
Dmitry V. Levin <ldv@altlinux.org>
2015-07-16 23:28:07 +0000

    swapon.c: make use of RVAL_DECODED
    * swapon.c (sys_swapon): Update for RVAL_DECODED.

--- swapon.c
+++ swapon.c
@@ -6,14 +6,14 @@
 
 SYS_FUNC(swapon)
 {
-	if (entering(tcp)) {
-		int flags = tcp->u_arg[1];
-		printpath(tcp, tcp->u_arg[0]);
-		tprints(", ");
-		printflags(swap_flags, flags & ~SWAP_FLAG_PRIO_MASK,
-			"SWAP_FLAG_???");
-		if (flags & SWAP_FLAG_PREFER)
-			tprintf("|%d", flags & SWAP_FLAG_PRIO_MASK);
-	}
-	return 0;
+	int flags = tcp->u_arg[1];
+
+	printpath(tcp, tcp->u_arg[0]);
+	tprints(", ");
+	printflags(swap_flags, flags & ~SWAP_FLAG_PRIO_MASK,
+		"SWAP_FLAG_???");
+	if (flags & SWAP_FLAG_PREFER)
+		tprintf("|%d", flags & SWAP_FLAG_PRIO_MASK);
+
+	return RVAL_DECODED;
 }
