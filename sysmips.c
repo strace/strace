034f98e10dc850ef1b93f8cfa09aa6e4cb397543
Dmitry V. Levin <ldv@altlinux.org>
2015-07-16 21:35:41 +0000

    sysmips.c: make use of RVAL_DECODED
    * sysmips.c [MIPS] (sys_sysmips): Update for RVAL_DECODED.

--- sysmips.c
+++ sysmips.c
@@ -17,30 +17,28 @@
 
 SYS_FUNC(sysmips)
 {
-	if (entering(tcp)) {
-		printxval(sysmips_operations, tcp->u_arg[0], "???");
-		if (!verbose(tcp)) {
-			tprintf("%ld, %ld, %ld", tcp->u_arg[1], tcp->u_arg[2], tcp->u_arg[3]);
-		} else if (tcp->u_arg[0] == SETNAME) {
-			char nodename[__NEW_UTS_LEN + 1];
-			tprints(", ");
-			if (umovestr(tcp, tcp->u_arg[1], (__NEW_UTS_LEN + 1),
-				     nodename) < 0) {
-				tprintf("%#lx", tcp->u_arg[1]);
-			} else {
-				print_quoted_string(nodename, __NEW_UTS_LEN + 1,
-						    QUOTE_0_TERMINATED);
-			}
-		} else if (tcp->u_arg[0] == MIPS_ATOMIC_SET) {
-			tprintf(", %#lx, 0x%lx", tcp->u_arg[1], tcp->u_arg[2]);
-		} else if (tcp->u_arg[0] == MIPS_FIXADE) {
-			tprintf(", 0x%lx", tcp->u_arg[1]);
+	printxval(sysmips_operations, tcp->u_arg[0], "???");
+	if (!verbose(tcp)) {
+		tprintf("%ld, %ld, %ld", tcp->u_arg[1], tcp->u_arg[2], tcp->u_arg[3]);
+	} else if (tcp->u_arg[0] == SETNAME) {
+		char nodename[__NEW_UTS_LEN + 1];
+		tprints(", ");
+		if (umovestr(tcp, tcp->u_arg[1], (__NEW_UTS_LEN + 1),
+			     nodename) < 0) {
+			tprintf("%#lx", tcp->u_arg[1]);
 		} else {
-			tprintf("%ld, %ld, %ld", tcp->u_arg[1], tcp->u_arg[2], tcp->u_arg[3]);
+			print_quoted_string(nodename, __NEW_UTS_LEN + 1,
+					    QUOTE_0_TERMINATED);
 		}
+	} else if (tcp->u_arg[0] == MIPS_ATOMIC_SET) {
+		tprintf(", %#lx, 0x%lx", tcp->u_arg[1], tcp->u_arg[2]);
+	} else if (tcp->u_arg[0] == MIPS_FIXADE) {
+		tprintf(", 0x%lx", tcp->u_arg[1]);
+	} else {
+		tprintf("%ld, %ld, %ld", tcp->u_arg[1], tcp->u_arg[2], tcp->u_arg[3]);
 	}
 
-	return 0;
+	return RVAL_DECODED;
 }
 
 #endif /* MIPS */
