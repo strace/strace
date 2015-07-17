8ef8a4c095824d9913f5133165685514596bc61a
Dmitry V. Levin <ldv@altlinux.org>
2015-07-17 17:26:27 +0000

    readahead.c: make use of RVAL_DECODED
    * readahead.c (sys_readahead.c): Update for RVAL_DECODED.

--- readahead.c
+++ readahead.c
@@ -2,11 +2,11 @@
 
 SYS_FUNC(readahead)
 {
-	if (entering(tcp)) {
-		int argn;
-		printfd(tcp, tcp->u_arg[0]);
-		argn = printllval(tcp, ", %lld", 1);
-		tprintf(", %ld", tcp->u_arg[argn]);
-	}
-	return 0;
+	int argn;
+
+	printfd(tcp, tcp->u_arg[0]);
+	argn = printllval(tcp, ", %lld", 1);
+	tprintf(", %ld", tcp->u_arg[argn]);
+
+	return RVAL_DECODED;
 }
