684c9e9d3da74468a15997a4eba6a1628221b1cc
Dmitry V. Levin <ldv@altlinux.org>
2015-07-17 20:39:49 +0000

    process_vm.c: make use of RVAL_DECODED
    * process_vm.c (sys_process_vm_writev): Update for RVAL_DECODED.

--- process_vm.c
+++ process_vm.c
@@ -29,18 +29,17 @@ SYS_FUNC(process_vm_readv)
 
 SYS_FUNC(process_vm_writev)
 {
-	if (entering(tcp)) {
-		/* arg 1: pid */
-		tprintf("%ld, ", tcp->u_arg[0]);
-		/* arg 2: local iov */
-		tprint_iov(tcp, tcp->u_arg[2], tcp->u_arg[1], 1);
-		/* arg 3: local iovcnt */
-		tprintf(", %lu, ", tcp->u_arg[2]);
-		/* arg 4: remote iov */
-		tprint_iov(tcp, tcp->u_arg[4], tcp->u_arg[3], 0);
-		/* arg 5: remote iovcnt */
-		/* arg 6: flags */
-		tprintf(", %lu, %lu", tcp->u_arg[4], tcp->u_arg[5]);
-	}
-	return 0;
+	/* arg 1: pid */
+	tprintf("%ld, ", tcp->u_arg[0]);
+	/* arg 2: local iov */
+	tprint_iov(tcp, tcp->u_arg[2], tcp->u_arg[1], 1);
+	/* arg 3: local iovcnt */
+	tprintf(", %lu, ", tcp->u_arg[2]);
+	/* arg 4: remote iov */
+	tprint_iov(tcp, tcp->u_arg[4], tcp->u_arg[3], 0);
+	/* arg 5: remote iovcnt */
+	/* arg 6: flags */
+	tprintf(", %lu, %lu", tcp->u_arg[4], tcp->u_arg[5]);
+
+	return RVAL_DECODED;
 }
