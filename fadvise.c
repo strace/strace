429c6775d42c609568b3b1c1f06ac848f16ac3d0
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 13:55:47 +0000

    fadvise.c: make use of RVAL_DECODED
    * fadvise.c (sys_fadvise64, sys_fadvise64_64): Update for RVAL_DECODED.

--- fadvise.c
+++ fadvise.c
@@ -6,28 +6,28 @@
 
 SYS_FUNC(fadvise64)
 {
-	if (entering(tcp)) {
-		int argn;
-		printfd(tcp, tcp->u_arg[0]);
-		argn = printllval(tcp, ", %lld", 1);
-		tprintf(", %ld, ", tcp->u_arg[argn++]);
-		printxval(advise, tcp->u_arg[argn], "POSIX_FADV_???");
-	}
-	return 0;
+	int argn;
+
+	printfd(tcp, tcp->u_arg[0]);
+	argn = printllval(tcp, ", %lld", 1);
+	tprintf(", %ld, ", tcp->u_arg[argn++]);
+	printxval(advise, tcp->u_arg[argn], "POSIX_FADV_???");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(fadvise64_64)
 {
-	if (entering(tcp)) {
-		int argn;
-		printfd(tcp, tcp->u_arg[0]);
-		argn = printllval(tcp, ", %lld, ", 1);
-		argn = printllval(tcp, "%lld, ", argn);
+	int argn;
+
+	printfd(tcp, tcp->u_arg[0]);
+	argn = printllval(tcp, ", %lld, ", 1);
+	argn = printllval(tcp, "%lld, ", argn);
 #if defined __ARM_EABI__ || defined AARCH64 || defined POWERPC || defined XTENSA
-		printxval(advise, tcp->u_arg[1], "POSIX_FADV_???");
+	printxval(advise, tcp->u_arg[1], "POSIX_FADV_???");
 #else
-		printxval(advise, tcp->u_arg[argn], "POSIX_FADV_???");
+	printxval(advise, tcp->u_arg[argn], "POSIX_FADV_???");
 #endif
-	}
-	return 0;
+
+	return RVAL_DECODED;
 }
