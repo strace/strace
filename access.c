0b2eabeed43978bbd6351a10b29a868086d2c0a3
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 19:27:51 +0000

    access.c: make use of RVAL_DECODED
    * access.c (decode_access, sys_faccessat): Update for RVAL_DECODED.

--- access.c
+++ access.c
@@ -7,12 +7,11 @@
 static int
 decode_access(struct tcb *tcp, int offset)
 {
-	if (entering(tcp)) {
-		printpath(tcp, tcp->u_arg[offset]);
-		tprints(", ");
-		printflags(access_flags, tcp->u_arg[offset + 1], "?_OK");
-	}
-	return 0;
+	printpath(tcp, tcp->u_arg[offset]);
+	tprints(", ");
+	printflags(access_flags, tcp->u_arg[offset + 1], "?_OK");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(access)
@@ -22,7 +21,6 @@ SYS_FUNC(access)
 
 SYS_FUNC(faccessat)
 {
-	if (entering(tcp))
-		print_dirfd(tcp, tcp->u_arg[0]);
+	print_dirfd(tcp, tcp->u_arg[0]);
 	return decode_access(tcp, 1);
 }
