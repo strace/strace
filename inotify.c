fe3cdfc68dbf7e9bf1dd69f8c82f5f1e58eaa5a7
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 11:25:52 +0000

    inotify.c: make use of RVAL_DECODED
    * inotify.c (sys_inotify_add_watch, sys_inotify_rm_watch,
    sys_inotify_init1): Update for RVAL_DECODED.

--- inotify.c
+++ inotify.c
@@ -6,33 +6,31 @@
 
 SYS_FUNC(inotify_add_watch)
 {
-	if (entering(tcp)) {
-		/* file descriptor */
-		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
-		/* pathname */
-		printpath(tcp, tcp->u_arg[1]);
-		tprints(", ");
-		/* mask */
-		printflags(inotify_flags, tcp->u_arg[2], "IN_???");
-	}
-	return 0;
+	/* file descriptor */
+	printfd(tcp, tcp->u_arg[0]);
+	tprints(", ");
+	/* pathname */
+	printpath(tcp, tcp->u_arg[1]);
+	tprints(", ");
+	/* mask */
+	printflags(inotify_flags, tcp->u_arg[2], "IN_???");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(inotify_rm_watch)
 {
-	if (entering(tcp)) {
-		/* file descriptor */
-		printfd(tcp, tcp->u_arg[0]);
-		/* watch descriptor */
-		tprintf(", %d", (int) tcp->u_arg[1]);
-	}
-	return 0;
+	/* file descriptor */
+	printfd(tcp, tcp->u_arg[0]);
+	/* watch descriptor */
+	tprintf(", %d", (int) tcp->u_arg[1]);
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(inotify_init1)
 {
-	if (entering(tcp))
-		printflags(inotify_init_flags, tcp->u_arg[0], "IN_???");
-	return 0;
+	printflags(inotify_init_flags, tcp->u_arg[0], "IN_???");
+
+	return RVAL_DECODED;
 }
