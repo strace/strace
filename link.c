4e33965809e0c4f2e382eedb5dd2c2afd4b4e6e2
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 00:01:55 +0000

    link.c: make use of RVAL_DECODED
    * link.c (sys_link, sys_linkat, sys_unlinkat, sys_symlinkat): Update
    for RVAL_DECODED.

--- link.c
+++ link.c
@@ -6,46 +6,42 @@
 
 SYS_FUNC(link)
 {
-	if (entering(tcp)) {
-		printpath(tcp, tcp->u_arg[0]);
-		tprints(", ");
-		printpath(tcp, tcp->u_arg[1]);
-	}
-	return 0;
+	printpath(tcp, tcp->u_arg[0]);
+	tprints(", ");
+	printpath(tcp, tcp->u_arg[1]);
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(linkat)
 {
-	if (entering(tcp)) {
-		print_dirfd(tcp, tcp->u_arg[0]);
-		printpath(tcp, tcp->u_arg[1]);
-		tprints(", ");
-		print_dirfd(tcp, tcp->u_arg[2]);
-		printpath(tcp, tcp->u_arg[3]);
-		tprints(", ");
-		printflags(at_flags, tcp->u_arg[4], "AT_???");
-	}
-	return 0;
+	print_dirfd(tcp, tcp->u_arg[0]);
+	printpath(tcp, tcp->u_arg[1]);
+	tprints(", ");
+	print_dirfd(tcp, tcp->u_arg[2]);
+	printpath(tcp, tcp->u_arg[3]);
+	tprints(", ");
+	printflags(at_flags, tcp->u_arg[4], "AT_???");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(unlinkat)
 {
-	if (entering(tcp)) {
-		print_dirfd(tcp, tcp->u_arg[0]);
-		printpath(tcp, tcp->u_arg[1]);
-		tprints(", ");
-		printflags(at_flags, tcp->u_arg[2], "AT_???");
-	}
-	return 0;
+	print_dirfd(tcp, tcp->u_arg[0]);
+	printpath(tcp, tcp->u_arg[1]);
+	tprints(", ");
+	printflags(at_flags, tcp->u_arg[2], "AT_???");
+
+	return RVAL_DECODED;
 }
 
 SYS_FUNC(symlinkat)
 {
-	if (entering(tcp)) {
-		printpath(tcp, tcp->u_arg[0]);
-		tprints(", ");
-		print_dirfd(tcp, tcp->u_arg[1]);
-		printpath(tcp, tcp->u_arg[2]);
-	}
-	return 0;
+	printpath(tcp, tcp->u_arg[0]);
+	tprints(", ");
+	print_dirfd(tcp, tcp->u_arg[1]);
+	printpath(tcp, tcp->u_arg[2]);
+
+	return RVAL_DECODED;
 }
