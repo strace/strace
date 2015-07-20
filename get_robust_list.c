a9183c39581350b1e5a1d0cfaa3d070a2845c8fb
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 11:40:43 +0000

    get_robust_list.c: use printnum_long
    * get_robust_list.c (sys_get_robust_list): Use printnum_long.

--- get_robust_list.c
+++ get_robust_list.c
@@ -5,24 +5,9 @@ SYS_FUNC(get_robust_list)
 	if (entering(tcp)) {
 		tprintf("%ld, ", (long) (pid_t) tcp->u_arg[0]);
 	} else {
-		void *addr;
-		size_t len;
-
-		if (syserror(tcp) ||
-		    !tcp->u_arg[1] ||
-		    umove(tcp, tcp->u_arg[1], &addr) < 0) {
-			tprintf("%#lx, ", tcp->u_arg[1]);
-		} else {
-			tprintf("[%p], ", addr);
-		}
-
-		if (syserror(tcp) ||
-		    !tcp->u_arg[2] ||
-		    umove(tcp, tcp->u_arg[2], &len) < 0) {
-			tprintf("%#lx", tcp->u_arg[2]);
-		} else {
-			tprintf("[%lu]", (unsigned long) len);
-		}
+		printnum_long(tcp, tcp->u_arg[1], "%#lx");
+		tprints(", ");
+		printnum_long(tcp, tcp->u_arg[2], "%lu");
 	}
 	return 0;
 }
