f891bb4373d83aed888e821c80500b376cabda0f
Dmitry V. Levin <ldv@altlinux.org>
2015-07-20 13:50:55 +0000

    fchownat.c: make use of RVAL_DECODED
    * fchownat.c (sys_fchownat): Update for RVAL_DECODED.

--- fchownat.c
+++ fchownat.c
@@ -2,13 +2,12 @@
 
 SYS_FUNC(fchownat)
 {
-	if (entering(tcp)) {
-		print_dirfd(tcp, tcp->u_arg[0]);
-		printpath(tcp, tcp->u_arg[1]);
-		printuid(", ", tcp->u_arg[2]);
-		printuid(", ", tcp->u_arg[3]);
-		tprints(", ");
-		printflags(at_flags, tcp->u_arg[4], "AT_???");
-	}
-	return 0;
+	print_dirfd(tcp, tcp->u_arg[0]);
+	printpath(tcp, tcp->u_arg[1]);
+	printuid(", ", tcp->u_arg[2]);
+	printuid(", ", tcp->u_arg[3]);
+	tprints(", ");
+	printflags(at_flags, tcp->u_arg[4], "AT_???");
+
+	return RVAL_DECODED;
 }
