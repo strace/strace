0e4b1f2eb31a15a6aa7177e0281785a05779bf44
Dmitry V. Levin <ldv@altlinux.org>
2015-07-29 21:32:35 +0000

    mips: fix stub files
    This complements commit 140ecf876686d49085c6eb3a2306e2ea6ea641bc.
    
    * linux/mips/genstub.sh: Output SEN(printargs) instead of printargs.
    
    Reported-by: Elliott Hughes <enh@google.com>

--- linux/mips/genstub.sh
+++ linux/mips/genstub.sh
@@ -6,5 +6,5 @@ dstdir="$1"; shift
 for n in n32 n64 o32; do
 	in="$srcdir/syscallent-$n.h"
 	out="$dstdir/syscallent-$n-stub.h"
-	sed -n '/^#if/,/^#else/ {s/^\([^{]*{[^,]*,[^,]*,[[:space:]]*\)[^,[:space:]]\+,[[:space:]]*"\([^"]\+".*\)/\1printargs, "'$n'_\2/; s/^\[.*/&/p}' < "$in" > "$out"
+	sed -n '/^#if/,/^#else/ {s/^\([^{]*{[^,]*,[^,]*,[[:space:]]*\)[^,[:space:]]\+,[[:space:]]*"\([^"]\+".*\)/\1SEN(printargs), "'$n'_\2/; s/^\[.*/&/p}' < "$in" > "$out"
 done
