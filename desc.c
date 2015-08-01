28471d17f99875c9141351e0f9e01d6528f20e16
Dmitry V. Levin <ldv@altlinux.org>
2015-08-01 20:36:53 +0000

    desc.c: move eventfd parsers to a separate file
    * eventfd.c: New file.
    * Makefile.am (strace_SOURCES): Add it.
    * desc.c (do_eventfd, sys_eventfd, sys_eventfd2): Move to eventfd.c.

--- desc.c
+++ desc.c
@@ -579,25 +579,3 @@ SYS_FUNC(pselect6)
 	}
 	return rc;
 }
-
-static int
-do_eventfd(struct tcb *tcp, int flags_arg)
-{
-	tprintf("%lu", tcp->u_arg[0]);
-	if (flags_arg >= 0) {
-		tprints(", ");
-		printflags(open_mode_flags, tcp->u_arg[flags_arg], "O_???");
-	}
-
-	return RVAL_DECODED;
-}
-
-SYS_FUNC(eventfd)
-{
-	return do_eventfd(tcp, -1);
-}
-
-SYS_FUNC(eventfd2)
-{
-	return do_eventfd(tcp, 1);
-}
---
+++ eventfd.c
@@ -0,0 +1,23 @@
+#include "defs.h"
+
+static int
+do_eventfd(struct tcb *tcp, int flags_arg)
+{
+	tprintf("%lu", tcp->u_arg[0]);
+	if (flags_arg >= 0) {
+		tprints(", ");
+		printflags(open_mode_flags, tcp->u_arg[flags_arg], "O_???");
+	}
+
+	return RVAL_DECODED;
+}
+
+SYS_FUNC(eventfd)
+{
+	return do_eventfd(tcp, -1);
+}
+
+SYS_FUNC(eventfd2)
+{
+	return do_eventfd(tcp, 1);
+}
