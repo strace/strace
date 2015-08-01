bf62c1b52db799335a13fa56db363ab5d94d44fa
Dmitry V. Levin <ldv@altlinux.org>
2015-08-01 23:04:32 +0000

    epoll_create1: fix flags decoding
    * xlat/epollflags.in: Remove EPOLL_NONBLOCK, provide fallback definition
    for EPOLL_CLOEXEC.
    * tests/epoll_create1.c: New file.
    * tests/epoll_create1.expected: Likewise.
    * tests/epoll_create1.test: New test.
    * tests/Makefile.am (check_PROGRAMS): Add epoll_create1.
    (TESTS): Add epoll_create1.test.
    (EXTRA_DIST): Add epoll_create1.expected.
    * tests/.gitignore: Add epoll_create1.

---
+++ tests/epoll_create1.c
@@ -0,0 +1,20 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int
+main(void)
+{
+#ifdef __NR_epoll_create1
+	(void) close(0);
+	if (syscall(__NR_epoll_create1, O_CLOEXEC))
+		return 77;
+	return syscall(__NR_epoll_create1, O_CLOEXEC | O_NONBLOCK) >= 0;
+#else
+        return 77;
+#endif
+}
