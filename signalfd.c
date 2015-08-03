2307d5c96884c3991067b2b04b25bb1ad90596fd
Dmitry V. Levin <ldv@altlinux.org>
2015-08-03 09:12:26 +0000

    signal.c: move signalfd parsers to a separate file
    * signalfd.c: New file.
    * Makefile.am (strace_SOURCES): Add it.
    * signal.c (do_signalfd, sys_signalfd, sys_signalfd4): Move to signalfd.c

---
+++ signalfd.c
@@ -0,0 +1,27 @@
+#include "defs.h"
+
+static int
+do_signalfd(struct tcb *tcp, int flags_arg)
+{
+	/* NB: kernel requires arg[2] == NSIG / 8 */
+	printfd(tcp, tcp->u_arg[0]);
+	tprints(", ");
+	print_sigset_addr_len(tcp, tcp->u_arg[1], tcp->u_arg[2]);
+	tprintf(", %lu", tcp->u_arg[2]);
+	if (flags_arg >= 0) {
+		tprints(", ");
+		printflags(open_mode_flags, tcp->u_arg[flags_arg], "O_???");
+	}
+
+	return RVAL_DECODED;
+}
+
+SYS_FUNC(signalfd)
+{
+	return do_signalfd(tcp, -1);
+}
+
+SYS_FUNC(signalfd4)
+{
+	return do_signalfd(tcp, 3);
+}
