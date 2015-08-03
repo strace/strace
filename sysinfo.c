3a58e46d456576832acf233de50834ab0d7c7a5d
Elvira Khabirova <lineprinter0@gmail.com>
2015-08-03 05:37:52 +0300

    sysinfo.c: remove obsolete code
    struct sysinfo members totalhigh, freehigh and mem_unit are present
    since Linux 2.3.48.
    
    * configure.ac (AC_CHECK_MEMBERS): Remove struct sysinfo checks.
    * sysinfo.c (sys_sysinfo): Remove HAVE_STRUCT_SYSINFO_* checks.

--- sysinfo.c
+++ sysinfo.c
@@ -18,15 +18,9 @@ SYS_FUNC(sysinfo)
 			", totalswap=%llu"
 			", freeswap=%llu"
 			", procs=%u"
-#ifdef HAVE_STRUCT_SYSINFO_TOTALHIGH
 			", totalhigh=%llu"
-#endif
-#ifdef HAVE_STRUCT_SYSINFO_FREEHIGH
 			", freehigh=%llu"
-#endif
-#ifdef HAVE_STRUCT_SYSINFO_MEM_UNIT
 			", mem_unit=%u"
-#endif
 			"}",
 			(unsigned long long) si.uptime
 			, (unsigned long long) si.loads[0]
@@ -39,15 +33,9 @@ SYS_FUNC(sysinfo)
 			, (unsigned long long) si.totalswap
 			, (unsigned long long) si.freeswap
 			, (unsigned) si.procs
-#ifdef HAVE_STRUCT_SYSINFO_TOTALHIGH
 			, (unsigned long long) si.totalhigh
-#endif
-#ifdef HAVE_STRUCT_SYSINFO_FREEHIGH
 			, (unsigned long long) si.freehigh
-#endif
-#ifdef HAVE_STRUCT_SYSINFO_MEM_UNIT
 			, si.mem_unit
-#endif
 			);
 	}
 
