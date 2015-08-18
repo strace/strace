fe50316d2cfe89963ba663e22cf28a6a0d309960
Dmitry V. Levin <ldv@altlinux.org>
2015-08-18 18:33:44 +0000

    Simplify printing of get_robust_list first argument
    * get_robust_list.c (sys_get_robust_list): Print first argument
    using "%d" format.

--- get_robust_list.c
+++ get_robust_list.c
@@ -3,7 +3,7 @@
 SYS_FUNC(get_robust_list)
 {
 	if (entering(tcp)) {
-		tprintf("%ld, ", (long) (pid_t) tcp->u_arg[0]);
+		tprintf("%d, ", (int) tcp->u_arg[0]);
 	} else {
 		printnum_ptr(tcp, tcp->u_arg[1]);
 		tprints(", ");
