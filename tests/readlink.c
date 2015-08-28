c633188e826bfaa5ed57d6d475b6c00049257c29
Dmitry V. Levin <ldv@altlinux.org>
2015-08-28 08:43:57 +0000

    tests: convert readlink.test and readlinkat.test from match_awk to match_diff
    * tests/readlink.c (main): Print expected output.
    * tests/readlinkat.c (main): Likewise.
    * tests/readlink.test: Use match_diff instead of match_awk.
    * tests/readlinkat.test: Likewise.
    * tests/readlink.awk: Remove.
    * tests/readlinkat.awk: Likewise.
    * tests/Makefile.am (EXTRA_DIST): Remove readlink.awk and readlinkat.awk.

--- tests/readlink.c
+++ tests/readlink.c
@@ -1,3 +1,8 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 
@@ -5,11 +10,24 @@ int
 main(void)
 {
 #ifdef __NR_readlink
-	char buf[31];
+	static const char fname[] = "readlink.link";
+	unsigned char buf[31];
+	long rc;
+	unsigned int i;
 
-	if (syscall(__NR_readlink, "readlink.link", buf, sizeof(buf)) != 10)
+	rc = syscall(__NR_readlink, fname, buf, sizeof(buf));
+	if (rc < 0)
 		return 77;
 
+	printf("readlink(\"");
+	for (i = 0; fname[i]; ++i)
+		printf("\\x%02x", (int) (unsigned char) fname[i]);
+	printf("\", \"");
+	for (i = 0; i < 3; ++i)
+		printf("\\x%02x", (int) buf[i]);
+	printf("\"..., %zu) = %ld\n", sizeof(buf), rc);
+
+	puts("+++ exited with 0 +++");
 	return 0;
 #else
 	return 77;
--- tests/readlinkat.c
+++ tests/readlinkat.c
@@ -1,3 +1,8 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 
@@ -5,11 +10,24 @@ int
 main(void)
 {
 #ifdef __NR_readlinkat
-	char buf[31];
+	static const char fname[] = "readlinkat.link";
+	unsigned char buf[31];
+	long rc;
+	unsigned int i;
 
-	if (syscall(__NR_readlinkat, -100, "readlinkat.link", buf, sizeof(buf)) != 12)
+	rc = syscall(__NR_readlinkat, -100, fname, buf, sizeof(buf));
+	if (rc < 0)
 		return 77;
 
+	printf("readlinkat(AT_FDCWD, \"");
+	for (i = 0; fname[i]; ++i)
+		printf("\\x%02x", (int) (unsigned char) fname[i]);
+	printf("\", \"");
+	for (i = 0; i < 3; ++i)
+		printf("\\x%02x", (int) buf[i]);
+	printf("\"..., %zu) = %ld\n", sizeof(buf), rc);
+
+	puts("+++ exited with 0 +++");
 	return 0;
 #else
 	return 77;
