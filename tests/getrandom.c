f008b01b691ca7ede0cf3f3bf2ef5bf2d77dcea6
Dmitry V. Levin <ldv@altlinux.org>
2015-08-28 07:11:17 +0000

    tests: convert getrandom.test from match_awk to match_diff
    * tests/getrandom.c (main): Print expected output.
    * tests/getrandom.test: Use match_diff instead of match_awk.
    * tests/getrandom.awk: Remove.
    * tests/Makefile.am (EXTRA_DIST): Remove getrandom.awk.

--- tests/getrandom.c
+++ tests/getrandom.c
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2015 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 
@@ -5,15 +37,24 @@ int
 main(void)
 {
 #ifdef __NR_getrandom
-	char buf[4];
+	unsigned char buf[4];
 
 	if (syscall(__NR_getrandom, buf, sizeof(buf) - 1, 0) != sizeof(buf) - 1)
 		return 77;
+	printf("getrandom(\"\\x%02x\\x%02x\\x%02x\", 3, 0) = 3\n",
+	       (int) buf[0], (int) buf[1], (int) buf[2]);
+
 	if (syscall(__NR_getrandom, buf, sizeof(buf), 1) != sizeof(buf))
 		return 77;
+	printf("getrandom(\"\\x%02x\\x%02x\\x%02x\"..., 4, GRND_NONBLOCK) = 4\n",
+	       (int) buf[0], (int) buf[1], (int) buf[2]);
+
 	if (syscall(__NR_getrandom, buf, sizeof(buf), 0x3003) != -1)
 		return 77;
+	printf("getrandom(%p, 4, GRND_NONBLOCK|GRND_RANDOM|0x3000) = "
+	       "-1 EINVAL (Invalid argument)\n", buf);
 
+	puts("+++ exited with 0 +++");
 	return 0;
 #else
 	return 77;
