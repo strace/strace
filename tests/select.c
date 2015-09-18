10a40757bf76c7a84894afcfa68fe69893b8b8de
Dmitry V. Levin <ldv@altlinux.org>
2015-09-18 21:45:36 +0000

    tests: uncouple pselect6 from select.test
    * tests/select.c Do not include <string.h>.
    Include <sys/syscall.h>.
    [!__NR_select]: Skip the test.
    (main): Remove pselect6 support, test just select syscall.
    * tests/select.test: Remove all pselect6 checks, test just
    select syscall.

--- tests/select.c
+++ tests/select.c
@@ -2,13 +2,23 @@
  * Based on test by Dr. David Alan Gilbert <dave@treblig.org>
  */
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/select.h>
+#include <sys/syscall.h>
+
+#undef NR_select
+
+#if defined __NR__newselect
+# define NR_select __NR__newselect
+#elif defined __NR_select
+# define NR_select __NR_select
+#endif
+
+#ifdef NR_select
 
 static fd_set set[0x1000000 / sizeof(fd_set)];
 
-int main(int ac, char **av)
+int main(void)
 {
 	int fds[2];
 	struct {
@@ -18,7 +28,6 @@ int main(int ac, char **av)
 		.tv = { .tv_usec = 123 },
 		.pad = { 0xdeadbeef, 0xbadc0ded }
 	};
-	int is_select = ac < 2 || strcmp(av[1], "pselect6");
 
 	if (pipe(fds))
 		return 77;
@@ -29,17 +38,11 @@ int main(int ac, char **av)
 	FD_ZERO(set);
 	FD_SET(fds[0], set);
 	FD_SET(fds[1], set);
-	if (select(fds[1] + 1, set, set, set, NULL) != 1)
+	if (syscall(NR_select, fds[1] + 1, set, set, set, NULL) != 1)
 		return 77;
-	if (is_select)
-		printf("select(%d, [%d %d], [%d %d], [%d %d], NULL) = 1 ()\n",
-		       fds[1] + 1, fds[0], fds[1],
-		       fds[0], fds[1], fds[0], fds[1]);
-	else
-		printf("pselect6(%d, [%d %d], [%d %d], [%d %d], NULL, NULL) "
-		       "= 1 ()\n",
-		       fds[1] + 1, fds[0], fds[1],
-		       fds[0], fds[1], fds[0], fds[1]);
+	printf("select(%d, [%d %d], [%d %d], [%d %d], NULL) = 1 ()\n",
+	       fds[1] + 1, fds[0], fds[1],
+	       fds[0], fds[1], fds[0], fds[1]);
 
 	/*
 	 * Now the crash case that trinity found, negative nfds
@@ -47,29 +50,31 @@ int main(int ac, char **av)
 	 */
 	FD_ZERO(set);
 	FD_SET(fds[1],set);
-	if (select(-1, NULL, set, NULL, NULL) != -1)
+	if (syscall(NR_select, -1, NULL, set, NULL, NULL) != -1)
 		return 77;
-	if (is_select)
-		printf("select(-1, NULL, %p, NULL, NULL) "
-		       "= -1 EINVAL (Invalid argument)\n", set);
-	else
-		printf("pselect6(-1, NULL, %p, NULL, NULL, NULL) "
-		       "= -1 EINVAL (Invalid argument)\n", set);
+	printf("select(-1, NULL, %p, NULL, NULL) "
+	       "= -1 EINVAL (Invalid argument)\n", set);
 
 	/*
 	 * Another variant, with nfds exceeding FD_SETSIZE limit.
 	 */
 	FD_ZERO(set);
 	FD_SET(fds[0],set);
-	if (select(FD_SETSIZE + 1, set, set + 1, NULL, &tm.tv))
+	if (syscall(NR_select, FD_SETSIZE + 1, set, set + 1, NULL, &tm.tv))
 		return 77;
-	if (is_select)
-		printf("select(%d, [%d], [], NULL, {0, 123}) = 0 (Timeout)\n",
-		       FD_SETSIZE + 1, fds[0]);
-	else
-		printf("pselect6(%d, [%d], [], NULL, {0, 123000}, NULL) "
-		       "= 0 (Timeout)\n", FD_SETSIZE + 1, fds[0]);
+	printf("select(%d, [%d], [], NULL, {0, 123}) = 0 (Timeout)\n",
+	       FD_SETSIZE + 1, fds[0]);
 
 	puts("+++ exited with 0 +++");
 	return 0;
 }
+
+#else
+
+int
+main(void)
+{
+	return 77;
+}
+
+#endif
