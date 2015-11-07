370eb16ed232e281010303143313c01ae5268428
Dmitry V. Levin <ldv@altlinux.org>
2015-11-07 23:22:24 +0000

    Fix *_printer_defs.h rules ambiguousness
    There is an ambiguousness in *_printer_defs.h rules definition:
    
    $ grep '^[^ ]*_defs\.h: ' Makefile.am
    m%_defs.h: $(srcdir_mpers_source_files)
    %_printer_defs.h: $(srcdir_mpers_source_files)
    
    While it appears to work with GNU make, it has to be fixed nevertheless.
    
    * Makefile.am (m%_defs.h): Rename to m%_type_defs.h.
    (mpers_m32_targets): Rename m32_defs.h to m32_type_defs.h.
    (mpers_mx32_targets): Rename mx32_defs.h to mx32_type_defs.h.
    * mpers_type.h: Rename m32_defs.h to m32_type_defs.h,
    mx32_defs.h to mx32_type_defs.h.
    * .gitignore: Likewise.
    
    Reported-by: Elliott Hughes <enh@google.com>

--- mpers_type.h
+++ mpers_type.h
@@ -3,10 +3,10 @@
 # define DEF_MPERS_TYPE(args) STRINGIFY(args.h)
 # ifdef MPERS_IS_m32
 #  define MPERS_PREFIX m32_
-#  define MPERS_DEFS "m32_defs.h"
+#  define MPERS_DEFS "m32_type_defs.h"
 # elif defined MPERS_IS_mx32
 #  define MPERS_PREFIX mx32_
-#  define MPERS_DEFS "mx32_defs.h"
+#  define MPERS_DEFS "mx32_type_defs.h"
 # endif
 #else
 # define MPERS_PREFIX
