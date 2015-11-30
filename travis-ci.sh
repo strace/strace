c9d2501a361766aeafe7a946a1c5883c4773fb22
Dmitry V. Levin <ldv@altlinux.org>
2015-11-30 22:18:16 +0000

    travis-ci: add build matrix

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,6 +4,16 @@ j=-j`getconf _NPROCESSORS_ONLN 2> /dev/null` || j=
 set -x
 git fetch --unshallow
 ./git-set-file-times
+case "${TARGET-}" in
+  x32)
+    CC="$CC -mx32"
+    ;;
+  x86)
+    export DISTCHECK_CONFIGURE_FLAGS='--build=i686-pc-linux-gnu'
+    CC="$CC -m32"
+    ;;
+esac
+export CC_FOR_BUILD="$CC"
 ./bootstrap
-./configure --enable-maintainer-mode ${ENABLE_GCC_WERROR-}
-make $j distcheck VERBOSE=${VERBOSE-}
+./configure --enable-maintainer-mode ${ENABLE_GCC_WERROR-} ${DISTCHECK_CONFIGURE_FLAGS-}
+make -k $j distcheck VERBOSE=${VERBOSE-}
