c1bb87a99d29dd0d106031d03d65cd4185c29d4b
Dmitry V. Levin <ldv@altlinux.org>
2015-12-01 00:02:45 +0000

    tests: add ftruncate.test and truncate.test
    * tests/ftruncate.c: New file.
    * tests/truncate.c: Likewise.
    * tests/ftruncate.test: New test.
    * tests/truncate.test: Likewise.
    * tests/Makefile.am (check_PROGRAMS): Add ftruncate and truncate.
    (TESTS): Add ftruncate.test and truncate.test.
    * tests/.gitignore: Add ftruncate and truncate.

---
+++ tests/truncate.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <sys/syscall.h>
+
+#ifdef __NR_truncate
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "kernel_types.h"
+
+int
+main(void)
+{
+	static const char fname[] = "truncate\nfilename";
+	static const char qname[] = "truncate\\nfilename";
+	const kernel_ulong_t len = (kernel_ulong_t) 0xdefaced0badc0deULL;
+	int rc;
+
+	if (sizeof(len) > sizeof(long))
+		rc = truncate(fname, len);
+	else
+		rc = syscall(__NR_truncate, fname, len);
+
+	if (rc != -1 || ENOENT != errno)
+		return 77;
+
+	printf("truncate(\"%s\", %Lu) = -1 ENOENT (No such file or directory)\n",
+	       qname, (unsigned long long) len);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+int
+main(void)
+{
+	return 77;
+}
+
+#endif
