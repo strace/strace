9db0797b22cbcf5687307329c6874d40ba5a6645
Dmitry V. Levin <ldv@altlinux.org>
2015-12-01 00:59:26 +0000

    tests: add lseek.test and llseek.test
    * tests/llseek.c: New file.
    * tests/lseek.c: Likewise.
    * tests/llseek.test: New test.
    * tests/lseek.test: Likewise.
    * tests/Makefile.am (check_PROGRAMS): Add llseek and lseek.
    (TESTS): Add llseek.test and lseek.test.
    * tests/.gitignore: Add llseek and lseek.

---
+++ tests/lseek.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2015 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <sys/syscall.h>
+
+#ifdef __NR_lseek
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "kernel_types.h"
+
+int
+main(void)
+{
+	const kernel_ulong_t offset = (kernel_ulong_t) 0xdefaced0badc0deULL;
+	int rc;
+
+	if (sizeof(offset) > sizeof(long))
+		rc = lseek(-1, offset, SEEK_SET);
+	else
+		rc = syscall(__NR_lseek, -1L, offset, SEEK_SET);
+
+	if (rc != -1 || EBADF != errno)
+		return 77;
+
+	printf("lseek(-1, %Lu, SEEK_SET) = -1 EBADF (Bad file descriptor)\n",
+	       (unsigned long long) offset);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+int
+main(void)
+{
+	return 77;
+}
+
+#endif
