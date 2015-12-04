5627cf90803b2f1c69ee5c7626294236e2be9c1c
Dmitry V. Levin <ldv@altlinux.org>
2015-12-04 14:31:07 +0000

    kernel_types.h: protect from double inclusion
    * kernel_types.h: Add #include guard.

--- kernel_types.h
+++ kernel_types.h
@@ -1,21 +1,25 @@
-#if defined HAVE___KERNEL_LONG_T && defined HAVE___KERNEL_ULONG_T
+#ifndef STRACE_KERNEL_TYPES_H
 
-#include <asm/posix_types.h>
+# define STRACE_KERNEL_TYPES_H
+
+# if defined HAVE___KERNEL_LONG_T && defined HAVE___KERNEL_ULONG_T
+
+# include <asm/posix_types.h>
 
 typedef __kernel_long_t kernel_long_t;
 typedef __kernel_ulong_t kernel_ulong_t;
 
-#elif defined __x86_64__ && defined __ILP32__
+# elif defined __x86_64__ && defined __ILP32__
 
 typedef long long kernel_long_t;
 typedef unsigned long long kernel_ulong_t;
 
-#else
+# else
 
 typedef long kernel_long_t;
 typedef unsigned long kernel_ulong_t;
 
-#endif
+# endif
 
 typedef struct {
 	kernel_ulong_t	d_ino;
@@ -23,3 +27,5 @@ typedef struct {
 	unsigned short	d_reclen;
 	char		d_name[1];
 } kernel_dirent;
+
+#endif
