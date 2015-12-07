97af1be4c6449c7efbb3537d2005a2868f0af4c2
Dmitry V. Levin <ldv@altlinux.org>
2015-12-07 00:10:58 +0000

    Update umount2 flags
    * umount.c (MNT_FORCE, MNT_DETACH, MNT_EXPIRE): Move definitions ...
    * xlat/umount_flags.in: ... here.  Add UMOUNT_NOFOLLOW.

--- umount.c
+++ umount.c
@@ -1,9 +1,4 @@
 #include "defs.h"
-
-#define MNT_FORCE	0x00000001	/* Attempt to forcibily umount */
-#define MNT_DETACH	0x00000002	/* Just detach from the tree */
-#define MNT_EXPIRE	0x00000004	/* Mark for expiry */
-
 #include "xlat/umount_flags.h"
 
 SYS_FUNC(umount2)
