18eeb96799564c38c9339fe80b566242ceb4bff2
Dmitry V. Levin <ldv@altlinux.org>
2016-01-05 23:06:14 +0000

    tests/fanotify_mark.c: use libtests
    * tests/fanotify_mark.c: Use SKIP_MAIN_UNDEFINED.

--- tests/fanotify_mark.c
+++ tests/fanotify_mark.c
@@ -1,16 +1,18 @@
 #include "tests.h"
+
 #if defined HAVE_SYS_FANOTIFY_H && defined HAVE_FANOTIFY_MARK
+
 # include <sys/fanotify.h>
+
 int
 main(void)
 {
 	fanotify_mark(-1, FAN_MARK_ADD, FAN_MODIFY | FAN_ONDIR, -100, ".");
 	return 0;
 }
+
 #else
-int
-main(void)
-{
-	return 77;
-}
+
+SKIP_MAIN_UNDEFINED("HAVE_SYS_FANOTIFY_H && HAVE_FANOTIFY_MARK")
+
 #endif
