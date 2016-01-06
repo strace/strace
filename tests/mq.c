bbbb97fb3021d696e58d83e3bf9524d1ab884092
Dmitry V. Levin <ldv@altlinux.org>
2016-01-06 11:52:43 +0000

    tests/mq.c: use libtests
    * tests/mq.c: Use SKIP_MAIN_UNDEFINED.
    (main): Use perror_msg_and_skip.

--- tests/mq.c
+++ tests/mq.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Elvira Khabirova <lineprinter0@gmail.com>
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,33 +27,33 @@
  */
 
 #include "tests.h"
+
 #ifdef HAVE_MQUEUE_H
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <mqueue.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <mqueue.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
 
 int
 main (void)
 {
 	struct mq_attr attr;
 	(void) close(0);
-	if (mq_open("/strace-mq.test", O_CREAT, S_IRWXU, 0) ||
-		mq_getattr(0, &attr) ||
-		mq_setattr(0, &attr, 0) ||
-		mq_unlink("/strace-mq.test"))
-		return 77;
+	if (mq_open("/strace-mq.test", O_CREAT, S_IRWXU, 0))
+		perror_msg_and_skip("mq_open");
+	if (mq_getattr(0, &attr))
+		perror_msg_and_skip("mq_getattr");
+	if (mq_setattr(0, &attr, 0))
+		perror_msg_and_skip("mq_setattr");
+	if (mq_unlink("/strace-mq.test"))
+		perror_msg_and_skip("mq_unlink");
 	return 0;
 }
 
 #else
 
-int
-main(void)
-{
-	return 77;
-}
+SKIP_MAIN_UNDEFINED("HAVE_MQUEUE_H")
 
 #endif
