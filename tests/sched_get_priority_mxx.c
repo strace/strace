1eed594f30ee7030cb1c224267f4fdb5f34bf773
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-03-10 17:12:23 +0800

    tests: add sched_get_priority_mxx.test
    * tests/sched_get_priority_mxx.c: New file.
    * tests/sched_get_priority_mxx.test: New test.
    * tests/.gitignore: Add sched_get_priority_mxx.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (TESTS): Add sched_get_priority_mxx.test.

---
+++ tests/sched_get_priority_mxx.c
@@ -0,0 +1,29 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#if defined(__NR_sched_get_priority_min) \
+ && defined(__NR_sched_get_priority_max)
+
+# include <sched.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	int rc = syscall(__NR_sched_get_priority_min, SCHED_FIFO);
+	printf("sched_get_priority_min(SCHED_FIFO) = %d\n", rc);
+
+	rc = syscall(__NR_sched_get_priority_max, SCHED_RR);
+	printf("sched_get_priority_max(SCHED_RR) = %d\n", rc);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_sched_get_priority_min"
+                    " && defined __NR_sched_get_priority_max");
+
+#endif
