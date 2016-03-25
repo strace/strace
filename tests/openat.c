6598026d6a04b3ee256124f437dc7653c601ad35
Katerina Koukiou <k.koukiou@googlemail.com>
2016-03-25 21:40:12 +0200

    tests: add openat.test
    * tests/openat.c: New file.
    * tests/openat.test: New test.
    * tests/.gitignore: Add openat.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add openat.test.

---
+++ tests/openat.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016 Katerina Koukiou <k.koukiou@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+#include <sys/syscall.h>
+
+#if defined __NR_openat
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <errno.h>
+
+int
+main(void)
+{
+	static const char fname[] = "openat_test_file";
+	int fd;
+
+	fd = syscall(__NR_openat, -100, fname, O_RDONLY|O_CREAT, 0400);
+	if (fd == -1) {
+		if (errno == ENOSYS) {
+			printf("openat(AT_FDCWD, \"%s\", O_RDONLY|O_CREAT"
+		               ", 0400) = -1 ENOSYS (%m)\n", fname);
+		} else {
+			perror_msg_and_fail("openat");
+		}
+	} else {
+		printf("openat(AT_FDCWD, \"%s\", O_RDONLY|O_CREAT"
+		       ", 0400) = %d\n", fname, fd);
+		close(fd);
+		if (unlink(fname) == -1)
+			perror_msg_and_fail("unlink");
+		fd = syscall(__NR_openat, -100, fname, O_RDONLY);
+		if (fd == -1 && errno == ENOENT) {
+			printf("openat(AT_FDCWD, \"%s\", O_RDONLY) = %d ENOENT"
+			       " (%m)\n", fname, fd);
+		} else {
+			perror_msg_and_fail("openat");
+		}
+	}
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_openat")
+
+#endif
