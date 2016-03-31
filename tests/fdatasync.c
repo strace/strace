4c4ab0fe823ff20e4076e50dd9ef9a1ab1da0199
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-03-31 16:06:51 +0800

    tests: add fdatasync.test
    * tests/fdatasync.c: New file.
    * tests/fdatasync.test: New test.
    * tests/.gitignore: Add fdatasync.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add fdatasync.test.

---
+++ tests/fdatasync.c
@@ -0,0 +1,27 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_fdatasync
+
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	const long int fd = (long int) 0xdeadbeefffffffff;
+	int rc = syscall(__NR_fdatasync, fd);
+	printf("fdatasync(%d) = %d %s (%m)\n",
+	       (int) fd, rc,
+	       errno == ENOSYS ? "ENOSYS" : "EBADF");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_fdatasync")
+
+#endif
