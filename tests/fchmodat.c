baeff232cee9e2a408ddf6b6d36d6ce70a26d005
Dmitry V. Levin <ldv@altlinux.org>
2016-04-02 13:13:16 +0000

    tests: add fchmodat.test
    * tests/fchmodat.c: New file.
    * tests/fchmodat.test: New test.
    * tests/.gitignore: Add fchmodat.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add fchmodat.test.

---
+++ tests/fchmodat.c
@@ -0,0 +1,76 @@
+/*
+ * Check decoding of fchmodat syscall.
+ *
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_fchmodat
+
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample[] = "fchmodat_sample";
+	const char *err;
+
+	if (open(sample, O_RDONLY | O_CREAT, 0400) == -1)
+		perror_msg_and_fail("open");
+
+	if (syscall(__NR_fchmodat, -100, sample, 0600) == 0) {
+		printf("fchmodat(AT_FDCWD, \"%s\", 0600) = 0\n", sample);
+
+		if (unlink(sample))
+			perror_msg_and_fail("unlink");
+
+		if (syscall(__NR_fchmodat, -100, sample, 0600) != -1)
+			perror_msg_and_fail("fchmodat");
+
+		err = "ENOENT";
+	} else {
+		if (errno != ENOSYS)
+			perror_msg_and_fail("fchmodat");
+		err = "ENOSYS";
+	}
+
+	printf("fchmodat(AT_FDCWD, \"%s\", 0600) = -1 %s (%m)\n",
+	       sample, err);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_fchmodat")
+
+#endif
