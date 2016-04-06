5b2d25783971ca673c607b3be5ff23191323a97c
Dmitry V. Levin <ldv@altlinux.org>
2016-04-06 00:02:08 +0000

    tests: add sync_file_range.test and sync_file_range2.test
    * configure.ac (AC_CHECK_FUNCS): Add sync_file_range.
    * tests/sync_file_range.c: New file.
    * tests/sync_file_range2.c: Likewise.
    * tests/sync_file_range.test: New test.
    * tests/sync_file_range2.test: Likewise.
    * tests/.gitignore: Add sync_file_range and sync_file_range2.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add sync_file_range.test and sync_file_range2.test.

---
+++ tests/sync_file_range2.c
@@ -0,0 +1,65 @@
+/*
+ * Check decoding of sync_file_range2 syscall.
+ *
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+#include <fcntl.h>
+#include <sys/syscall.h>
+
+#if defined HAVE_SYNC_FILE_RANGE && defined  __NR_sync_file_range2
+
+# include <assert.h>
+# include <errno.h>
+# include <stdio.h>
+
+int
+main(void)
+{
+	const int fd = -1;
+	const off64_t offset = 0xdeadbeefbadc0ded;
+	const off64_t nbytes = 0xfacefeedcafef00d;
+	const unsigned int flags = -1;
+
+	assert(sync_file_range(fd, offset, nbytes, flags) == -1);
+	printf("%s(%d, SYNC_FILE_RANGE_WAIT_BEFORE"
+	       "|SYNC_FILE_RANGE_WRITE|SYNC_FILE_RANGE_WAIT_AFTER"
+	       "|0xfffffff8, %lld, %lld) = -1 %s (%m)\n",
+	       "sync_file_range2", fd,
+	       (long long) offset,
+	       (long long) nbytes,
+	       errno == ENOSYS ? "ENOSYS" : "EINVAL");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("HAVE_SYNC_FILE_RANGE && __NR_sync_file_range2")
+
+#endif
