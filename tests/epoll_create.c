bbdd23c8ed00d90909a9de56ddd5ece9c93fba38
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-08 15:59:14 +0800

    tests: add epoll_create.test
    * tests/epoll_create.c: New file.
    * tests/epoll_create.test: New test.
    * tests/.gitignore: Add epoll_create.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add epoll_create.test.

---
+++ tests/epoll_create.c
@@ -0,0 +1,25 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_epoll_create
+
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	int rc = syscall(__NR_epoll_create, -1);
+	printf("epoll_create(-1) = %d %s (%m)\n",
+	       rc, errno == ENOSYS ? "ENOSYS" : "EINVAL");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_epoll_creat")
+
+#endif
