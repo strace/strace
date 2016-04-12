c2523e0d9cc80d894ed47189b4fb39e56d41f915
Dmitry V. Levin <ldv@altlinux.org>
2016-04-12 00:20:03 +0000

    tests: add seccomp-filter.test
    * tests/seccomp-filter.c: New file.
    * tests/seccomp-filter.test: New test.
    * tests/.gitignore: Add seccomp-filter.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add seccomp-filter.test.

---
+++ tests/seccomp-filter.c
@@ -0,0 +1,77 @@
+/*
+ * Check decoding of seccomp SECCOMP_SET_MODE_FILTER.
+ *
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#ifdef HAVE_LINUX_SECCOMP_H
+# include <linux/seccomp.h>
+#endif
+#ifdef HAVE_LINUX_FILTER_H
+# include <linux/filter.h>
+#endif
+
+#if defined __NR_seccomp && defined SECCOMP_SET_MODE_FILTER
+
+# define N 7
+
+int
+main(void)
+{
+	struct sock_filter *const filter = tail_alloc(sizeof(*filter) * N);
+	const void *const efault = tail_alloc(1);
+	struct sock_fprog *const prog = tail_alloc(sizeof(*prog));
+
+	prog->filter = filter;
+	prog->len = N;
+	assert(syscall(__NR_seccomp, SECCOMP_SET_MODE_FILTER, -1, prog) == -1);
+	printf("seccomp(SECCOMP_SET_MODE_FILTER, %s, {len=%u, filter=%p})"
+	       " = -1 %s (%m)\n", "SECCOMP_FILTER_FLAG_TSYNC|0xfffffffe",
+	       prog->len, prog->filter,
+	       ENOSYS == errno ? "ENOSYS" : "EINVAL");
+
+	assert(syscall(__NR_seccomp, SECCOMP_SET_MODE_FILTER, -2L, efault) == -1);
+	printf("seccomp(SECCOMP_SET_MODE_FILTER, %s, %p) = -1 %s (%m)\n",
+	       "0xfffffffe /* SECCOMP_FILTER_FLAG_??? */", efault,
+	       ENOSYS == errno ? "ENOSYS" : "EINVAL");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_seccomp && SECCOMP_SET_MODE_FILTER")
+
+#endif
