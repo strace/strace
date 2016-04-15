f559a876f8d8102bad185bb5c450dd5b9054e8ec
Dmitry V. Levin <ldv@altlinux.org>
2016-04-15 23:03:29 +0000

    tests: check decoding of chown32 syscall
    * tests/chown32.c: New file.
    * tests/chown32.test: New test.
    * tests/.gitignore: Add chown32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add chown32.test.

---
+++ tests/chown32.c
@@ -0,0 +1,14 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_chown32
+
+# define SYSCALL_NR __NR_chown32
+# define SYSCALL_NAME "chown32"
+# include "xchownx.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_chown32")
+
+#endif
