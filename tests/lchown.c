408420f51f6781081429fdfa7948f05679db1c90
Dmitry V. Levin <ldv@altlinux.org>
2016-04-16 23:02:27 +0000

    tests: check decoding of lchown syscall
    * tests/lchown.c: New file.
    * tests/lchown.test: New test.
    * tests/.gitignore: Add lchown.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add lchown.test.

---
+++ tests/lchown.c
@@ -0,0 +1,19 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_lchown
+
+# define SYSCALL_NR __NR_lchown
+# define SYSCALL_NAME "lchown"
+
+# if defined __NR_lchown32 && __NR_lchown != __NR_lchown32
+#  define UGID_TYPE_IS_SHORT
+# endif
+
+# include "xchownx.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_lchown")
+
+#endif
