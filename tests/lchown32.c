11f98f21b781f4da069591c0441e87d5155102b2
Dmitry V. Levin <ldv@altlinux.org>
2016-04-16 23:02:27 +0000

    tests: check decoding of lchown32 syscall
    * tests/lchown32.c: New file.
    * tests/lchown32.test: New test.
    * tests/.gitignore: Add lchown32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add lchown32.test.

---
+++ tests/lchown32.c
@@ -0,0 +1,14 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_lchown32
+
+# define SYSCALL_NR __NR_lchown32
+# define SYSCALL_NAME "lchown32"
+# include "xchownx.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_lchown32")
+
+#endif
