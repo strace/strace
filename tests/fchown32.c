96492b2acd87bc593df8328967a9cc2af4951003
Dmitry V. Levin <ldv@altlinux.org>
2016-04-17 22:24:41 +0000

    tests: check decoding of fchown32 syscall
    * tests/fchown32.c: New file.
    * tests/fchown32.test: New test.
    * tests/.gitignore: Add fchown32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add fchown32.test.

---
+++ tests/fchown32.c
@@ -0,0 +1,15 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_fchown32
+
+# define SYSCALL_NR __NR_fchown32
+# define SYSCALL_NAME "fchown32"
+# define ACCESS_BY_DESCRIPTOR
+# include "xchownx.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_fchown32")
+
+#endif
