fa7c988bd9be9c073beeaceaf0a291a1e6b84cdb
Dmitry V. Levin <ldv@altlinux.org>
2016-04-18 15:23:56 +0000

    tests: extend test coverage of setresgid32 syscall
    * tests/setresgid32.c: New file.
    * tests/setresgid32.test: New test.
    * tests/.gitignore: Add setresgid32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setresgid32.test.

---
+++ tests/setresgid32.c
@@ -0,0 +1,17 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_setresgid32
+
+# define SYSCALL_NR	__NR_setresgid32
+# define SYSCALL_NAME	"setresgid32"
+# define UGID_TYPE	int
+# define GETUGID	getegid()
+# define CHECK_OVERFLOWUGID(arg)
+# include "setresugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setresgid32")
+
+#endif
