fba762db719ffe3475f79784192a374d8f664923
Dmitry V. Levin <ldv@altlinux.org>
2016-04-18 16:08:38 +0000

    tests: check decoding of setfsgid syscall
    * tests/setfsugid.c: New file.
    * tests/setfsgid.c: New file.
    * tests/setfsgid.test: New test.
    * tests/.gitignore: Add setfsgid.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setfsgid.test.
    (EXTRA_DIST): Add setfsugid.c.

---
+++ tests/setfsgid.c
@@ -0,0 +1,23 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_setfsgid
+
+# define SYSCALL_NR	__NR_setfsgid
+# define SYSCALL_NAME	"setfsgid"
+
+# if defined __NR_setfsgid32 && __NR_setfsgid != __NR_setfsgid32
+#  define UGID_TYPE	short
+#  define GETUGID	syscall(__NR_getegid)
+# else
+#  define UGID_TYPE	int
+#  define GETUGID	getegid()
+# endif
+
+# include "setfsugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setfsgid")
+
+#endif
