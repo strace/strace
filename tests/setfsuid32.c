2def3d4a0427d0dabb96a56d2c247c755ebb2268
Dmitry V. Levin <ldv@altlinux.org>
2016-04-18 16:03:51 +0000

    tests: check decoding of setfsuid32 syscall
    * tests/setfsuid32.c: New file.
    * tests/setfsuid32.test: New test.
    * tests/.gitignore: Add setfsuid32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setfsuid32.test.

---
+++ tests/setfsuid32.c
@@ -0,0 +1,16 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_setfsuid32
+
+# define SYSCALL_NR	__NR_setfsuid32
+# define SYSCALL_NAME	"setfsuid32"
+# define UGID_TYPE	int
+# define GETUGID	geteuid()
+# include "setfsugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setfsuid32")
+
+#endif
