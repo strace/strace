8f0e41afb7aea5006cce8bf5f002509374aacc1f
Dmitry V. Levin <ldv@altlinux.org>
2016-04-18 13:41:25 +0000

    tests: extend test coverage of setuid32 syscall
    * tests/setuid32.c: New file.
    * tests/setuid32.test: New test.
    * tests/.gitignore: Add setuid32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setuid32.test.

---
+++ tests/setuid32.c
@@ -0,0 +1,17 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_setuid32
+
+# define SYSCALL_NR	__NR_setuid32
+# define SYSCALL_NAME	"setuid32"
+# define UGID_TYPE	int
+# define GETUGID	geteuid()
+# define CHECK_OVERFLOWUGID(arg)
+# include "setugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setuid32")
+
+#endif
