dcc66602560714f196dd2f98f8e0a485567a909e
Dmitry V. Levin <ldv@altlinux.org>
2016-04-19 17:34:00 +0000

    tests: extend test coverage of getresuid32 syscall
    * tests/getresuid32.c: New file.
    * tests/getresuid32.test: New test.
    * tests/.gitignore: Add getresuid32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add getresuid32.test.

---
+++ tests/getresuid32.c
@@ -0,0 +1,15 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_getresuid32
+
+# define SYSCALL_NR	__NR_getresuid32
+# define SYSCALL_NAME	"getresuid32"
+# define UGID_TYPE	int
+# include "getresugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_getresuid32")
+
+#endif
