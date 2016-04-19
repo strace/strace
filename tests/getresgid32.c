7f3da2ff4c69e26e820408ed07856181b034c2c4
Dmitry V. Levin <ldv@altlinux.org>
2016-04-19 17:45:26 +0000

    tests: extend test coverage of getresgid32 syscall
    * tests/getresgid32.c: New file.
    * tests/getresgid32.test: New test.
    * tests/.gitignore: Add getresgid32.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add getresgid32.test.

---
+++ tests/getresgid32.c
@@ -0,0 +1,15 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_getresgid32
+
+# define SYSCALL_NR	__NR_getresgid32
+# define SYSCALL_NAME	"getresgid32"
+# define UGID_TYPE	int
+# include "getresugid.c"
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_getresgid32")
+
+#endif
