bff70d431190428f60f4a654d2824f367ffd7353
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-19 15:38:34 +0800

    tests: add symlinkat.test
    * tests/symlinkat.c: New file.
    * tests/symlinkat.test: New test.
    * tests/.gitignore: Add symlinkat.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add symlinkat.test.

---
+++ tests/symlinkat.c
@@ -0,0 +1,29 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_symlinkat
+
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	const long int fd = (long int) 0xdeadbeefffffffff;
+	static const char oldpath[] = "symlink_old";
+	static const char newpath[] = "symlink_new";
+	long rc = syscall(__NR_symlinkat, oldpath, fd, newpath);
+	printf("symlinkat(\"%s\", %d, \"%s\") = %ld %s (%m)\n",
+	       oldpath, (int) fd, newpath, rc,
+	       errno == ENOSYS ? "ENOSYS" : "EBADF");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_symlinkat")
+
+#endif
