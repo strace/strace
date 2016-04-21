f5748b006f127e06c701c474e3389b5557f00912
Dmitry V. Levin <ldv@altlinux.org>
2016-04-21 21:24:01 +0000

    tests/splice.c: stop using assert

--- tests/splice.c
+++ tests/splice.c
@@ -49,12 +49,13 @@ main(void)
 	const size_t len = (size_t) 0xdeadbef3facefed3ULL;
 	const unsigned int flags = 15;
 
-	assert(syscall(__NR_splice, fd_in, off_in, fd_out, off_out,
-		       len, flags) == -1);
-	printf("splice(%d, [%lld], %d, [%lld], %zu, %s) = -1 %s (%m)\n",
+	long rc = syscall(__NR_splice,
+			  fd_in, off_in, fd_out, off_out, len, flags);
+	printf("splice(%d, [%lld], %d, [%lld], %zu, %s) = %ld %s (%m)\n",
 	       (int) fd_in, *off_in, (int) fd_out, *off_out, len,
 	       "SPLICE_F_MOVE|SPLICE_F_NONBLOCK|SPLICE_F_MORE|SPLICE_F_GIFT",
-	       errno2name());
+	       rc, errno2name());
+
 	puts("+++ exited with 0 +++");
 	return 0;
 }
