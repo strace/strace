5c1776fe007205a314d0a84f072484dd19aa3842
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-21 15:53:49 +0800

    tests: add mkdir.test
    * tests/mkdir.c: New file.
    * tests/mkdir.test: New test.
    * tests/.gitignore: Add mkdir.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add mkdir.test.

---
+++ tests/mkdir.c
@@ -0,0 +1,27 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_mkdir
+
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample[] = "mkdir";
+
+	long rc = syscall(__NR_mkdir, sample, 0600);
+	printf("mkdir(\"%s\", 0600) = %ld %s (%m)\n",
+	       sample, rc, errno == ENOSYS ? "ENOSYS" : "EEXIST");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_mkdir")
+
+#endif
