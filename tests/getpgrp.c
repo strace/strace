ef6b4262bb2f287fe103f54ddee13e8cb0adcf37
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-21 15:53:52 +0800

    tests: add getpgrp.test
    * tests/getpgrp.c: New file.
    * tests/getpgrp.test: New test.
    * tests/.gitignore: Add getpgrp.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add getpgrp.test.

---
+++ tests/getpgrp.c
@@ -0,0 +1,22 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_getpgrp
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	printf("getpgrp() = %ld\n", syscall(__NR_getpgrp));
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_getpgrp")
+
+#endif
