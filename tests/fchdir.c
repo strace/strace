e0057a578f25c8a5f611d0fb43d6b67fb8d80bf1
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-21 15:53:50 +0800

    tests: add fchdir.test
    * tests/fchdir.c: New file.
    * tests/fchdir.test: New test.
    * tests/.gitignore: Add fchdir.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add fchdir.test.

---
+++ tests/fchdir.c
@@ -0,0 +1,27 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_fchdir
+
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	const long int fd = (long int) 0xdeadbeefffffffff;
+
+	long rc = syscall(__NR_fchdir, fd);
+	printf("fchdir(%d) = %ld %s (%m)\n",
+	       (int) fd, rc, errno == ENOSYS ? "ENOSYS" : "EBADF");
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_fchdir")
+
+#endif
