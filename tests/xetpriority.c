e5297aeb131a408637d6dbc6b5308d390d7588a6
Dmitry V. Levin <ldv@altlinux.org>
2016-04-21 21:34:19 +0000

    tests/xetpriority.c: print syscall() result using %ld format

--- tests/xetpriority.c
+++ tests/xetpriority.c
@@ -5,7 +5,6 @@
 
 # include <errno.h>
 # include <stdio.h>
-# include <sys/time.h>
 # include <sys/resource.h>
 # include <unistd.h>
 
@@ -13,19 +12,18 @@ int
 main(void)
 {
 	const int pid = getpid();
-	int rc = syscall(__NR_getpriority, PRIO_PROCESS,
-	         (unsigned long) 0xffffffff00000000 | pid);
-	printf("getpriority(PRIO_PROCESS, %d) = %d\n",
-	       pid, rc);
+	long rc = syscall(__NR_getpriority, PRIO_PROCESS,
+			  (unsigned long) 0xffffffff00000000 | pid);
+	printf("getpriority(PRIO_PROCESS, %d) = %ld\n", pid, rc);
 
-	if ((syscall(__NR_setpriority, PRIO_PROCESS,
-	    (unsigned long) 0xffffffff00000000 | pid,
-	    (unsigned long) 0xffffffff00000000)) == 0) {
+	rc = syscall(__NR_setpriority, PRIO_PROCESS,
+		     (unsigned long) 0xffffffff00000000 | pid,
+		     (unsigned long) 0xffffffff00000000);
+	if (rc)
+		printf("setpriority(PRIO_PROCESS, %d, 0) = %ld %s (%m)\n",
+		       pid, rc, errno2name());
+	else
 		printf("setpriority(PRIO_PROCESS, %d, 0) = 0\n", pid);
-	} else {
-		printf("setpriority(PRIO_PROCESS, %d, 0) = -1 %s (%m)\n",
-		       pid, errno2name());
-	}
 
 	puts("+++ exited with 0 +++");
 	return 0;
