dd02f09f4b4e4609b9b8dfe8af35b8fcc22cda4d
Dmitry V. Levin <ldv@altlinux.org>
2016-04-21 20:16:32 +0000

    tests: cleanup acct.test
    * tests/acct.c (main): Use errno2name, stop using assert,
    change sample file name.
    * tests/acct.test: Update.

--- tests/acct.c
+++ tests/acct.c
@@ -8,25 +8,14 @@
 # include <stdio.h>
 # include <unistd.h>
 
-# define TMP_FILE "acct_tmpfile"
-
 int
 main(void)
 {
-	assert(syscall(__NR_acct, TMP_FILE) == -1);
-	const char *errno_text;
-	switch(errno) {
-		case ENOSYS:
-			errno_text = "ENOSYS";
-			break;
-		case EPERM:
-			errno_text = "EPERM";
-			break;
-		default:
-			errno_text = "ENOENT";
-	}
-	printf("acct(\"%s\") = -1 %s (%m)\n",
-	       TMP_FILE, errno_text);
+	const char sample[] = "acct_sample";
+
+	long rc = syscall(__NR_acct, sample);
+	printf("acct(\"%s\") = %ld %s (%m)\n",
+	       sample, rc, errno2name());
 
 	puts("+++ exited with 0 +++");
 	return 0;
