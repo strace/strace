f75e034c848aac387c8cd16c9df9d5fd37d52665
Dmitry V. Levin <ldv@altlinux.org>
2016-04-21 21:24:43 +0000

    tests/swap.c: cleanup
    * tests/swap.c (error_msg): Remove.
    (main): Use errno2name.

--- tests/swap.c
+++ tests/swap.c
@@ -8,48 +8,38 @@
 # include <sys/swap.h>
 # include <unistd.h>
 
-static const char *
-error_msg(int error_num)
-{
-	switch (error_num) {
-		case ENOSYS: return "ENOSYS";
-		case EPERM: return "EPERM";
-		case EINVAL: return "EINVAL";
-		default: return "ENOENT";
-	}
-}
-
 int
 main(void)
 {
 	static const char sample[] = "swap.sample";
+	long rc;
 
-	int rc = syscall(__NR_swapon, sample, 0);
-	printf("swapon(\"%s\", 0) = %d %s (%m)\n",
-	       sample, rc, error_msg(errno));
+	rc = syscall(__NR_swapon, sample, 0);
+	printf("swapon(\"%s\", %s) = %ld %s (%m)\n",
+	       sample, "0", rc, errno2name());
 
 	rc = syscall(__NR_swapon, sample, 42);
-	printf("swapon(\"%s\", %s) = %d %s (%m)\n",
-	       sample, "42", rc, error_msg(errno));
+	printf("swapon(\"%s\", %s) = %ld %s (%m)\n",
+	       sample, "42", rc, errno2name());
 
 	rc = syscall(__NR_swapon, sample, SWAP_FLAG_PREFER);
-	printf("swapon(\"%s\", %s) = %d %s (%m)\n",
-	       sample, "SWAP_FLAG_PREFER", rc, error_msg(errno));
+	printf("swapon(\"%s\", %s) = %ld %s (%m)\n",
+	       sample, "SWAP_FLAG_PREFER", rc, errno2name());
 
 	rc = syscall(__NR_swapon, sample, SWAP_FLAG_PREFER | 42);
-	printf("swapon(\"%s\", %s) = %d %s (%m)\n",
-	       sample, "SWAP_FLAG_PREFER|42", rc, error_msg(errno));
+	printf("swapon(\"%s\", %s) = %ld %s (%m)\n",
+	       sample, "SWAP_FLAG_PREFER|42", rc, errno2name());
 
 	rc = syscall(__NR_swapon, sample, -1L);
-	printf("swapon(\"%s\", %s) = %d %s (%m)\n",
+	printf("swapon(\"%s\", %s) = %ld %s (%m)\n",
 	       sample,
 	       "SWAP_FLAG_PREFER|SWAP_FLAG_DISCARD|SWAP_FLAG_DISCARD_ONCE"
 	       "|SWAP_FLAG_DISCARD_PAGES|0xfff80000|32767",
-	       rc, error_msg(errno));
+	       rc, errno2name());
 
 	rc = syscall(__NR_swapoff, sample);
-	printf("swapoff(\"%s\") = %d %s (%m)\n",
-	       sample, rc, error_msg(errno));
+	printf("swapoff(\"%s\") = %ld %s (%m)\n",
+	       sample, rc, errno2name());
 
 	puts("+++ exited with 0 +++");
 	return 0;
