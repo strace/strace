02269e5da0fe92e472fbb81b5cbbea1cd29215dc
Dmitry V. Levin <ldv@altlinux.org>
2016-04-21 20:41:38 +0000

    tests/fdatasync.c: print syscall() result using %ld format

--- tests/fdatasync.c
+++ tests/fdatasync.c
@@ -11,10 +11,9 @@ int
 main(void)
 {
 	const long int fd = (long int) 0xdeadbeefffffffff;
-	int rc = syscall(__NR_fdatasync, fd);
-	printf("fdatasync(%d) = %d %s (%m)\n",
-	       (int) fd, rc,
-	       errno2name());
+
+	long rc = syscall(__NR_fdatasync, fd);
+	printf("fdatasync(%d) = %ld %s (%m)\n", (int) fd, rc, errno2name());
 
 	puts("+++ exited with 0 +++");
 	return 0;
