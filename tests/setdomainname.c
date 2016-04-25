7f051136240d95329d949d2a08529cb6bd86cb24
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-25 16:18:21 +0800

    tests: add setdomainname.test
    * tests/setdomainname.c: New file.
    * tests/setdomainname.test: New test.
    * tests/.gitignore: Add setdomainname.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setdomainname.test.

---
+++ tests/setdomainname.c
@@ -0,0 +1,24 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_setdomainname
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	long rc = syscall(__NR_setdomainname, 0, 63);
+	printf("setdomainname(NULL, 63) = %ld %s (%m)\n",
+	       rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setdomainname")
+
+#endif
