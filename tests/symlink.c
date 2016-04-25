13c1d0899d05e3f3349629bf40c671b89044ecdd
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-25 16:18:23 +0800

    tests: add symlink.test
    * tests/symlink.c: New file.
    * tests/symlink.test: New test.
    * tests/.gitignore: Add symlink.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add symlink.test.

---
+++ tests/symlink.c
@@ -0,0 +1,27 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_symlink
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample_1[] = "symlink_new";
+	static const char sample_2[] = "symlink";
+
+	long rc = syscall(__NR_symlink, sample_1, sample_2);
+	printf("symlink(\"%s\", \"%s\") = %ld %s (%m)\n",
+	       sample_1, sample_2, rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_symlink")
+
+#endif
