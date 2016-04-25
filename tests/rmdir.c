506a42f8c3058fc1c06ebfdbaf686474515fa297
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-25 16:18:22 +0800

    tests: add rmdir.test
    * tests/rmdir.c: New file.
    * tests/rmdir.test: New test.
    * tests/.gitignore: Add rmdir.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add rmdir.test.

---
+++ tests/rmdir.c
@@ -0,0 +1,25 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_rmdir
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample[] = "rmdir_sample";
+	long rc = syscall(__NR_rmdir, sample);
+	printf("rmdir(\"%s\") = %ld %s (%m)\n",
+	       sample, rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_rmdir")
+
+#endif
