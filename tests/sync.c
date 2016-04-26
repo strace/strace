86ff93e527d4392583fd17d3c37cab8e1f431f97
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-26 16:03:10 +0800

    tests: add sync.test
    * tests/sync.c: New file.
    * tests/sync.test: New test.
    * tests/.gitignore: Add sync.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add sync.test.

---
+++ tests/sync.c
@@ -0,0 +1,22 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_sync
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	printf("sync() = %ld\n", syscall(__NR_sync));
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_sync")
+
+#endif
