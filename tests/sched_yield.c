68b1d24e59c80f37e5d8009852004b3c2239f539
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-26 16:03:11 +0800

    tests: add sched_yield.test
    * tests/sched_yield.c: New file.
    * tests/sched_yield.test: New test.
    * tests/.gitignore: Add sched_yield.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add sched_yield.test.

---
+++ tests/sched_yield.c
@@ -0,0 +1,22 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_sched_yield
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	printf("sched_yield() = %ld\n", syscall(__NR_sched_yield));
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_sched_yield")
+
+#endif
