363b71d2815ad89672cd2b4acdeb53c5a4204c32
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-04-26 16:03:08 +0800

    tests: add unlink.test
    * tests/unlink.c: New file.
    * tests/unlink.test: New test.
    * tests/.gitignore: Add unlink.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add unlink.test.

---
+++ tests/unlink.c
@@ -0,0 +1,26 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_unlink
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample[] = "unlink_sample";
+
+	long rc = syscall(__NR_unlink, sample);
+	printf("unlink(\"%s\") = %ld %s (%m)\n",
+	       sample, rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_unlink")
+
+#endif
