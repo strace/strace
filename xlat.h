0f4982691eaf60f6b23e9f7d02c63ee3b9cb0460
Dmitry V. Levin <ldv@altlinux.org>
2016-04-29 00:03:48 +0000

    Introduce XLAT_TYPE and XLAT_TYPE_PAIR macros
    * xlat.h (XLAT_TYPE): New macro, similar to XLAT but casts
    to the specified type instead of unsigned int.
    (XLAT_TYPE_PAIR): New macro, similar to XLAT_PAIR but casts
    to the specified type instead of unsigned int.

--- xlat.h
+++ xlat.h
@@ -7,6 +7,8 @@ struct xlat {
 
 # define XLAT(val)			{ (unsigned)(val), #val }
 # define XLAT_PAIR(val, str)		{ (unsigned)(val), str  }
+# define XLAT_TYPE(type, val)		{     (type)(val), #val }
+# define XLAT_TYPE_PAIR(val, str)	{     (type)(val), str  }
 # define XLAT_END			{		0, 0    }
 
 #endif
