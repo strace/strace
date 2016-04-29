070fec224827e64970deb42d39852293379c7c86
Dmitry V. Levin <ldv@altlinux.org>
2016-04-29 20:04:38 +0000

    Fix typo in XLAT_TYPE_PAIR
    * xlat.h (XLAT_TYPE_PAIR): Take "type" parameter.
    
    Reported-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- xlat.h
+++ xlat.h
@@ -10,7 +10,7 @@ struct xlat {
 # define XLAT(val)			{ (unsigned)(val), #val }
 # define XLAT_PAIR(val, str)		{ (unsigned)(val), str  }
 # define XLAT_TYPE(type, val)		{     (type)(val), #val }
-# define XLAT_TYPE_PAIR(val, str)	{     (type)(val), str  }
+# define XLAT_TYPE_PAIR(type, val, str)	{     (type)(val), str  }
 # define XLAT_END			{		0, 0    }
 
 #endif
