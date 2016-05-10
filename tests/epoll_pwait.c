d12c44bcac445c2c8899722178b8f7e0dd010fda
Dmitry V. Levin <ldv@altlinux.org>
2016-05-10 01:10:51 +0000

    tests/epoll_pwait.c: fix for x32
    * tests/epoll_pwait.c (main): Explicitly cast last syscall argument
    to kernel_ulong_t.

--- tests/epoll_pwait.c
+++ tests/epoll_pwait.c
@@ -8,13 +8,16 @@
 # include <sys/epoll.h>
 # include <unistd.h>
 
+# include "kernel_types.h"
+
 int
 main(void)
 {
 	sigset_t set[2];
 	struct epoll_event *const ev = tail_alloc(sizeof(*ev));
 
-	long rc = syscall(__NR_epoll_pwait, -1, ev, 1, -2, set, sizeof(set));
+	long rc = syscall(__NR_epoll_pwait, -1, ev, 1, -2,
+			  set, (kernel_ulong_t) sizeof(set));
 	printf("epoll_pwait(-1, %p, 1, -2, %p, %u) = %ld %s (%m)\n",
 	       ev, set, (unsigned) sizeof(set), rc, errno2name());
 
