0684c35c258033e6c374ff6611771188a1cd9271
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-05-11 15:07:28 +0800

    tests: add link.test
    * tests/link.c: New file.
    * tests/link.test: New test.
    * tests/.gitignore: Add link.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add link.test.

---
+++ tests/link.c
@@ -0,0 +1,27 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_link
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	static const char sample_1[] = "link_sample_old";
+	static const char sample_2[] = "link_sample_new";
+
+	long rc = syscall(__NR_link, sample_1, sample_2);
+	printf("link(\"%s\", \"%s\") = %ld %s (%m)\n",
+	       sample_1, sample_2, rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_link")
+
+#endif
