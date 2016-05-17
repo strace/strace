854a25930c41b726934175a18fd533c10bb4f013
Fei Jie <feij.fnst@cn.fujitsu.com>
2016-05-17 17:59:45 +0800

    tests: add brk.test
    * tests/brk.c: New file.
    * tests/brk.test: New test.
    * tests/.gitignore: Add brk.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add brk.test.

---
+++ tests/brk.c
@@ -0,0 +1,22 @@
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_brk
+
+# include <stdio.h>
+# include <unistd.h>
+
+int
+main(void)
+{
+	long rc = syscall(__NR_brk, NULL);
+	printf("brk\\(NULL\\) = %#lx\n", rc);
+
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_brk")
+
+#endif
