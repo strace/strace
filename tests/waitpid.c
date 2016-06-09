e9c1ee9c63cc9df543b2f936fc69c0b05a5f89d6
Dmitry V. Levin <ldv@altlinux.org>
2016-06-09 01:29:45 +0000

    tests: check decoding of waitpid syscall
    * tests/waitpid.c: New file.
    * tests/waitpid.test: New test.
    * tests/.gitignore: Add waitpid.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add waitpid.test.

---
+++ tests/waitpid.c
@@ -0,0 +1,56 @@
+/*
+ * Check decoding of waitpid syscall.
+ *
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+#include <sys/syscall.h>
+
+#ifdef __NR_waitpid
+
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+
+int
+main(void)
+{
+	unsigned long pid =
+		(unsigned long) 0xdefaced00000000 | (unsigned) getpid();
+	long rc = syscall(__NR_waitpid, pid, 0L, (unsigned long) WNOHANG);
+	printf("waitpid(%d, NULL, WNOHANG) = %ld %s (%m)\n",
+	       (int) pid, rc, errno2name());
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_waitpid")
+
+#endif
