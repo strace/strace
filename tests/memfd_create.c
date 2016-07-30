c4fdce5ceaabc559d6a21b42a05ac3695e505853
Dmitry V. Levin <ldv@altlinux.org>
2016-07-30 22:52:27 +0000

    memfd_create.test: rewrite from match_grep to match_diff
    * tests/memfd_create.c (main): Print expected output.
    * tests/memfd_create.test: Use run_strace_match_diff.
    * tests/memfd_create.expected: Remove.
    * tests/Makefile.am (EXTRA_DIST): Remove it.

--- tests/memfd_create.c
+++ tests/memfd_create.c
@@ -1,13 +1,21 @@
 #include "tests.h"
-#include <unistd.h>
 #include <sys/syscall.h>
 
 #ifdef __NR_memfd_create
 
+# include <stdio.h>
+# include <unistd.h>
+
 int
 main(void)
 {
-	syscall(__NR_memfd_create, "strace", 7);
+	static const char text[] = "strace";
+	int rc = syscall(__NR_memfd_create, text, 7);
+
+	printf("memfd_create(\"%s\", %s) = %d %s (%m)\n",
+	       text, "MFD_CLOEXEC|MFD_ALLOW_SEALING|0x4", rc, errno2name());
+
+	puts("+++ exited with 0 +++");
 	return 0;
 }
 
