38d854e425b71cc810adea817272fc7b988ceb97
Dmitry V. Levin <ldv@altlinux.org>
2016-08-09 14:35:06 +0000

    ia64: replace SYS_clone2 with __NR_clone2
    Migrate to __NR_* the last user of SYS_* macros provided
    by <sys/syscall.h>.
    
    * clone.c [IA64] (ARG_STACKSIZE, ARG_PTID, ARG_CTID, ARG_TLS): Replace
    SYS_clone2 with __NR_clone2.

--- clone.c
+++ clone.c
@@ -42,10 +42,10 @@
 #if defined IA64
 # define ARG_FLAGS	0
 # define ARG_STACK	1
-# define ARG_STACKSIZE	(tcp->scno == SYS_clone2 ? 2 : -1)
-# define ARG_PTID	(tcp->scno == SYS_clone2 ? 3 : 2)
-# define ARG_CTID	(tcp->scno == SYS_clone2 ? 4 : 3)
-# define ARG_TLS	(tcp->scno == SYS_clone2 ? 5 : 4)
+# define ARG_STACKSIZE	(tcp->scno == __NR_clone2 ? 2 : -1)
+# define ARG_PTID	(tcp->scno == __NR_clone2 ? 3 : 2)
+# define ARG_CTID	(tcp->scno == __NR_clone2 ? 4 : 3)
+# define ARG_TLS	(tcp->scno == __NR_clone2 ? 5 : 4)
 #elif defined S390 || defined S390X || defined CRISV10 || defined CRISV32
 # define ARG_STACK	0
 # define ARG_FLAGS	1
