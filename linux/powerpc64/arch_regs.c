aaa7a77b5f79ec3b99806f5de9abdb5d70516b1d
Dmitry V. Levin <ldv@altlinux.org>
2016-08-14 00:03:03 +0000

    powerpc64, sparc64: fix redefinitions of ARCH_PC_REG
    This fixes compilation warning that ARCH_PC_REG is redefined.
    
    * linux/powerpc64/arch_regs.c (ARCH_PC_REG): Undefine before
    the new definition.
    * linux/sparc64/arch_regs.c (ARCH_PC_REG): Likewise.

--- linux/powerpc64/arch_regs.c
+++ linux/powerpc64/arch_regs.c
@@ -1,2 +1,3 @@
 #include "powerpc/arch_regs.c"
+#undef ARCH_PC_REG
 #define ARCH_PC_REG ppc_regs.nip
--- linux/sparc64/arch_regs.c
+++ linux/sparc64/arch_regs.c
@@ -1,2 +1,3 @@
 #include "sparc/arch_regs.c"
+#undef ARCH_PC_REG
 #define ARCH_PC_REG sparc_regs.tpc
