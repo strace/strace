2fdb64efd66e60a56ce547254454797456ce5e35
Eugene Syromyatnikov <evgsyr@gmail.com>
2016-09-05 04:31:48 +0300

    tests: use sprintrc for return code output in sched_rr_get_interval test
    * tests/sched_rr_get_interval.c (main): Use sprintrc for return code
    output.

--- tests/sched_rr_get_interval.c
+++ tests/sched_rr_get_interval.c
@@ -12,8 +12,7 @@ main(void)
 {
 	struct timespec *const tp = tail_alloc(sizeof(struct timespec));
 	long rc = syscall(__NR_sched_rr_get_interval, -1, tp);
-	printf("sched_rr_get_interval(-1, %p) = %ld %s (%m)\n",
-	       tp, rc, errno2name());
+	printf("sched_rr_get_interval(-1, %p) = %s\n", sprintrc(rc));
 
 	puts("+++ exited with 0 +++");
 	return 0;
