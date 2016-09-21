1929c2216ab065082903c299147fb8290873da49
Eugene Syromyatnikov <evgsyr@gmail.com>
2016-09-22 00:21:42 +0300

    x32: use proper decoder for fadvise64 syscall
    fadvise_64_64 decoder used before this change prints different sign
    of the "len" argument (loff_t vs size_t).
    
    * linux/x32/syscallent.h (fadvise64): Replace SEN(fadvise64_64)
    with SEN(fadvise64).

--- linux/x32/syscallent.h
+++ linux/x32/syscallent.h
@@ -219,7 +219,7 @@
 [218] = { 1,	0,		SEN(set_tid_address),		"set_tid_address"	},
 [219] = { 0,	0,		SEN(restart_syscall),		"restart_syscall"	},
 [220] = { 4,	TI,		SEN(semtimedop),		"semtimedop"		},
-[221] = { 4,	TD,		SEN(fadvise64_64),		"fadvise64"		},
+[221] = { 4,	TD,		SEN(fadvise64),			"fadvise64"		},
 [222] = { 3,	0,		SEN(printargs),			"64:timer_create"	},
 [223] = { 4,	0,		SEN(timer_settime),		"timer_settime"		},
 [224] = { 2,	0,		SEN(timer_gettime),		"timer_gettime"		},
