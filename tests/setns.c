3fa672fa7acb57ed762b38da2a300b9879c83836
Eugene Syromyatnikov <evgsyr@gmail.com>
2016-10-18 22:32:07 +0300

    tests: check decoding of setns syscall
    * tests/setns.c: New file.
    * tests/setns.test: New test.
    * tests/.gitignore: Add setns.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add setns.test.

---
+++ tests/setns.c
@@ -0,0 +1,75 @@
+/*
+ * Check decoding of setns syscall.
+ *
+ * Copyright (c) 2016 Eugene Syromyatnikov <evgsyr@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+
+#include <asm/unistd.h>
+
+#ifdef __NR_setns
+
+# include <stdio.h>
+# include <unistd.h>
+
+# include "kernel_types.h"
+
+int
+main(void)
+{
+	static const kernel_ulong_t bogus_fd =
+		(kernel_ulong_t) 0xfeedfacedeadc0deULL;
+
+	static struct {
+		kernel_ulong_t val;
+		const char *str;
+	} nstypes[] = {
+		{ (kernel_ulong_t) 0xdefaced100000000ULL, "0" },
+		{ (kernel_ulong_t) 0xbadc0dedfeedfaceULL,
+			"0xfeedface /* CLONE_NEW??? */" },
+		{ (kernel_ulong_t) 0xca75f15702000000ULL, "CLONE_NEWCGROUP" },
+	};
+
+	long rc;
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(nstypes); i++) {
+		rc = syscall(__NR_setns, bogus_fd, nstypes[i].val);
+		printf("setns(%d, %s) = %s\n",
+			(int) bogus_fd, nstypes[i].str, sprintrc(rc));
+	}
+
+	puts("+++ exited with 0 +++");
+
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_setns");
+
+#endif
