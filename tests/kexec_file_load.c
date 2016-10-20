88cac72e9540832990f3d78fac0bb4a4e6b35d71
Eugene Syromyatnikov <evgsyr@gmail.com>
2016-10-20 06:46:02 +0300

    tests: check decoding of kexec_file_load and kexec_load syscalls
    * tests/kexec_file_load.c: New file.
    * tests/kexec_load.c: Likewise.
    * tests/kexec_file_load.test: New test.
    * tests/kexec_load.test: Likewise.
    * tests/.gitignore: Add kexec_file_load and kexec_load.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add kexec_file_load.test and kexec_load.test.

---
+++ tests/kexec_file_load.c
@@ -0,0 +1,129 @@
+/*
+ * Check decoding of kexec_file_load syscall.
+ *
+ * Copyright (c) 2016 Eugene Syromyatnikov <evgsyr@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+
+#include <asm/unistd.h>
+
+#ifdef __NR_kexec_file_load
+
+# include <inttypes.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# include "kernel_types.h"
+
+struct strval {
+	kernel_ulong_t val;
+	const char *str64;
+	const char *str32;
+	const char *str;
+};
+
+#define CMDLINE_STR "deadcodebaddatadefaced";
+
+int
+main(void)
+{
+	static const kernel_ulong_t bogus_kernel_fd =
+		(kernel_ulong_t) 0xdeadca57badda7a1ULL;
+	static const kernel_ulong_t bogus_initrd_fd =
+		(kernel_ulong_t) 0xdec0ded1defaced2ULL;
+	static const char cmdline_str[] = CMDLINE_STR;
+	static const char cmdline_short_str[] = "abcdef";
+
+	static const kernel_ulong_t cmdline_lens[] = {
+		0,
+		(kernel_ulong_t) 0xcaffeeeddeadbeefULL,
+		sizeof(cmdline_str),
+		sizeof(cmdline_str) - 1,
+		sizeof(cmdline_short_str),
+		sizeof(cmdline_short_str) - 1,
+		sizeof(cmdline_short_str) + 1,
+	};
+	static const struct strval flags[] = {
+		{ (kernel_ulong_t) 0xbadc0dedda7a1058ULL,
+			"0xbadc0ded", "0x",
+			"da7a1058 /* KEXEC_FILE_??? */", },
+		{ 0, "", "", "0" },
+		{ 0xdeadbeef, "", "", "KEXEC_FILE_UNLOAD|KEXEC_FILE_ON_CRASH|"
+			"KEXEC_FILE_NO_INITRAMFS|0xdeadbee8" },
+	};
+
+
+	long rc;
+	char *cmdline = tail_memdup(cmdline_str, sizeof(cmdline_str));
+	char *cmdline_short =
+		tail_memdup(cmdline_short_str, sizeof(cmdline_short_str));
+	char cmdline_ptr[sizeof("0x") + sizeof(void *) * 2];
+	char cmdline_short_ptr[sizeof("0x") + sizeof(void *) * 2];
+	unsigned int i;
+	unsigned int j;
+
+	struct strval cmdlines[] = {
+		{ (uintptr_t) NULL, "", "", "NULL" },
+		{ (uintptr_t) (cmdline + sizeof(cmdline_str)), "", "",
+			cmdline_ptr },
+		{ (uintptr_t) cmdline, "", "", "\"deadcodeb\"..." },
+		{ (uintptr_t) cmdline, "", "", "\"deadcodeb\"..." },
+		{ (uintptr_t) cmdline_short, "", "", "\"abcdef\\0\"" },
+		{ (uintptr_t) cmdline_short, "", "", "\"abcdef\"" },
+		{ (uintptr_t) cmdline_short, "", "", cmdline_short_ptr },
+	};
+
+
+	snprintf(cmdline_ptr, sizeof(cmdline_ptr), "%p",
+		cmdline + sizeof(cmdline_str));
+	snprintf(cmdline_short_ptr, sizeof(cmdline_short_ptr), "%p",
+		cmdline_short);
+
+	for (i = 0; i < ARRAY_SIZE(flags); i++) {
+		for (j = 0; j < ARRAY_SIZE(cmdlines); j++) {
+			rc = syscall(__NR_kexec_file_load, bogus_kernel_fd,
+				     bogus_initrd_fd, cmdline_lens[j],
+				     cmdlines[j].val, flags[i].val);
+			printf("kexec_file_load(%d, %d, %llu, %s, %s%s) = %s\n",
+			       (int) bogus_kernel_fd, (int) bogus_initrd_fd,
+			       (unsigned long long) cmdline_lens[j],
+			       cmdlines[j].str,
+			       sizeof(kernel_ulong_t) == 8 ? flags[i].str64 :
+			       flags[i].str32, flags[i].str, sprintrc(rc));
+		}
+	}
+
+	puts("+++ exited with 0 +++");
+
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_kexec_file_load");
+
+#endif
---
+++ tests/kexec_load.c
@@ -0,0 +1,172 @@
+/*
+ * Check decoding of kexec_load syscall.
+ *
+ * Copyright (c) 2016 Eugene Syromyatnikov <evgsyr@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+
+#include <asm/unistd.h>
+
+#ifdef __NR_kexec_load
+
+# include <stdio.h>
+# include <unistd.h>
+
+# include "kernel_types.h"
+
+struct strval {
+	kernel_ulong_t val;
+	const char *str64;
+	const char *str32;
+	const char *str;
+};
+
+struct segm {
+	void *buf;
+	size_t bufsz;
+	void *mem;
+	size_t memsz;
+};
+
+int
+main(void)
+{
+	enum {
+		NUM_SEGMS = 17,
+		NUM_SEGMS_UNCUT = 5,
+		NUM_SEGMS_UNCUT_MAX = 9,
+		NUM_SEGMS_CUT = 12,
+		SEGMS_ARRAY_SIZE = sizeof(struct segm) * NUM_SEGMS,
+	};
+
+	static const kernel_ulong_t bogus_zero =
+		sizeof(long) < sizeof(kernel_long_t) ?
+			(kernel_ulong_t) 0xffffffff00000000ULL : 0;
+	static const kernel_ulong_t bogus_entry =
+		(kernel_ulong_t) 0xdeadca57badda7a1ULL;
+	static const kernel_ulong_t bogus_nsegs =
+		(kernel_ulong_t) 0xdec0ded1defaced2ULL;
+
+	static const struct strval flags[] = {
+		{ (kernel_ulong_t) 0xbadc0dedda7a1054ULL,
+			"0xda7a0000 /* KEXEC_ARCH_??? */|0xbadc0ded0000",
+			"0xda7a0000 /* KEXEC_ARCH_??? */|0x",
+			"1054 /* KEXEC_??? */" },
+		{ 0, "", "", "KEXEC_ARCH_DEFAULT" },
+		{ 0x2a0003, "", "",
+			"KEXEC_ARCH_SH|KEXEC_ON_CRASH|KEXEC_PRESERVE_CONTEXT" },
+		{ 0xdead0000, "", "", "0xdead0000 /* KEXEC_ARCH_??? */" },
+	};
+
+	const char *errstr;
+	long rc;
+	struct segm *segms = tail_alloc(SEGMS_ARRAY_SIZE);
+	unsigned int i;
+
+	fill_memory((char *) segms, SEGMS_ARRAY_SIZE);
+	segms[0].buf = segms[0].mem = NULL;
+
+	rc = syscall(__NR_kexec_load, bogus_zero, bogus_zero, bogus_zero,
+		flags[0].val);
+	printf("kexec_load(NULL, 0, NULL, %s%s) = %s\n",
+	       sizeof(long) == 8 ? flags[0].str64 : flags[0].str32,
+	       flags[0].str, sprintrc(rc));
+
+	rc = syscall(__NR_kexec_load, bogus_entry, bogus_nsegs,
+		     segms + SEGMS_ARRAY_SIZE, flags[1].val);
+	printf("kexec_load(%#lx, %lu, %p, %s) = %s\n",
+	       (unsigned long) bogus_entry, (unsigned long) bogus_nsegs,
+	       segms + SEGMS_ARRAY_SIZE, flags[1].str, sprintrc(rc));
+
+	rc = syscall(__NR_kexec_load, bogus_entry, NUM_SEGMS,
+		     segms, flags[2].val);
+	printf("kexec_load(%#lx, %lu, %p, %s) = %s\n",
+	       (unsigned long) bogus_entry, (unsigned long) NUM_SEGMS,
+	       segms, flags[2].str, sprintrc(rc));
+
+	rc = syscall(__NR_kexec_load, bogus_entry, NUM_SEGMS_CUT,
+		     segms, flags[3].val);
+	errstr = sprintrc(rc);
+	printf("kexec_load(%#lx, %lu, [{buf=NULL, bufsz=%zu, mem=NULL, "
+	       "memsz=%zu}, ",
+	       (unsigned long) bogus_entry, (unsigned long) NUM_SEGMS_CUT,
+	       segms[0].bufsz, segms[0].memsz);
+	for (i = 1; i < NUM_SEGMS_UNCUT_MAX; i++)
+		printf("{buf=%p, bufsz=%zu, mem=%p, memsz=%zu}, ",
+		       segms[i].buf, segms[i].bufsz,
+		       segms[i].mem, segms[i].memsz);
+	printf("...], %s) = %s\n", flags[3].str, errstr);
+
+	rc = syscall(__NR_kexec_load, bogus_entry, NUM_SEGMS_CUT,
+		     segms + (NUM_SEGMS - NUM_SEGMS_UNCUT_MAX),
+		     flags[0].val);
+	errstr = sprintrc(rc);
+	printf("kexec_load(%#lx, %lu, [",
+	       (unsigned long) bogus_entry, (unsigned long) NUM_SEGMS_CUT);
+	for (i = NUM_SEGMS - NUM_SEGMS_UNCUT_MAX; i < NUM_SEGMS; i++)
+		printf("{buf=%p, bufsz=%zu, mem=%p, memsz=%zu}, ",
+		       segms[i].buf, segms[i].bufsz,
+		       segms[i].mem, segms[i].memsz);
+	printf("%p], %s%s) = %s\n",
+	       segms + NUM_SEGMS,
+	       sizeof(long) == 8 ? flags[0].str64 : flags[0].str32,
+	       flags[0].str, errstr);
+
+	rc = syscall(__NR_kexec_load, bogus_entry, NUM_SEGMS_UNCUT,
+		     segms + (NUM_SEGMS - NUM_SEGMS_UNCUT),
+		     flags[1].val);
+	errstr = sprintrc(rc);
+	printf("kexec_load(%#lx, %lu, [",
+	       (unsigned long) bogus_entry, (unsigned long) NUM_SEGMS_UNCUT);
+	for (i = NUM_SEGMS - NUM_SEGMS_UNCUT; i < NUM_SEGMS; i++)
+		printf("{buf=%p, bufsz=%zu, mem=%p, memsz=%zu}%s",
+		       segms[i].buf, segms[i].bufsz,
+		       segms[i].mem, segms[i].memsz,
+		       (i == NUM_SEGMS - 1) ? "" : ", ");
+	printf("], %s) = %s\n", flags[1].str, errstr);
+
+	rc = syscall(__NR_kexec_load, bogus_entry, NUM_SEGMS_CUT,
+		     segms + 1, flags[2].val);
+	errstr = sprintrc(rc);
+	printf("kexec_load(%#lx, %lu, [",
+	       (unsigned long) bogus_entry, (unsigned long) NUM_SEGMS_CUT);
+	for (i = 1; i < NUM_SEGMS_UNCUT_MAX + 1; i++)
+		printf("{buf=%p, bufsz=%zu, mem=%p, memsz=%zu}, ",
+		       segms[i].buf, segms[i].bufsz,
+		       segms[i].mem, segms[i].memsz);
+	printf("...], %s) = %s\n", flags[2].str, errstr);
+
+	puts("+++ exited with 0 +++");
+
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_kexec_load");
+
+#endif
