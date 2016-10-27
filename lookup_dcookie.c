2119f8d6b0ef361f37f31e307267b7cda249e2c2
Eugene Syromyatnikov <evgsyr@gmail.com>
2016-10-27 10:24:21 +0300

    lookup_dcookie: print len parameter as kernel_ulong_t
    It is size_t and without compat on x32/n32.
    
    * lookup_dcookie.c (SYS_FUNC(lookup_dcookie)): Change conversion
    specifier from "%lu" to "%llu", use getarg_ull for len argument
    retrieval.

--- lookup_dcookie.c
+++ lookup_dcookie.c
@@ -43,7 +43,7 @@ SYS_FUNC(lookup_dcookie)
 		printstr(tcp, tcp->u_arg[argn], tcp->u_rval);
 
 	/* len */
-	tprintf(", %lu", tcp->u_arg[argn + 1]);
+	tprintf(", %llu", getarg_ull(tcp, argn + 1));
 
 	return 0;
 }
