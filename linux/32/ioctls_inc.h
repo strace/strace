3317da7a3ad06f00009dde63d557c256589c8c0f
Andreas Schwab <schwab@linux-m68k.org>
2016-11-12 15:58:26 +0100

    Add ioctl definitions for 16-bit alignment
    * Makefile.am (EXTRA_DIST): Add linux/32/ioctls_inc_align16.h.
    * linux/32/ioctls_inc_align16.h: New file.
    * linux/32/ioctls_inc.h [M68K]: Include it.

--- linux/32/ioctls_inc.h
+++ linux/32/ioctls_inc.h
@@ -1,4 +1,6 @@
-#if defined X86_64 || defined X32 \
+#if defined M68K
+# include "32/ioctls_inc_align16.h"
+#elif defined X86_64 || defined X32 \
  || SIZEOF_STRUCT_I64_I32 < SIZEOF_LONG_LONG * 2
 # include "32/ioctls_inc_align32.h"
 #else
