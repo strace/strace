5df31f90ec798f8d74eb79b18cb8c5f5c26523fb
Dmitry V. Levin <ldv@altlinux.org>
2016-11-14 01:07:59 +0000

    arc: wire up new syscalls
    * linux/arc/syscallent.h [247, 248]: New entries.

--- linux/arc/syscallent.h
+++ linux/arc/syscallent.h
@@ -1,6 +1,8 @@
 #define sys_ARCH_mmap sys_mmap_pgoff
 #include "32/syscallent.h"
 /* [244 ... 259] are arch specific */
-[244] = { 3,	0,	SEN(printargs),	"arc_cacheflush"},
+[244] = { 3,	0,	SEN(printargs),	"cacheflush"	},
 [245] = { 1,	0,	SEN(printargs),	"arc_settls"	},
 [246] = { 0,	0,	SEN(printargs),	"arc_gettls"	},
+[247] = { 3,	0,	SEN(sysfs),	"sysfs"		},
+[248] = { 3,	0,	SEN(printargs),	"arc_usr_cmpxchg"},
