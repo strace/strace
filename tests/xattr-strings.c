2aa03c2af62b54ca3c5ed458f215c06f3dac5275
Gabriel Laskar <gabriel@lse.epita.fr>
2016-12-22 16:10:52 +0100

    tests: test for correct ellipsis with -s in xattr
    * tests/xattr-strings.c: New file.
    * tests/xattr-strings.test: New test.
    * tests/.gitignore: Add xattr-string.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (DECODER_TESTS): Add xattr-string.test.
    
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

---
+++ tests/xattr-strings.c
@@ -0,0 +1,30 @@
+#include "tests.h"
+
+#ifdef HAVE_SYS_XATTR_H
+
+# include <stdio.h>
+# include <sys/xattr.h>
+
+int
+main(void)
+{
+	static const char name[] = "strace.test";
+	static const char c_value[] = "foobar";
+
+	const char *const z_value = tail_memdup(c_value, sizeof(c_value));
+	long rc;
+
+	rc = fsetxattr(-1, name, z_value, sizeof(c_value), XATTR_REPLACE);
+	printf("fsetxattr(-1, \"%.*s\"..., \"%.*s\"..., %u, XATTR_REPLACE) = %s\n",
+	       4, name, 4, c_value, (unsigned) sizeof(c_value), sprintrc(rc));
+
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("HAVE_SYS_XATTR_H")
+
+#endif
