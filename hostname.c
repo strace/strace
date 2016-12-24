9af28a4d1313ffb1f5ef57a352ba77070b0977e1
Dmitry V. Levin <ldv@altlinux.org>
2016-12-24 16:23:45 +0000

    Fix decoding of sethostname syscall
    The second argument of sethostname syscall is not an unsigned long but
    unsigned int.  The kernel does not look at the string argument when
    the length argument is too long.
    
    * hostname.c [HAVE_LINUX_UTSNAME_H]: Include <linux/utsname.h>.
    [!__NEW_UTS_LEN] (__NEW_UTS_LEN): Define.
    (SYS_FUNC(sethostname)): Treat the second argument as unsigned int.
    Print the first argument as a pointer when the second argument exceeds
    __NEW_UTS_LEN.
    * tests/sethostname.c [HAVE_LINUX_UTSNAME_H]: Include <linux/utsname.h>.
    [!__NEW_UTS_LEN] (__NEW_UTS_LEN): Define.
    (main): Use it.  Check that the second argument of sethostname
    is handled as unsigned int.  Check that the first argument is printed
    as a pointer when the second argument exceeds __NEW_UTS_LEN.

--- hostname.c
+++ hostname.c
@@ -1,9 +1,24 @@
 #include "defs.h"
 
+#ifdef HAVE_LINUX_UTSNAME_H
+# include <linux/utsname.h>
+#endif
+
+#ifndef __NEW_UTS_LEN
+# define __NEW_UTS_LEN 64
+#endif
+
 SYS_FUNC(sethostname)
 {
-	printstrn(tcp, tcp->u_arg[0], tcp->u_arg[1]);
-	tprintf(", %lu", tcp->u_arg[1]);
+	unsigned int len = tcp->u_arg[1];
+
+	if (len > __NEW_UTS_LEN) {
+		printaddr(tcp->u_arg[0]);
+	} else {
+		printstrn(tcp, tcp->u_arg[0], len);
+	}
+
+	tprintf(", %u", len);
 
 	return RVAL_DECODED;
 }
--- tests/sethostname.c
+++ tests/sethostname.c
@@ -1,3 +1,33 @@
+/*
+ * Check decoding of sethostname syscall.
+ *
+ * Copyright (c) 2016 Fei Jie <feij.fnst@cn.fujitsu.com>
+ * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "tests.h"
 #include <asm/unistd.h>
 
@@ -6,12 +36,37 @@
 # include <stdio.h>
 # include <unistd.h>
 
+#ifdef HAVE_LINUX_UTSNAME_H
+# include <linux/utsname.h>
+#endif
+
+#ifndef __NEW_UTS_LEN
+# define __NEW_UTS_LEN 64
+#endif
+
 int
 main(void)
 {
-	long rc = syscall(__NR_sethostname, 0, 63);
-	printf("sethostname(NULL, 63) = %ld %s (%m)\n",
-	       rc, errno2name());
+	kernel_ulong_t len;
+	long rc;
+
+	len = __NEW_UTS_LEN;
+	rc = syscall(__NR_sethostname, 0, len);
+	printf("sethostname(NULL, %u) = %s\n",
+	       (unsigned) len, sprintrc(rc));
+
+	if (F8ILL_KULONG_MASK) {
+		len |= F8ILL_KULONG_MASK;
+		rc = syscall(__NR_sethostname, 0, len);
+		printf("sethostname(NULL, %u) = %s\n",
+		       (unsigned) len, sprintrc(rc));
+	}
+
+	len = __NEW_UTS_LEN + 1;
+	void *const p = tail_alloc(len);
+	rc = syscall(__NR_sethostname, p, len);
+	printf("sethostname(%p, %u) = %s\n",
+	       p, (unsigned) len, sprintrc(rc));
 
 	puts("+++ exited with 0 +++");
 	return 0;
