strace/strace

commit 41d647ce674bfe601600988f6dda295804db9c2f

Implement success injection
This extends fault injection syntax with :retval= option.
When :retval=VALUE is specified, the syscall number is replaced by -1
and a bogus success VALUE is returned to the callee.

* defs.h (fault_opts): Remove err field, add rval field.
(MAX_ERRNO_VALUE, FAULT_OPTS_RVAL_DEFAULT, FAULT_OPTS_RVAL_DISABLE):
New macros.
* qualify.c (parse_fault_token): Handle retval= token.
(qualify_fault): Update fault_opts initialization after the move
from struct fault_opts.err to struct fault_opts.rval.
* syscall.c (arch_set_success): New prototype.
(inject_syscall_fault_entering): Check opts->rval instead of opts->err.
(update_syscall_fault_exiting): Implement retval injection.
* strace.1: Update the section on fault injection.
* NEWS: Mention retval= option.
* linux/aarch64/set_error.c (arch_set_success): New function.
* linux/alpha/set_error.c: Likewise.
* linux/arc/set_error.c: Likewise.
* linux/arm/set_error.c: Likewise.
* linux/avr32/set_error.c: Likewise.
* linux/bfin/set_error.c: Likewise.
* linux/crisv10/set_error.c: Likewise.
* linux/hppa/set_error.c: Likewise.
* linux/i386/set_error.c: Likewise.
* linux/ia64/set_error.c: Likewise.
* linux/m68k/set_error.c: Likewise.
* linux/metag/set_error.c: Likewise.
* linux/microblaze/set_error.c: Likewise.
* linux/mips/set_error.c: Likewise.
* linux/nios2/set_error.c: Likewise.
* linux/or1k/set_error.c: Likewise.
* linux/powerpc/set_error.c: Likewise.
* linux/riscv/set_error.c: Likewise.
* linux/s390/set_error.c: Likewise.
* linux/sh/set_error.c: Likewise.
* linux/sh64/set_error.c: Likewise.
* linux/sparc/set_error.c: Likewise.
* linux/sparc64/set_error.c: Likewise.
* linux/tile/set_error.c: Likewise.
* linux/x86_64/set_error.c: Likewise.
* linux/xtensa/set_error.c: Likewise.

Closes: https://github.com/strace/strace/issues/3

linux/or1k/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	or1k_regs.gpr[11] = -tcp->u_error;
 	return set_regs(tcp->pid);
 }
+
+static int
+arch_setsuccess(struct tcb *tcp)
+{
+	or1k_regs.gpr[11] = tcp->u_rval;
+	return set_regs(tcp->pid);
+}

linux/aarch64/set_error.c
@@ -1,5 +1,7 @@
 #define arch_set_error arm_set_error
+#define arch_set_success arm_set_success
 #include "arm/set_error.c"
+#undef arch_set_success
 #undef arch_set_error
 
 static int
@@ -9,5 +11,15 @@ arch_set_error(struct tcb *tcp)
 		return arm_set_error(tcp);
 
 	aarch64_regs.regs[0] = -tcp->u_error;
+	return set_regs(tcp->pid);
+}
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	if (aarch64_io.iov_len == sizeof(arm_regs))
+		return arm_set_success(tcp);
+
+	aarch64_regs.regs[0] = tcp->u_rval;
 	return set_regs(tcp->pid);
 }

linux/alpha/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	alpha_r0 = tcp->u_error;
 	return upoke(tcp->pid, REG_R0, alpha_r0);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	return upoke(tcp->pid, REG_A3, (alpha_a3 = 0))
+	       || upoke(tcp->pid, REG_R0, (alpha_r0 = tcp->u_rval));
+}

linux/arc/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	arc_regs.scratch.r0 = -tcp->u_error;
 	return set_regs(tcp->pid);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	arc_regs.scratch.r0 = tcp->u_rval;
+	return set_regs(tcp->pid);
+}

linux/arm/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	arm_regs.ARM_r0 = -tcp->u_error;
 	return set_regs(tcp->pid);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	arm_regs.ARM_r0 = tcp->u_rval;
+	return set_regs(tcp->pid);
+}

linux/avr32/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	avr32_regs.r12 = -tcp->u_error;
 	return set_regs(tcp->pid);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	avr32_regs.r12 = tcp->u_rval;
+	return set_regs(tcp->pid);
+}

linux/bfin/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	bfin_r0 = -tcp->u_error;
 	return upoke(tcp->pid, PT_R0, bfin_r0);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	bfin_r0 = tcp->u_rval;
+	return upoke(tcp->pid, PT_R0, bfin_r0);
+}

linux/crisv10/set_error.c
@@ -4,3 +4,10 @@ arch_set_error(struct tcb *tcp)
 	cris_r10 = -tcp->u_error;
 	return upoke(tcp->pid, 4 * PT_R10, cris_r10);
 }
+
+static int
+arch_set_success(struct tcb *tcp)
+{
+	cris_r10 = tcp->u_rval;
+	return upoke(tcp->pid, 4 * PT_R10, cris_r10);
+}

commit 5a2a54591d944a3761d96d8767616d32ee45b111

or1k: fix build
This fixes a typo in the arch_set_success for the or1k architecture.

* linux/or1k/set_error.c (arch_setsuccess): Rename to arch_set_success.

Fixes: 41d647c ("Implement success injection")
Closes: https://github.com/strace/strace/pull/6

linux/or1k/set_error.c
@@ -6,7 +6,7 @@ arch_set_error(struct tcb *tcp)
 }
 
 static int
-arch_setsuccess(struct tcb *tcp)
+arch_set_success(struct tcb *tcp)
 {
 	or1k_regs.gpr[11] = tcp->u_rval;
 	return set_regs(tcp->pid);