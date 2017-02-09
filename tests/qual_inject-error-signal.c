98313939b3d08f1ede8459630a1f5de934c29a36
Dmitry V. Levin <ldv@altlinux.org>
2017-02-09 22:26:14 +0000

    tests: check signal injection along with fault injection
    * tests/qual_inject-error-signal.c: New file.
    * tests/qual_inject-error-signal.expected: Likewise.
    * tests/qual_inject-error-signal.test: New test.
    * tests/.gitignore: Add qual_inject-error-signal.
    * tests/Makefile.am (check_PROGRAMS): Likewise.
    (MISC_TESTS): Add qual_inject-error-signal.test.
    (EXTRA_DIST): Add qual_inject-error-signal.expected.

---
+++ tests/qual_inject-error-signal.c
@@ -0,0 +1,69 @@
+/*
+ * Check fault injection along with signal injection.
+ *
+ * Copyright (c) 2017 Dmitry V. Levin <ldv@altlinux.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <asm/unistd.h>
+
+static struct stat before, after;
+
+static void
+handler(int sig)
+{
+	if (stat(".", &after))
+		syscall(__NR_exit_group, 2);
+
+	if (before.st_dev != after.st_dev || before.st_ino != after.st_ino)
+		syscall(__NR_exit_group, 3);
+
+	syscall(__NR_exit_group, 0);
+}
+
+int
+main(void)
+{
+	const struct sigaction act = { .sa_handler = handler };
+	if (sigaction(SIGUSR1, &act, NULL))
+		perror_msg_and_fail("sigaction");
+
+	sigset_t mask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGUSR1);
+	if (sigprocmask(SIG_UNBLOCK, &mask, NULL))
+		perror_msg_and_fail("sigprocmask");
+
+	if (stat(".", &before))
+		perror_msg_and_fail("stat");
+
+	syscall(__NR_chdir, ".");
+	syscall(__NR_exit_group, 1);
+	return 1;
+}
