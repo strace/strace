6117728aacf431a3e3c05761766c776874cc0861
Dmitry V. Levin <ldv@altlinux.org>
2017-03-10 19:45:06 +0000

    nios2: export nios2_sp_ptr
    Export SP register for later use by get_rt_sigframe_addr.
    
    * linux/nios2/arch_regs.h: New file.
    * Makefile.am (EXTRA_DIST): Add it.
    * linux/nios2/arch_regs.c (nios2_sp_ptr): New variable.

--- linux/nios2/arch_regs.c
+++ linux/nios2/arch_regs.c
@@ -1,3 +1,4 @@
 static struct user_pt_regs nios2_regs;
+unsigned long *const nios2_sp_ptr = &nios2_regs.sp;
 # define ARCH_REGS_FOR_GETREGSET nios2_regs
 #define ARCH_PC_REG nios2_regs.regs[PTR_EA]
---
+++ linux/nios2/arch_regs.h
@@ -0,0 +1,1 @@
+extern unsigned long *const nios2_sp_ptr;
