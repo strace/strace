645eb6405b198497b8ee023090582410103b5c70
Dmitry V. Levin <ldv@altlinux.org>
2017-03-13 11:37:36 +0000

    tests: rewrite sigreturn syscall decoding check from match_grep to match_diff
    Unlike the former test that was based on match_grep,
    the new one uses match_diff and does more rigorous testing.
    
    * tests/sigreturn.c: Rewrite.
    * tests/sigreturn.test: Likewise.

--- tests/sigreturn.c
+++ tests/sigreturn.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Dmitry V. Levin <ldv@altlinux.org>
+ * Copyright (c) 2015-2017 Dmitry V. Levin <ldv@altlinux.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,23 +26,38 @@
  */
 
 #include "tests.h"
-#include <stdlib.h>
-#include <signal.h>
+#include <asm/unistd.h>
 
-#ifdef ASM_SIGRTMIN
-# define RT_0 ASM_SIGRTMIN
-#else
-/* Linux kernel >= 3.18 defines SIGRTMIN to 32 on all architectures. */
-# define RT_0 32
+#if defined __powerpc64__ \
+ || (defined __sparc__ && defined __arch64__)
+/* Old sigreturn is defined but not implemented in the kernel. */
+# undef __NR_sigreturn
 #endif
 
-static void handler(int sig)
+#ifdef __NR_sigreturn
+
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# ifdef ASM_SIGRTMIN
+#  define RT_0 ASM_SIGRTMIN
+# else
+/* Linux kernel >= 3.18 defines SIGRTMIN to 32 on all architectures. */
+#  define RT_0 32
+# endif
+
+static void
+handler(int sig)
 {
 }
 
-int main(void) {
-	sigset_t set;
+int
+main(void)
+{
+	static sigset_t set;
 	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
 	sigaddset(&set, SIGUSR2);
 	sigaddset(&set, SIGCHLD);
 	sigaddset(&set, RT_0 +  3);
@@ -50,8 +65,28 @@ int main(void) {
 	sigaddset(&set, RT_0 +  5);
 	sigaddset(&set, RT_0 + 26);
 	sigaddset(&set, RT_0 + 27);
-	sigprocmask(SIG_SETMASK, &set, NULL);
-	signal(SIGUSR1, handler);
-	raise(SIGUSR1);
+	if (sigprocmask(SIG_SETMASK, &set, NULL))
+		perror_msg_and_fail("sigprocmask");
+	sigemptyset(&set);
+
+	/* This should result to old sigreturn. */
+	if (signal(SIGUSR1, handler) == SIG_ERR)
+		perror_msg_and_fail("sigaction");
+
+	if (raise(SIGUSR1))
+		perror_msg_and_fail("raise");
+
+	static const char *const sigs =
+		(SIGUSR2 < SIGCHLD) ? "INT USR2 CHLD" : "INT CHLD USR2";
+	static const char *const rt_sigs = "RT_3 RT_4 RT_5 RT_26 RT_27";
+	printf("sigreturn({mask=[%s %s]}) = 0\n", sigs, rt_sigs);
+
+	puts("+++ exited with 0 +++");
 	return 0;
 }
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_sigreturn")
+
+#endif
