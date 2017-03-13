8e1ebd868f23ca8d8419b38b4724308fc209fff0
Dmitry V. Levin <ldv@altlinux.org>
2017-03-13 01:58:39 +0000

    ia64, mips, x86_64: remove no longer used parsers of sigreturn syscalls
    As there is no sigreturn syscall on ia64, mips n32, mips n64, x32,
    and x86_64, no longer used parsers could be safely removed.
    
    * linux/ia64/arch_sigreturn.c: Remove.
    * Makefile.am (EXTRA_DIST): Remove it.
    * linux/mips/arch_sigreturn.c (arch_sigreturn): Remove [!LINUX_MIPSO32].
    * linux/x86_64/arch_sigreturn.c (arch_sigreturn): Remove.

--- linux/mips/arch_sigreturn.c
+++ linux/mips/arch_sigreturn.c
@@ -1,26 +1,17 @@
 static void
 arch_sigreturn(struct tcb *tcp)
 {
-#if defined LINUX_MIPSO32
+	/* 64-bit ABIs do not have old sigreturn. */
+#ifdef LINUX_MIPSO32
 	/*
 	 * offsetof(struct sigframe, sf_mask) ==
 	 * sizeof(sf_ass) + sizeof(sf_pad) + sizeof(struct sigcontext)
 	 */
 	const kernel_ulong_t addr = mips_REG_SP + 6 * 4 +
 				   sizeof(struct sigcontext);
-#else
-	/*
-	 * This decodes rt_sigreturn.
-	 * The 64-bit ABIs do not have sigreturn.
-	 *
-	 * offsetof(struct rt_sigframe, rs_uc) ==
-	 * sizeof(sf_ass) + sizeof(sf_pad) + sizeof(struct siginfo)
-	 */
-	const kernel_ulong_t addr = mips_REG_SP + 6 * 4 + 128 +
-				   offsetof(struct ucontext, uc_sigmask);
-#endif
 
 	tprints("{mask=");
 	print_sigset_addr(tcp, addr);
 	tprints("}");
+#endif
 }
