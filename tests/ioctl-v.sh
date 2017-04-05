c3529547861adad93a548ee26cb9219c29e4cd2c
Dmitry V. Levin <ldv@altlinux.org>
2017-04-05 00:44:30 +0000

    tests: generalize scripts that check non-abbreviated decoding of ioctls
    * tests/ioctl-v.sh: New file.
    * tests/Makefile.am (EXTRA_DIST): Add it.
    (DECODER_TESTS): Remove ioctl_evdev-v.test, ioctl_loop-v.test,
    and ioctl_rtc-v.test.
    * tests/gen_tests.in (ioctl_evdev-v, ioctl_loop-v, ioctl_rtc-v):
    New entries.
    * tests/ioctl_evdev-v.test: Remove.
    * tests/ioctl_loop-v.test: Ditto.
    * tests/ioctl_rtc-v.test: Ditto.

--- tests/ioctl-v.sh
+++ tests/ioctl-v.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Check non-abbreviated decoding of LOOP_* ioctls.
+# Check non-abbreviated decoding of ioctls.
 
 . "${srcdir=.}/init.sh"
 
 check_prog grep
 run_prog > /dev/null
-run_strace -a22 -v -eioctl $args > "$EXP"
+run_strace -a16 -v -eioctl $args > "$EXP"
 grep -v '^ioctl([012],' < "$LOG" > "$OUT"
 match_diff "$OUT" "$EXP"
