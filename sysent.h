a0851a70ef72ffd78cc6205381a20d0ee3d7e010
Eugene Syromyatnikov <evgsyr@gmail.com>
2017-04-16 15:24:00 +0200

    sysent.h: format syscall flag macros
    Add some tabulation and leading zeroes to the values.

--- sysent.h
+++ sysent.h
@@ -9,18 +9,18 @@ typedef struct sysent {
 	const char *sys_name;
 } struct_sysent;
 
-#define TRACE_FILE		001	/* Trace file-related syscalls. */
-#define TRACE_IPC		002	/* Trace IPC-related syscalls. */
-#define TRACE_NETWORK		004	/* Trace network-related syscalls. */
-#define TRACE_PROCESS		010	/* Trace process-related syscalls. */
-#define TRACE_SIGNAL		020	/* Trace signal-related syscalls. */
-#define TRACE_DESC		040	/* Trace file descriptor-related syscalls. */
-#define TRACE_MEMORY		0100	/* Trace memory mapping-related syscalls. */
-#define SYSCALL_NEVER_FAILS	0200	/* Syscall is always successful. */
-#define STACKTRACE_INVALIDATE_CACHE 0400  /* Trigger proc/maps cache updating */
-#define STACKTRACE_CAPTURE_ON_ENTER 01000 /* Capture stacktrace on "entering" stage */
-#define TRACE_INDIRECT_SUBCALL	02000	/* Syscall is an indirect socket/ipc subcall. */
-#define COMPAT_SYSCALL_TYPES	04000	/* A compat syscall that uses compat types. */
-#define TRACE_STATFS		040000	/* Trace statfs, statfs64, and statvfs syscalls. */
+#define TRACE_FILE			00000001	/* Trace file-related syscalls. */
+#define TRACE_IPC			00000002	/* Trace IPC-related syscalls. */
+#define TRACE_NETWORK			00000004	/* Trace network-related syscalls. */
+#define TRACE_PROCESS			00000010	/* Trace process-related syscalls. */
+#define TRACE_SIGNAL			00000020	/* Trace signal-related syscalls. */
+#define TRACE_DESC			00000040	/* Trace file descriptor-related syscalls. */
+#define TRACE_MEMORY			00000100	/* Trace memory mapping-related syscalls. */
+#define SYSCALL_NEVER_FAILS		00000200	/* Syscall is always successful. */
+#define STACKTRACE_INVALIDATE_CACHE	00000400	/* Trigger proc/maps cache updating */
+#define STACKTRACE_CAPTURE_ON_ENTER	00001000	/* Capture stacktrace on "entering" stage */
+#define TRACE_INDIRECT_SUBCALL		00002000	/* Syscall is an indirect socket/ipc subcall. */
+#define COMPAT_SYSCALL_TYPES		00004000	/* A compat syscall that uses compat types. */
+#define TRACE_STATFS			00040000	/* Trace statfs, statfs64, and statvfs syscalls. */
 
 #endif /* !STRACE_SYSENT_H */
