4d8eedbd4fde86062d109b9b05d83c36223a4d6f
Dmitry V. Levin <ldv@altlinux.org>
2017-11-05 20:18:44 +0000

    hppa: wire up statx syscall
    * linux/hppa/syscallent.h [349]: Add statx entry.

--- linux/hppa/syscallent.h
+++ linux/hppa/syscallent.h
@@ -352,3 +352,4 @@
 [346] = { 6,	TD,		SEN(copy_file_range),		"copy_file_range"	},
 [347] = { 6,	TD,		SEN(preadv2),			"preadv2"		},
 [348] = { 6,	TD,		SEN(pwritev2),			"pwritev2"		},
+[349] = { 5,	TD|TF|TSTA,	SEN(statx),			"statx"			},
