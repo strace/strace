5453c146dc7e299e6647689d4ce01324483fbd4a
Harsha Sharma <harshasharmaiitr@gmail.com>
2017-12-24 12:31:36 +0530

    tests: enhance run.sh usage error diagnostics
    * tests/run.sh: Execute timeout command only when invoked with
    an argument, otherwise print an error message.
    
    Signed-off-by: Harsha Sharma <harshasharmaiitr@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -9,4 +9,9 @@ TIMEOUT="timeout -k 5 -s XCPU $TIMEOUT_DURATION"
 $TIMEOUT true > /dev/null 2>&1 ||
 	TIMEOUT=
 
+if [ $# -eq 0 ]; then
+	echo 'No command or test-file specified' >&2
+	exit 1
+fi
+
 exec $TIMEOUT "$@"
