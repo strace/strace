306fbc65c24d5f36b8972203fed1c033bea94124
Dmitry V. Levin <ldv@altlinux.org>
2018-01-07 00:05:42 +0000

    personality.c: use sprintxval
    * personality.c (SYS_FUNC(personality)): Use sprintxval instead of local
    implementation.

--- personality.c
+++ personality.c
@@ -53,13 +53,9 @@ SYS_FUNC(personality)
 		return 0;
 
 	pers = tcp->u_rval;
-	const char *type = xlookup(personality_types, pers & PER_MASK);
-	char *p;
 	static char outstr[1024];
-	if (type)
-		p = stpcpy(outstr, type);
-	else
-		p = outstr + sprintf(outstr, "%#x /* %s */", pers & PER_MASK, "PER_???");
+	char *p = outstr + sprintxval(outstr, sizeof(outstr), personality_types,
+				      pers & PER_MASK, "PER_???");
 	pers &= ~PER_MASK;
 	if (pers)
 		strcpy(p, sprintflags("|", personality_flags, pers));
