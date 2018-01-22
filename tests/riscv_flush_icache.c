13f0c7a9555f711bb3fde84521fe23d8d03a659a
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-01-22 13:07:13 +0100

    tests: check riscv_flush_icache syscall decoder
    * tests/riscv_flush_icache.c: New file.
    * tests/.gitignore: Add riscv_flush_icache.
    * tests/gen_tests.in: Likewise.
    * tests/pure_executables.list: Likewise.

---
+++ tests/riscv_flush_icache.c
@@ -0,0 +1,93 @@
+/*
+ * Check decoding of riscv_flush_icache syscall.
+ *
+ * Copyright (c) 2018 The strace developers.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "tests.h"
+
+#include <asm/unistd.h>
+
+#include "scno.h"
+
+#ifdef __NR_riscv_flush_icache
+
+# include <stdint.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int main(void)
+{
+	static struct {
+		kernel_ulong_t addr;
+		const char *str;
+	} addrs[] = {
+		{ (kernel_ulong_t) (uintptr_t) ARG_STR(NULL) },
+		{ (kernel_ulong_t) 0xbadc0deddeadf157ULL,
+			sizeof(kernel_ulong_t) == 8 ? "0xbadc0deddeadf157" :
+			"0xdeadf157" },
+	};
+	static struct {
+		kernel_ulong_t val;
+		const char *str;
+	} flags[] = {
+		{ ARG_STR(0) },
+		{ 1, "SYS_RISCV_FLUSH_ICACHE_LOCAL" },
+		{ (kernel_ulong_t) 0xfacefeedfffffffeULL,
+			sizeof(kernel_ulong_t) == 8 ?
+			"0xfacefeedfffffffe /* SYS_RISCV_FLUSH_ICACHE_??? */" :
+			"0xfffffffe /* SYS_RISCV_FLUSH_ICACHE_??? */" },
+		{ (kernel_ulong_t) 0xfacefeedffffffffULL,
+			sizeof(kernel_ulong_t) == 8 ?
+			"SYS_RISCV_FLUSH_ICACHE_LOCAL|0xfacefeedfffffffe" :
+			"SYS_RISCV_FLUSH_ICACHE_LOCAL|0xfffffffe" },
+	};
+
+	for (size_t i = 0; i < ARRAY_SIZE(addrs); i++) {
+		for (size_t j = 0; j < ARRAY_SIZE(addrs); j++) {
+			for (size_t k = 0; k < ARRAY_SIZE(flags); k++) {
+				long rc = syscall(__NR_riscv_flush_icache,
+						  addrs[i].addr,
+						  addrs[j].addr,
+						  flags[k].val);
+
+				printf("riscv_flush_icache(%s, %s, %s) = %s\n",
+				       addrs[i].str, addrs[j].str, flags[k].str,
+				       sprintrc(rc));
+			}
+		}
+	}
+
+	puts("+++ exited with 0 +++");
+
+	return 0;
+}
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_riscv_flush_icache");
+
+#endif
