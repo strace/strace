037802b576e43bedb729ef78a07ec50d8adcf008
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-04-04 15:42:00 +0200

    s390: use printxvals_ex for command printing in s390_runtime_instr
    * s390.c (SYS_FUNC(s390_runtime_instr)): Replace manual command argument
    decoding with a printxvals_ex call.

--- s390.c
+++ s390.c
@@ -1204,12 +1204,10 @@ SYS_FUNC(s390_runtime_instr)
 	int command = (int) tcp->u_arg[0];
 	int signum = (int) tcp->u_arg[1];
 
-	const char *command_descr =
-		xlookup(s390_runtime_instr_commands, command);
 
-	tprintf("%d", command);
-	tprints_comment(command_descr ? command_descr :
-			"S390_RUNTIME_INSTR_???");
+	printxvals_ex(command, "S390_RUNTIME_INSTR_???",
+		      XLAT_STYLE_VERBOSE | XLAT_STYLE_FMT_D,
+		      s390_runtime_instr_commands, NULL);
 
 	/*
 	 * signum is ignored since Linux 4.4, but let's print it for start
