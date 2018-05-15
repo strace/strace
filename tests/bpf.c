b532e92c6ea0f8b7dc6db455248c34f8376489b1
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-05-15 14:04:11 +0200

    xlat: update bpf_attach_type constants
    * xlat/bpf_attach_type.in (BPF_SK_MSG_VERDICT): New constant,
    introduced by Linux commit v4.17-rc1~148^2~156^2~1^2~13.
    (BPF_CGROUP_INET4_BIND, BPF_CGROUP_INET6_BIND): New constants,
    introduced by Linux commit v4.17-rc1~148^2~19^2^2~6.
    (BPF_CGROUP_INET4_CONNECT, BPF_CGROUP_INET6_CONNECT): New constants,
    introduced by Linux commit v4.17-rc1~148^2~19^2^2~3.
    (BPF_CGROUP_INET4_POST_BIND, BPF_CGROUP_INET6_POST_BIND): New constants,
    introduced by Linux commit v4.17-rc1~148^2~19^2^2~1.
    * tests/bpf.c (BPF_PROG_QUERY_checks): Update expected output.

--- tests/bpf.c
+++ tests/bpf.c
@@ -848,13 +848,13 @@ static const struct bpf_attr_check BPF_PROG_QUERY_checks[] = {
 	{ /* 1 */
 		.data = { .BPF_PROG_QUERY_data = {
 			.target_fd = 3141592653U,
-			.attach_type = 6,
+			.attach_type = 13,
 			.query_flags = 1,
 			.attach_flags = 3,
 		} },
 		.size = offsetofend(struct BPF_PROG_QUERY_struct, attach_flags),
 		.str = "query={target_fd=-1153374643"
-		       ", attach_type=BPF_CGROUP_DEVICE"
+		       ", attach_type=BPF_CGROUP_INET6_POST_BIND"
 		       ", query_flags=BPF_F_QUERY_EFFECTIVE"
 		       ", attach_flags=BPF_F_ALLOW_OVERRIDE|BPF_F_ALLOW_MULTI"
 		       ", prog_ids=NULL, prog_cnt=0}",
@@ -862,7 +862,7 @@ static const struct bpf_attr_check BPF_PROG_QUERY_checks[] = {
 	{ /* 2 */
 		.data = { .BPF_PROG_QUERY_data = {
 			.target_fd = 3141592653U,
-			.attach_type = 7,
+			.attach_type = 14,
 			.query_flags = 0xfffffffe,
 			.attach_flags = 0xfffffffc,
 			.prog_ids = 0xffffffffffffffffULL,
@@ -870,7 +870,7 @@ static const struct bpf_attr_check BPF_PROG_QUERY_checks[] = {
 		} },
 		.size = offsetofend(struct BPF_PROG_QUERY_struct, prog_cnt),
 		.str = "query={target_fd=-1153374643"
-		       ", attach_type=0x7 /* BPF_??? */"
+		       ", attach_type=0xe /* BPF_??? */"
 		       ", query_flags=0xfffffffe /* BPF_F_QUERY_??? */"
 		       ", attach_flags=0xfffffffc /* BPF_F_??? */"
 		       ", prog_ids=0xffffffffffffffff, prog_cnt=2718281828}",
