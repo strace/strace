a94e301a9f202e53c74b504542d6858dd7be84a5
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2018-05-16 20:54:59 +0300

    Update ioctl entries from linux v4.17
    * linux/32/ioctls_inc_align16.h: Update from linux v4.17-rc5
    using ioctls_gen.sh.
    * linux/32/ioctls_inc_align32.h: Likewise.
    * linux/32/ioctls_inc_align64.h: Likewise.
    * linux/64/ioctls_inc.h: Likewise.
    * linux/x32/ioctls_inc0.h: Likewise.
    * linux/i386/ioctls_arch0.h: Likewise.
    * linux/x86_64/ioctls_arch0.h: Likewise.
    * NEWS: Mention this.

--- linux/x86_64/ioctls_arch0.h
+++ linux/x86_64/ioctls_arch0.h
@@ -58,6 +58,7 @@
 { "linux/kvm.h", "KVM_GET_XCRS", _IOC_READ, 0xaea6, 0x188 },
 { "linux/kvm.h", "KVM_GET_XSAVE", _IOC_READ, 0xaea4, 0x1000 },
 { "linux/kvm.h", "KVM_HAS_DEVICE_ATTR", _IOC_WRITE, 0xaee3, 0x18 },
+{ "linux/kvm.h", "KVM_HYPERV_EVENTFD", _IOC_WRITE, 0xaebd, 0x18 },
 { "linux/kvm.h", "KVM_INTERRUPT", _IOC_WRITE, 0xae86, 0x04 },
 { "linux/kvm.h", "KVM_IOEVENTFD", _IOC_WRITE, 0xae79, 0x40 },
 { "linux/kvm.h", "KVM_IRQFD", _IOC_WRITE, 0xae76, 0x20 },
