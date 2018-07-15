3dbffb74cc2b4323016ba8ba05380a6aba5f3a8f
Dmitry V. Levin <ldv@altlinux.org>
2018-07-15 22:14:29 +0000

    tests: fix build with fresh glibc
    Workaround additional incompatibility between <sys/stat.h> and
    <asm/stat.h> introduced by glibc-2.27.9000-566-gfd70af4.
    
    * tests/xstatx.c (statx, statx_timestamp): Redefine before the inclusion
    of <sys/stat.h> to avoid conflicts between definitions provided by the
    linux kernel headers and the GNU libc.

--- tests/xstatx.c
+++ tests/xstatx.c
@@ -64,10 +64,14 @@ typedef off_t libc_off_t;
 
 # define stat libc_stat
 # define stat64 libc_stat64
+# define statx libc_statx
+# define statx_timestamp libc_statx_timestamp
 # include <fcntl.h>
 # include <sys/stat.h>
-# undef stat
+# undef statx_timestamp
+# undef statx
 # undef stat64
+# undef stat
 
 # undef st_atime
 # undef st_mtime
