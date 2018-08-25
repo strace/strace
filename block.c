044c1560f2fdaf08f1b262c3ab95576a1aa78180
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-08-25 22:02:35 +0200

    block: move fallback definitions for ioctl commands to xlat
    * xlat/block_ioctl_cmds.in: New file.
    * block.c: Include "xlat/block_ioctl_cmds.h".
    (BLKPG, BLKTRACESETUP, BLKTRACESTART, BLKTRACESTOP, BLKTRACETEARDOWN,
    BLKDISCARD, BLKIOMIN, BLKIOOPT, BLKALIGNOFF, BLKPBSZGET,
    BLKDISCARDZEROES, BLKSECDISCARD, BLKROTATIONAL, BLKZEROOUT): Remove
    fallback definitions as those are porvided by block_ioctl_cmds xlat now.

--- block.c
+++ block.c
@@ -65,59 +65,14 @@ typedef struct blk_user_trace_setup {
 	uint32_t pid;
 } struct_blk_user_trace_setup;
 
+/* Provide fall-back definitions for BLK* ioctls */
+#define XLAT_MACROS_ONLY
+# include "xlat/block_ioctl_cmds.h"
+#undef XLAT_MACROS_ONLY
+
 #include MPERS_DEFS
 
 #include "print_fields.h"
-
-#ifndef BLKPG
-# define BLKPG      _IO(0x12, 105)
-#endif
-
-/*
- * ioctl numbers <= 114 are present in Linux 2.4.  The following ones have been
- * added since then and headers containing them may not be available on every
- * system.
- */
-
-#ifndef BLKTRACESETUP
-# define BLKTRACESETUP _IOWR(0x12, 115, struct_blk_user_trace_setup)
-#endif
-#ifndef BLKTRACESTART
-# define BLKTRACESTART _IO(0x12, 116)
-#endif
-#ifndef BLKTRACESTOP
-# define BLKTRACESTOP _IO(0x12, 117)
-#endif
-#ifndef BLKTRACETEARDOWN
-# define BLKTRACETEARDOWN _IO(0x12, 118)
-#endif
-#ifndef BLKDISCARD
-# define BLKDISCARD _IO(0x12, 119)
-#endif
-#ifndef BLKIOMIN
-# define BLKIOMIN _IO(0x12, 120)
-#endif
-#ifndef BLKIOOPT
-# define BLKIOOPT _IO(0x12, 121)
-#endif
-#ifndef BLKALIGNOFF
-# define BLKALIGNOFF _IO(0x12, 122)
-#endif
-#ifndef BLKPBSZGET
-# define BLKPBSZGET _IO(0x12, 123)
-#endif
-#ifndef BLKDISCARDZEROES
-# define BLKDISCARDZEROES _IO(0x12, 124)
-#endif
-#ifndef BLKSECDISCARD
-# define BLKSECDISCARD _IO(0x12, 125)
-#endif
-#ifndef BLKROTATIONAL
-# define BLKROTATIONAL _IO(0x12, 126)
-#endif
-#ifndef BLKZEROOUT
-# define BLKZEROOUT _IO(0x12, 127)
-#endif
 
 #include "xlat/blkpg_ops.h"
 
