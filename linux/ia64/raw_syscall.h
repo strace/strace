004e6b5b7af458dd465b30a6461cae13c5c98457
Dmitry V. Levin <ldv@altlinux.org>
2018-12-17 17:30:24 +0000

    ia64: do not clobber r12 for raw syscalls
    According to
    https://sourceware.org/ml/libc-alpha/2018-12/msg00470.html
    GCC mainline now gives errors for an asm that clobbers the stack
    pointer.
    
    According to
    https://gcc.gnu.org/ml/gcc-patches/2018-12/msg00932.html>
    GCC previously ignored such a clobber; thus, this patch removes it
    from the clobbers for ia64 raw syscalls.
    
    * linux/ia64/raw_syscall.h (raw_syscall_0): Remove "r12" from clobbers.

--- linux/ia64/raw_syscall.h
+++ linux/ia64/raw_syscall.h
@@ -23,7 +23,7 @@ raw_syscall_0(const kernel_ulong_t nr, kernel_ulong_t *err)
 			     :
 			     : "memory", "out0", "out1", "out2",
 			       "out3", "out4", "out5", "out6", "out7",
-			       "r2", "r3", "r9", "r11", "r12", "r13",
+			       "r2", "r3", "r9", "r11", "r13",
 			       "r14", "r16", "r17", "r18", "r19", "r20",
 			       "r21", "r22", "r23", "r24", "r25", "r26",
 			       "r27", "r28", "r29", "r30", "r31",
