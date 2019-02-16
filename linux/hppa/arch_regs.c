146bfcb8ac654263c511fd213de2a19f4a6e1a9f
Dmitry V. Levin <ldv@altlinux.org>
2019-02-16 00:33:38 +0000

    hppa: define PT_* constants as offsets inside struct pt_regs
    Define constants passed to upeek and upoke using a method that works
    with different wordsize values.
    
    * linux/hppa/arch_regs.c (PT_GR26, PT_GR30, PT_IAOQ0, PT_IAOQ1): Remove.
    (PT_GR20, PT_GR28, ARCH_PC_PEEK_ADDR, ARCH_SP_PEEK_ADDR): Redefine using
    offsets inside struct pt_regs.
    * linux/hppa/get_syscall_args.c (arch_get_syscall_args): Replace PT_GR26
    with offsets inside struct pt_regs.

--- linux/hppa/arch_regs.c
+++ linux/hppa/arch_regs.c
@@ -7,12 +7,8 @@
 
 static unsigned long hppa_r28;
 
-#define PT_GR20 (20*4)
-#define PT_GR26 (26*4)
-#define PT_GR28 (28*4)
-#define PT_GR30 (30*4)
-#define PT_IAOQ0 (106*4)
-#define PT_IAOQ1 (107*4)
+#define PT_GR20 offsetof(struct pt_regs, gr[20])
+#define PT_GR28 offsetof(struct pt_regs, gr[28])
 
-#define ARCH_PC_PEEK_ADDR PT_IAOQ0
-#define ARCH_SP_PEEK_ADDR PT_GR30
+#define ARCH_PC_PEEK_ADDR offsetof(struct pt_regs, iaoq[0])
+#define ARCH_SP_PEEK_ADDR offsetof(struct pt_regs, gr[30])
--- linux/hppa/get_syscall_args.c
+++ linux/hppa/get_syscall_args.c
@@ -9,10 +9,11 @@
 static int
 arch_get_syscall_args(struct tcb *tcp)
 {
-	unsigned int i;
+	for (unsigned int i = 0; i < n_args(tcp); ++i) {
+		unsigned int addr = offsetof(struct pt_regs, gr[26 - i]);
 
-	for (i = 0; i < n_args(tcp); ++i)
-		if (upeek(tcp, PT_GR26-4*i, &tcp->u_arg[i]) < 0)
+		if (upeek(tcp, addr, &tcp->u_arg[i]) < 0)
 			return -1;
+	}
 	return 1;
 }
