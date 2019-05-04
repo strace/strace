e55a41ae1a2db4c96c2ae0ab67b63ed0450aacfb
Dmitry V. Levin <ldv@altlinux.org>
2019-05-04 23:15:52 +0000

    time.c: introduce do_timerfd_gettime and do_timerfd_settime
    These are going to be used to implement parsers of timerfd_gettime64
    and timerfd_settime64 syscalls.
    
    * time.c (do_timerfd_settime, do_timerfd_gettime): New functions.
    ((SYS_FUNC(timerfd_settime)): Turn into a thin wrapper around
    do_timerfd_settime.
    ((SYS_FUNC(timerfd_gettime)): Turn into a thin wrapper around
    do_timerfd_gettime.

--- time.c
+++ time.c
@@ -347,28 +347,40 @@ SYS_FUNC(timerfd_create)
 	return RVAL_DECODED | RVAL_FD;
 }
 
-SYS_FUNC(timerfd_settime)
+static int
+do_timerfd_settime(struct tcb *const tcp, const print_obj_by_addr_fn print_its)
 {
 	if (entering(tcp)) {
 		printfd(tcp, tcp->u_arg[0]);
 		tprints(", ");
 		printflags(timerfdflags, tcp->u_arg[1], "TFD_???");
 		tprints(", ");
-		print_itimerspec(tcp, tcp->u_arg[2]);
+		print_its(tcp, tcp->u_arg[2]);
 		tprints(", ");
 	} else {
-		print_itimerspec(tcp, tcp->u_arg[3]);
+		print_its(tcp, tcp->u_arg[3]);
+	}
+	return 0;
+}
+
+SYS_FUNC(timerfd_settime)
+{
+	return do_timerfd_settime(tcp, print_itimerspec);
+}
+
+static int
+do_timerfd_gettime(struct tcb *const tcp, const print_obj_by_addr_fn print_its)
+{
+	if (entering(tcp)) {
+		printfd(tcp, tcp->u_arg[0]);
+		tprints(", ");
+	} else {
+		print_its(tcp, tcp->u_arg[1]);
 	}
 	return 0;
 }
 
 SYS_FUNC(timerfd_gettime)
 {
-	if (entering(tcp)) {
-		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
-	} else {
-		print_itimerspec(tcp, tcp->u_arg[1]);
-	}
-	return 0;
+	return do_timerfd_gettime(tcp, print_itimerspec);
 }
