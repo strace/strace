1a1a23da04d9ea913d2e98c001b89e9f94873deb
Dmitry V. Levin <ldv@altlinux.org>
2019-07-04 18:25:49 +0000

    sparc, sparc64: fix syscall tampering when PTRACE_GET_SYSCALL_INFO is in use
    When PTRACE_GET_SYSCALL_INFO is in use on sparc, psr is not loaded,
    so it has to be loaded explicitly before tampering.
    Likewise, when PTRACE_GET_SYSCALL_INFO is in use on sparc64, tstate
    is not loaded, so it has to be loaded explicitly before tampering.
    
    * linux/sparc/set_error.c (arch_set_error, arch_set_success): Explicitly
    call get_regs before changing psr when PTRACE_GET_SYSCALL_INFO is in use.
    * linux/sparc64/set_error.c (arch_set_error, arch_set_success):
    Explicitly call get_regs before changing tstate when
    PTRACE_GET_SYSCALL_INFO is in use.
    * NEWS: Mention this fix.

--- linux/sparc64/set_error.c
+++ linux/sparc64/set_error.c
@@ -8,6 +8,8 @@
 static int
 arch_set_error(struct tcb *tcp)
 {
+	if (ptrace_syscall_info_is_valid() && get_regs(tcp) < 0)
+		return -1;
 	sparc_regs.tstate |= 0x1100000000UL;
 	sparc_regs.u_regs[U_REG_O0] = tcp->u_error;
 	return set_regs(tcp->pid);
@@ -16,6 +18,8 @@ arch_set_error(struct tcb *tcp)
 static int
 arch_set_success(struct tcb *tcp)
 {
+	if (ptrace_syscall_info_is_valid() && get_regs(tcp) < 0)
+		return -1;
 	sparc_regs.tstate &= ~0x1100000000UL;
 	sparc_regs.u_regs[U_REG_O0] = tcp->u_rval;
 	return set_regs(tcp->pid);
