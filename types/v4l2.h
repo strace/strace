ec3a11aff2f40e65887a7118bfe8463552ba39e8
Eugene Syromyatnikov <evgsyr@gmail.com>
2019-11-01 02:00:53 +0100

    v4l2: bundle struct v4l2_create_buffers definition
    * configure.ac (AC_CHECK_TYPES): Add a check for
    struct v4l2_create_buffers definition.
    * types/v4l2.h: New file.
    * v4l2.c (DEF_MPERS_TYPE(struct_v4l2_create_buffers)): Remove
    VIDIOC_CREATE_BUFS guard.
    (struct_v4l2_create_buffers): Remove typedef.
    (struct_v4l2_buffer, struct_v4l2_clip, struct_v4l2_ext_control,
    struct_v4l2_ext_controls, struct_v4l2_format, struct_v4l2_framebuffer,
    struct_v4l2_input, struct_v4l2_standard): Move to types/v4l2.h.
    [HAVE_STRUCT_V4L2_CREATE_BUFFERS]: Check struct v4l2_create_buffers
    with CHECK_V4L2_STRUCT_RESERVED_SIZE.
    * xlat/v4l2_ioctl_cmds.in (VIDIOC_CREATE_BUFS): Uncomment.
    
    References: https://bugzilla.redhat.com/show_bug.cgi?id=1758201

---
+++ types/v4l2.h
@@ -0,0 +1,37 @@
+/*
+ * V4L2-related type definitions.
+ *
+ * Copyright (c) 2016-2020 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef STRACE_TYPES_V4L2_H
+#define STRACE_TYPES_V4L2_H
+
+#include <stdint.h>
+#include <linux/ioctl.h>
+#include <linux/types.h>
+#include <linux/videodev2.h>
+
+typedef struct v4l2_buffer struct_v4l2_buffer;
+typedef struct v4l2_clip struct_v4l2_clip;
+typedef struct v4l2_ext_control struct_v4l2_ext_control;
+typedef struct v4l2_ext_controls struct_v4l2_ext_controls;
+typedef struct v4l2_format struct_v4l2_format;
+typedef struct v4l2_framebuffer struct_v4l2_framebuffer;
+typedef struct v4l2_input struct_v4l2_input;
+typedef struct v4l2_standard struct_v4l2_standard;
+
+typedef struct {
+	uint32_t index;
+	uint32_t count;
+	uint32_t memory;
+	struct_v4l2_format format;
+	/** V4L2_BUF_CAP_*, added by Linux commit v4.20-rc1~51^2~14 */
+	uint32_t capabilities;
+	uint32_t reserved[7];
+} struct_v4l2_create_buffers;
+
+#endif /* STRACE_TYPES_V4L2_H */
