86ad99181dbe95ce3ebbb8e47bb744b31330f33b
Dmitry V. Levin <ldv@altlinux.org>
2019-11-15 22:33:53 +0000

    tests: rewrite ioctl_ptp test using sprintrc
    * tests/ioctl_ptp.c: Do not include <errno.h>.
    (main): Use sprintrc.

--- tests/ioctl_ptp.c
+++ tests/ioctl_ptp.c
@@ -12,7 +12,6 @@
 
 #ifdef HAVE_STRUCT_PTP_SYS_OFFSET
 
-# include <errno.h>
 # include <fcntl.h>
 # include <inttypes.h>
 # include <stdio.h>
@@ -28,6 +27,8 @@
 static void
 test_no_device(void)
 {
+	const char *errstr;
+
 	TAIL_ALLOC_OBJECT_CONST_PTR(struct ptp_clock_caps, caps);
 	fill_memory(caps, sizeof(*caps));
 
@@ -40,60 +41,54 @@ test_no_device(void)
 	TAIL_ALLOC_OBJECT_CONST_PTR(struct ptp_perout_request, perout);
 	fill_memory(perout, sizeof(*perout));
 
-	int saved_errno;
-
 	/* PTP_CLOCK_GETCAPS */
-	ioctl(-1, PTP_CLOCK_GETCAPS, NULL);
-	printf("ioctl(-1, PTP_CLOCK_GETCAPS, NULL) = -1 EBADF (%m)\n");
-	ioctl(-1, PTP_CLOCK_GETCAPS, caps);
-	printf("ioctl(-1, PTP_CLOCK_GETCAPS, %p) = -1 EBADF (%m)\n", caps);
+	errstr = sprintrc(ioctl(-1, PTP_CLOCK_GETCAPS, NULL));
+	printf("ioctl(-1, PTP_CLOCK_GETCAPS, NULL) = %s\n", errstr);
+	errstr = sprintrc(ioctl(-1, PTP_CLOCK_GETCAPS, caps));
+	printf("ioctl(-1, PTP_CLOCK_GETCAPS, %p) = %s\n", caps, errstr);
 
 	/* PTP_SYS_OFFSET */
-	ioctl(-1, PTP_SYS_OFFSET, NULL);
-	printf("ioctl(-1, PTP_SYS_OFFSET, NULL) = -1 EBADF (%m)\n");
-	ioctl(-1, PTP_SYS_OFFSET, sysoff);
-	printf("ioctl(-1, PTP_SYS_OFFSET, {n_samples=%u}) = -1 EBADF (%m)\n",
-	       sysoff->n_samples);
+	errstr = sprintrc(ioctl(-1, PTP_SYS_OFFSET, NULL));
+	printf("ioctl(-1, PTP_SYS_OFFSET, NULL) = %s\n", errstr);
+	errstr = sprintrc(ioctl(-1, PTP_SYS_OFFSET, sysoff));
+	printf("ioctl(-1, PTP_SYS_OFFSET, {n_samples=%u}) = %s\n",
+	       sysoff->n_samples, errstr);
 
 	/* PTP_ENABLE_PPS */
-	ioctl(-1, PTP_ENABLE_PPS, 0);
-	printf("ioctl(-1, PTP_ENABLE_PPS, 0) = -1 EBADF (%m)\n");
-	ioctl(-1, PTP_ENABLE_PPS, 1);
-	printf("ioctl(-1, PTP_ENABLE_PPS, 1) = -1 EBADF (%m)\n");
+	errstr = sprintrc(ioctl(-1, PTP_ENABLE_PPS, 0));
+	printf("ioctl(-1, PTP_ENABLE_PPS, 0) = %s\n", errstr);
+	errstr = sprintrc(ioctl(-1, PTP_ENABLE_PPS, 1));
+	printf("ioctl(-1, PTP_ENABLE_PPS, 1) = %s\n", errstr);
 
 	/* PTP_EXTTS_REQUEST */
-	ioctl(-1, PTP_EXTTS_REQUEST, NULL);
-	printf("ioctl(-1, PTP_EXTTS_REQUEST, NULL) = -1 EBADF (%m)\n");
-	ioctl(-1, PTP_EXTTS_REQUEST, extts);
-	saved_errno = errno;
+	errstr = sprintrc(ioctl(-1, PTP_EXTTS_REQUEST, NULL));
+	printf("ioctl(-1, PTP_EXTTS_REQUEST, NULL) = %s\n", errstr);
+	errstr = sprintrc(ioctl(-1, PTP_EXTTS_REQUEST, extts));
 	printf("ioctl(-1, PTP_EXTTS_REQUEST, {index=%d, flags=", extts->index);
 	printflags(ptp_extts_flags, extts->flags, "PTP_???");
-	errno = saved_errno;
-	printf("}) = -1 EBADF (%m)\n");
+	printf("}) = %s\n", errstr);
 
 	/* PTP_PEROUT_REQUEST */
-	ioctl(-1, PTP_PEROUT_REQUEST, NULL);
-	printf("ioctl(-1, PTP_PEROUT_REQUEST, NULL) = -1 EBADF (%m)\n");
-	ioctl(-1, PTP_PEROUT_REQUEST, perout);
-	saved_errno = errno;
+	errstr = sprintrc(ioctl(-1, PTP_PEROUT_REQUEST, NULL));
+	printf("ioctl(-1, PTP_PEROUT_REQUEST, NULL) = %s\n", errstr);
+	errstr = sprintrc(ioctl(-1, PTP_PEROUT_REQUEST, perout));
 	printf("ioctl(-1, PTP_PEROUT_REQUEST, {start={sec=%" PRId64
 	       ", nsec=%" PRIu32 "}, period={sec=%" PRId64 ", nsec=%" PRIu32 "}"
 	       ", index=%d, flags=",
 	       (int64_t) perout->start.sec, perout->start.nsec,
 	       (int64_t)perout->period.sec, perout->period.nsec, perout->index);
 	printflags(ptp_perout_flags, perout->flags, "PTP_???");
-	errno = saved_errno;
-	printf("}) = -1 EBADF (%m)\n");
+	printf("}) = %s\n", errstr);
 
 	/* unrecognized */
 	ioctl(-1, _IOC(_IOC_READ, PTP_CLK_MAGIC, 0xff, 0xfe), 0);
-	printf("ioctl(-1, _IOC(_IOC_READ, %#x, 0xff, 0xfe), 0)"
-	       " = -1 EBADF (%m)\n", PTP_CLK_MAGIC);
+	printf("ioctl(-1, _IOC(_IOC_READ, %#x, 0xff, 0xfe), 0) = %s\n",
+	       PTP_CLK_MAGIC, errstr);
 
 	const unsigned long arg = (unsigned long) 0xfacefeeddeadbeefULL;
 	ioctl(-1, _IOC(_IOC_WRITE, PTP_CLK_MAGIC, 0xfd, 0xfc), arg);
 	printf("ioctl(-1, _IOC(_IOC_WRITE, %#x, 0xfd, 0xfc), %#lx)"
-	       " = -1 EBADF (%m)\n", PTP_CLK_MAGIC, arg);
+	       " = %s\n", PTP_CLK_MAGIC, arg, errstr);
 }
 
 int
