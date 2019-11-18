fd67ca9136955d94d4435c7e4b3ab9c07636960c
Dmitry V. Levin <ldv@altlinux.org>
2019-11-18 16:58:50 +0000

    tests: robustify umoven-illptr test against undefined __NR_nanosleep
    * tests/umoven-illptr.c: Skip the test if __NR_nanosleep is not defined.
    Include "kernel_old_timespec.h" instead of <time.h>, use
    kernel_old_timespec_t instead of struct timespec.

--- tests/umoven-illptr.c
+++ tests/umoven-illptr.c
@@ -9,30 +9,49 @@
  */
 
 #include "tests.h"
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
 #include "scno.h"
+
+#ifdef __NR_nanosleep
+
+# include <stdio.h>
+# include <unistd.h>
+
+# include "kernel_old_timespec.h"
+
+static const char *errstr;
+
+static long
+k_nanosleep(const kernel_ulong_t arg1, const kernel_ulong_t arg2)
+{
+	const kernel_ulong_t bad = (kernel_ulong_t) 0xbadc0dedbadc0dedULL;
+	const long rc = syscall(__NR_nanosleep, arg1, arg2, bad, bad, bad, bad);
+	errstr = sprintrc(rc);
+	return rc;
+}
 
 int
 main(void)
 {
-	if (F8ILL_KULONG_SUPPORTED) {
-		struct timespec ts = { 0, 0 };
-		const void *const p = tail_memdup(&ts, sizeof(ts));
-
-		long rc = syscall(__NR_nanosleep, p, NULL);
-		printf("nanosleep({tv_sec=0, tv_nsec=0}, NULL) = %s\n",
-		       sprintrc(rc));
-
-		const kernel_ulong_t ill = f8ill_ptr_to_kulong(p);
-		rc = syscall(__NR_nanosleep, ill, NULL);
-		printf("nanosleep(%#llx, NULL) = %s\n",
-		       (unsigned long long) ill, sprintrc(rc));
-
-		puts("+++ exited with 0 +++");
-		return 0;
-	} else {
+	if (!F8ILL_KULONG_SUPPORTED)
 		return 77;
-	}
+
+	kernel_old_timespec_t ts = { 0, 0 };
+	const void *const p = tail_memdup(&ts, sizeof(ts));
+
+	k_nanosleep((unsigned long) p, 0);
+	printf("nanosleep({tv_sec=0, tv_nsec=0}, NULL) = %s\n", errstr);
+
+	const kernel_ulong_t ill = f8ill_ptr_to_kulong(p);
+	k_nanosleep(ill, 0);
+	printf("nanosleep(%#llx, NULL) = %s\n",
+	       (unsigned long long) ill, errstr);
+
+	puts("+++ exited with 0 +++");
+	return 0;
 }
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_nanosleep")
+
+#endif
