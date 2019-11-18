a1787425c848fe39922c2ef51be295af77c964a1
Dmitry V. Levin <ldv@altlinux.org>
2019-11-18 16:58:50 +0000

    tests: change adjtimex test to use __NR_adjtimex
    Starting with glibc commit glibc-2.30.9000-215-gc81aa64e81,
    adjtimex function in glibc is implemented on top of clock_adjtime,
    which makes it unsuitable for the test of adjtimex decoder.
    
    * configure.ac (AC_CHECK_MEMBERS): Remove struct timex.tai.
    * tests/adjtimex.c: Skip the test if __NR_adjtimex is not defined.
    Include "kernel_old_timex.h".
    (k_adjtimex): New function.
    (main): Change the type of tx pointer to kernel_old_timex_t,
    use k_adjtimex to invoke adjtimex syscall.

--- tests/adjtimex.c
+++ tests/adjtimex.c
@@ -8,26 +8,45 @@
  */
 
 #include "tests.h"
-#include <stddef.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <sys/timex.h>
+#include "scno.h"
 
-#include "xlat.h"
-#include "xlat/adjtimex_state.h"
-#include "xlat/adjtimex_status.h"
+#ifdef __NR_adjtimex
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdint.h>
+# include <string.h>
+# include <sys/timex.h>
+# include <unistd.h>
+
+# include "kernel_old_timex.h"
+
+# include "xlat.h"
+# include "xlat/adjtimex_state.h"
+# include "xlat/adjtimex_status.h"
+
+static const char *errstr;
+
+static long
+k_adjtimex(void *const buf)
+{
+	const kernel_ulong_t bad = (kernel_ulong_t) 0xbadc0dedbadc0dedULL;
+	const kernel_ulong_t arg1 = (uintptr_t) buf;
+	const long rc = syscall(__NR_adjtimex, arg1, bad, bad, bad, bad, bad);
+	errstr = sprintrc(rc);
+	return rc;
+}
 
 int
 main(void)
 {
-	int state = adjtimex(NULL);
-	printf("adjtimex(NULL) = %s\n", sprintrc(state));
+	int state = k_adjtimex(NULL);
+	printf("adjtimex(NULL) = %s\n", errstr);
 
-	TAIL_ALLOC_OBJECT_CONST_PTR(struct timex, tx);
+	TAIL_ALLOC_OBJECT_CONST_PTR(kernel_old_timex_t, tx);
 	memset(tx, 0, sizeof(*tx));
 
-	state = adjtimex(tx);
+	state = k_adjtimex(tx);
 	if (state < 0)
 		perror_msg_and_skip("adjtimex");
 
@@ -60,11 +79,7 @@ main(void)
 	       (intmax_t) tx->calcnt,
 	       (intmax_t) tx->errcnt,
 	       (intmax_t) tx->stbcnt,
-#ifdef HAVE_STRUCT_TIMEX_TAI
 	       tx->tai,
-#else
-	       *(const int *)((const void *) tx + offsetofend(struct timex, stbcnt)),
-#endif
 	       state);
 	printxval(adjtimex_state, (unsigned int) state, NULL);
 	puts(")");
@@ -72,3 +87,9 @@ main(void)
 	puts("+++ exited with 0 +++");
 	return 0;
 }
+
+#else
+
+SKIP_MAIN_UNDEFINED("__NR_adjtimex")
+
+#endif
