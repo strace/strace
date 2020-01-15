6ec1309473ea0ec41217a1f07ec37ec499a790b6
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-01-15 14:01:03 +0100

    xlat: add IORING_SETUP_CQSIZE to uring_setup_flags
    * xlat/uring_setup_flags.in (IORING_SETUP_CQSIZE): New constant,
    introduced by Linux commit v5.5-rc1~204^2~60.
    * tests/io_uring_setup.c (main): Update expected output.

--- tests/io_uring_setup.c
+++ tests/io_uring_setup.c
@@ -63,9 +63,9 @@ main(void)
 	params->flags = -1;
 	sys_io_uring_setup(1, params);
 	printf("io_uring_setup(%u, {flags=IORING_SETUP_IOPOLL"
-	       "|IORING_SETUP_SQPOLL|IORING_SETUP_SQ_AFF|%#x"
-	       ", sq_thread_cpu=%#x, sq_thread_idle=%u, resv=[",
-	       1, -1U - 7, params->sq_thread_cpu, params->sq_thread_idle);
+	       "|IORING_SETUP_SQPOLL|IORING_SETUP_SQ_AFF|IORING_SETUP_CQSIZE"
+	       "|%#x, sq_thread_cpu=%#x, sq_thread_idle=%u, resv=[",
+	       1, -1U - 15, params->sq_thread_cpu, params->sq_thread_idle);
 	for (unsigned int i = RESV_START; i < ARRAY_SIZE(params->resv); ++i)
 		printf("%s%#x", i != RESV_START ? ", " : "", params->resv[i]);
 	printf("]}) = %s\n", errstr);
