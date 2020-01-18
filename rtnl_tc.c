0ce0e53343b5c2134306d3c18a045fb26941546d
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-01-18 13:49:43 +0100

    rtnl_tc: add support for TCA_STATS_PKT64 attribute decoding
    Introduced by Linux kernel commit v5.5-rc1~174^2~219^2.
    
    * configure.ac (AC_CHECK_DECLS): Check for TCA_STATS_PKT64.
    * rtnl_tc.c (tca_stats_nla_decoders) <[TCA_STATS_PKT64]>: New array
    item, call decode_nla_u64.
    * xlat/rtnl_tca_stats_attrs.in (TCA_STATS_PKT64): New constant.
    * tests/nlattr_tc_stats.c: Add TCA_STATS_PKT64 attribute decoding check.

--- rtnl_tc.c
+++ rtnl_tc.c
@@ -174,6 +174,7 @@ static const nla_decoder_t tca_stats_nla_decoders[] = {
 	[TCA_STATS_RATE_EST64]	= decode_gnet_stats_rate_est64,
 	[TCA_STATS_PAD]		= NULL,
 	[TCA_STATS_BASIC_HW]	= decode_gnet_stats_basic,
+	[TCA_STATS_PKT64]	= decode_nla_u64,
 };
 
 bool
--- tests/nlattr_tc_stats.c
+++ tests/nlattr_tc_stats.c
@@ -16,6 +16,10 @@
 # include <linux/gen_stats.h>
 # include <linux/rtnetlink.h>
 
+#if !HAVE_DECL_TCA_STATS_PKT64
+enum { TCA_STATS_PKT64 = 8 };
+#endif
+
 const unsigned int hdrlen = sizeof(struct tcmsg);
 
 static void
@@ -119,6 +123,12 @@ main(void)
 				  PRINT_FIELD_U(", ", est64, pps);
 				  printf("}"));
 # endif
+
+	static const uint64_t pkt64 = 0xdeadc0defacefeedULL;
+	TEST_NESTED_NLATTR_OBJECT(fd, nlh0, hdrlen,
+				  init_tcmsg, print_tcmsg,
+				  TCA_STATS_PKT64, pattern, pkt64,
+				  printf("16045693111314087661"));
 
 	puts("+++ exited with 0 +++");
 	return 0;
