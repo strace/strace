8e515c744935fe67e6a1b941f4c5414472c163b7
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-01-20 16:19:40 +0100

    tests: add strace-k-p test
    Add a check of the stack unwinding for attached processes.
    
    * tests/stack-fcall-attach.c: New file.
    * tests/strace-k-p.expected: Likewise.
    * tests/strace-k-p.test: New test.
    * tests/Makefile.am (EXTRA_DIST): Add strace-k-p.expected and strace-k-p.test.
    (STACKTRACE_TESTS): Add strace-k-p.test
    (check_PROGRAMS): Add stack-fcall-attach.
    (stack_fcall_attach_SOURCES): New variable.
    * tests/stack-fcall.c: Include "tests.h" and <unistd.h>.
    [!ATTACH_MODE] (ATTACH_MODE): Define to 0.
    (main) [ATTACH_MODE]: Wait a bit.
    * tests/strace-k.test: Add attach mode.
    
    Complements: v5.4-18-g69b2c33 "unwind-libdw: fix initialization of libdwfl cache"

--- tests/stack-fcall.c
+++ tests/stack-fcall.c
@@ -5,10 +5,21 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "tests.h"
+#include <unistd.h>
 #include "stack-fcall.h"
+
+#ifndef ATTACH_MODE
+# define ATTACH_MODE 0
+#endif
 
 int main(void)
 {
+#if ATTACH_MODE
+	/* sleep a bit to let the tracer time to catch up */
+	sleep(1);
+#endif
+
 	f0(0, (unsigned long) (void *) main);
 	f0(1, (unsigned long) (void *) main);
 	return 0;
