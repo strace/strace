afbbbdceb736182140337fb02c4e32171c8dd306
Dmitry V. Levin <ldv@altlinux.org>
2020-01-21 18:02:38 +0000

    io_uring: check struct io_* types automatically
    Move definitions of struct_io_sqring_offsets, struct_io_cqring_offsets,
    and struct_io_uring_params to a separate types/ header file.
    
    * configure.ac (AC_CHECK_TYPES): Remove struct io_sqring_offsets,
    struct io_cqring_offsets, and struct io_uring_params.
    (AC_CHECK_MEMBERS): Remove struct io_cqring_offsets.resv.
    * io_uring.c: Include "types/io_uring.h" instead of <linux/io_uring.h>.
    [HAVE_STRUCT_IO_CQRING_OFFSETS]: Remove static_assert checks of
    sizeof(struct io_sqring_offsets).
    (struct_io_sqring_offsets, struct_io_cqring_offsets,
    struct_io_uring_params): Move to ...
    * types/io_uring.h: ... new file.

---
+++ types/io_uring.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2019-2020 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef STRACE_TYPES_IO_URING_H
+# define STRACE_TYPES_IO_URING_H
+
+#ifdef HAVE_LINUX_IO_URING_H
+# include <linux/io_uring.h>
+#endif
+
+typedef struct {
+	uint32_t head;
+	uint32_t tail;
+	uint32_t ring_mask;
+	uint32_t ring_entries;
+	uint32_t flags;
+	uint32_t dropped;
+	uint32_t array;
+	uint32_t resv1;
+	uint64_t resv2;
+} struct_io_sqring_offsets;
+
+typedef struct {
+	uint32_t head;
+	uint32_t tail;
+	uint32_t ring_mask;
+	uint32_t ring_entries;
+	uint32_t overflow;
+	uint32_t cqes;
+	uint64_t resv[2];
+} struct_io_cqring_offsets;
+
+typedef struct {
+	uint32_t sq_entries;
+	uint32_t cq_entries;
+	uint32_t flags;
+	uint32_t sq_thread_cpu;
+	uint32_t sq_thread_idle;
+	uint32_t features;
+	uint32_t resv[4];
+	struct_io_sqring_offsets sq_off;
+	struct_io_cqring_offsets cq_off;
+} struct_io_uring_params;
+
+#endif /* !STRACE_TYPES_IO_URING_H */
