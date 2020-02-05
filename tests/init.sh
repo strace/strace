46442988ceebbe268d2dfd5a304435c22fe0534b
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-02-05 19:10:45 +0100

    tests: move getstr/getoption from strace-V.test to init.sh
    And rename to more generic get_config_str/get_config_option.
    
    * tests/strace-V.test (getstr, getoption): Rename to get_config_str
    and get_config_option, respectively.  All users updated.
    (get_config_str, get_config_option): Move ...
    * tests/init.sh: ... here.

--- tests/init.sh
+++ tests/init.sh
@@ -11,12 +11,40 @@ ME_="${0##*/}"
 LOG="log"
 OUT="out"
 EXP="exp"
+CONFIG_H="../../config.h"
 
 warn_() { printf >&2 '%s\n' "$*"; }
 fail_() { warn_ "$ME_: failed test: $*"; exit 1; }
 skip_() { warn_ "$ME_: skipped test: $*"; exit 77; }
 framework_failure_() { warn_ "$ME_: framework failure: $*"; exit 99; }
 framework_skip_() { warn_ "$ME_: framework skip: $*"; exit 77; }
+
+# get_config_str OPTION
+#
+# Returns the value of OPTION from config.h (path to which set
+# in the CONFIG_H variable).
+get_config_str()
+{
+	sed -r -n 's/#define[[:space:]]*'"$1"'[[:space:]]*"([^"]*)".*/\1/p' \
+		"$CONFIG_H"
+}
+
+# get_config_option OPTION YES_STRING [NO_STRING]
+#
+# Returns YES_STRING in case OPTION is enabled (present in config.h and has
+# a non-zero numeric value). Otherwise, NO_STRING (or empty string, if not
+# specified) is returned.
+get_config_option()
+{
+	local opt
+	opt=$(sed -r -n 's/#define[[:space:]]*'"$1"'[[:space:]]*([0-9]+)$/\1/p' \
+		"$CONFIG_H")
+	if [ -n "$opt" -a "$opt" -ne 0 ]; then
+		printf "%s" "$2"
+	else
+		printf "%s" "${3-}"
+	fi
+}
 
 check_prog()
 {
