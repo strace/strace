88cce1dd669a452a724a5775e3af01f3a6e57760
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-02-11 18:18:54 +0100

    strace: typo in usage (s/milliseconds/microseconds/)
    * strace.c (usage): s/milliseconds/microseconds/
    
    Fixes: v5.5~14 "strace: describe -e options in the help message"

--- strace.c
+++ strace.c
@@ -370,7 +370,7 @@ Tampering:\n\
   --inject=SET[:error=ERRNO|:retval=VALUE][:signal=SIG][:syscall=SYSCALL]\n\
            [:delay_enter=DELAY][:delay_exit=DELAY][:when=WHEN]\n\
                  perform syscall tampering for the syscalls in SET\n\
-     delay:      milliseconds or NUMBER{s|ms|us|ns}\n\
+     delay:      microseconds or NUMBER{s|ms|us|ns}\n\
      when:       FIRST, FIRST+, or FIRST+STEP\n\
   -e fault=SET[:error=ERRNO][:when=WHEN], --fault=SET[:error=ERRNO][:when=WHEN]\n\
                  synonym for -e inject with default ERRNO set to ENOSYS.\n\
