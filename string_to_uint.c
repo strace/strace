e1e451a0c2a87abfee98961a8d62cbe2b91e9ec8
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-03-18 02:46:29 +0100

    string_to_uint: implement string_to_bool

--- string_to_uint.c
+++ string_to_uint.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "macros.h"
+
 #include "string_to_uint.h"
 
 long long
@@ -40,4 +42,38 @@ string_to_uint_ex(const char *const str, char **const endptr,
 		*endptr = end;
 
 	return val;
+}
+
+int
+string_to_bool(const char *const str, const char *const accepted_ending)
+{
+#define _(s_) { s_, sizeof(s_) - 1 }
+	struct strsz {
+		const char *str;
+		size_t sz;
+	};
+	static const struct strsz true_strs[] =
+		{ _("y"), _("yes"), _("t"), _("true"), _("1") };
+	static const struct strsz false_strs[] =
+		{ _("n"), _("no"), _("f"), _("false"), _("0") };
+
+	for (size_t i = 0; i < ARRAY_SIZE(true_strs); i++) {
+		if (strncasecmp(str, true_strs[i].str, true_strs[i].sz))
+			continue;
+
+		if (!str[true_strs[i].sz] || (accepted_ending &&
+		    strchr(accepted_ending, str[true_strs[i].sz])))
+			return true;
+	}
+
+	for (size_t i = 0; i < ARRAY_SIZE(false_strs); i++) {
+		if (strncasecmp(str, false_strs[i].str, false_strs[i].sz))
+			continue;
+
+		if (!str[false_strs[i].sz] || (accepted_ending &&
+		    strchr(accepted_ending, str[false_strs[i].sz])))
+			return false;
+	}
+
+	return -1;
 }
--- string_to_uint.h
+++ string_to_uint.h
@@ -46,4 +46,20 @@ string_to_ulonglong(const char *str)
 	return string_to_uint_upto(str, LLONG_MAX);
 }
 
+/**
+ * Converts string to a boolean value.  Accepts "y", "yes", "t", "true", and "1"
+ * as true, "n", "no", "f", "false", and "0" as false (case-insensitive).
+ * str may be terminated with one of the characters provided in accepted_ending,
+ * instead of '\0'.
+ *
+ * @param str             String to convert to boolean value.
+ * @param accepted_ending If not NULL, used as a list of characters that can be
+ *                        possible string terminators apart from '\0'.
+ * @return                true if str has matched against one of true strings,
+ *                        false if str has matched against one of false strings,
+ *                        -1 if str hasn't been matched.
+ */
+extern int
+string_to_bool(const char *const str, const char *const accepted_ending);
+
 #endif /* !STRACE_STRING_TO_UINT_H */
