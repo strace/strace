b27e90a331d2989144118f475399705fee6a9486
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-03-20 04:55:58 +0100

    hdio: factor out the HDIO_GETGEO handler into a separate function
    And update it up to the current coding style.
    
    * hdio.c: Include "print_fields.h".
    (print_hdio_getgeo): New function.
    (hdio_ioctl) <case HDIO_GETGEO>: Call print_hdio_getgeo instead
    of decoding in place.

--- hdio.c
+++ hdio.c
@@ -17,26 +17,38 @@ typedef struct hd_geometry struct_hd_geometry;
 
 #include MPERS_DEFS
 
+#include "print_fields.h"
+
+static int
+print_hdio_getgeo(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	if (entering(tcp)) {
+		tprints(", ");
+
+		return 0;
+	}
+
+	/* exiting */
+	struct_hd_geometry geo;
+
+	if (umove_or_printaddr(tcp, arg, &geo))
+		return RVAL_IOCTL_DECODED;
+
+	PRINT_FIELD_U("{", geo, heads);
+	PRINT_FIELD_U(", ", geo, sectors);
+	PRINT_FIELD_U(", ", geo, cylinders);
+	PRINT_FIELD_U(", ", geo, start);
+	tprints("}");
+
+	return RVAL_IOCTL_DECODED;
+}
+
 MPERS_PRINTER_DECL(int, hdio_ioctl, struct tcb *const tcp,
 		   const unsigned int code, const kernel_ulong_t arg)
 {
 	switch (code) {
 	case HDIO_GETGEO:
-		if (entering(tcp))
-			return 0;
-		else {
-			struct_hd_geometry geo;
-
-			tprints(", ");
-			if (!umove_or_printaddr(tcp, arg, &geo))
-				tprintf("{heads=%u, sectors=%u, "
-					"cylinders=%hu, start=%lu}",
-					(unsigned) geo.heads,
-					(unsigned) geo.sectors,
-					geo.cylinders,
-					(unsigned long) geo.start);
-		}
-		break;
+		return print_hdio_getgeo(tcp, arg);
 	default:
 		return RVAL_DECODED;
 	}
