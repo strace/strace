2429c69961e2598902bded9c02dd601b362b66b4
Dmitry V. Levin <ldv@altlinux.org>
2020-03-29 22:50:54 +0000

    xtensa: switch to PTRACE_GETREGS API
    Implement PTRACE_GETREGS API introduced on xtensa by Linux kernel commit
    v2.6.13-rc1~68^2~366.
    
    * configure.ac [xtensa]: Remove arch_enable_Werror=no.
    * linux/xtensa/get_syscall_result.c: Remove.
    * Makefile.am (EXTRA_DIST): Remove it.
    * linux/xtensa/arch_regs.c (xtensa_regs): New variable.
    (ARCH_REGS_FOR_GETREGS, ARCH_PC_REG, ARCH_SP_REG): New macros.
    (xtensa_a2, ARCH_PC_PEEK_ADDR, ARCH_SP_PEEK_ADDR): Remove.
    * linux/xtensa/get_error.c (arch_get_error): Use xtensa_regs.a[2].
    * linux/xtensa/get_syscall_args.c (arch_get_syscall_args): Use
    xtensa_regs.a.
    * linux/xtensa/set_error.c (arch_set_error, arch_set_success): Use
    xtensa_regs.a[2].
    * NEWS: Mention this change.

--- linux/xtensa/get_error.c
+++ linux/xtensa/get_error.c
@@ -10,10 +10,10 @@
 static void
 arch_get_error(struct tcb *tcp, const bool check_errno)
 {
-	if (check_errno && is_negated_errno(xtensa_a2)) {
+	if (check_errno && is_negated_errno(xtensa_regs.a[2])) {
 		tcp->u_rval = -1;
-		tcp->u_error = -xtensa_a2;
+		tcp->u_error = -xtensa_regs.a[2];
 	} else {
-		tcp->u_rval = xtensa_a2;
+		tcp->u_rval = xtensa_regs.a[2];
 	}
 }
--- linux/xtensa/get_syscall_args.c
+++ linux/xtensa/get_syscall_args.c
@@ -9,19 +9,9 @@
 static int
 arch_get_syscall_args(struct tcb *tcp)
 {
-	/* arg0: a6, arg1: a3, arg2: a4, arg3: a5, arg4: a8, arg5: a9 */
-	static const int xtensaregs[MAX_ARGS] = {
-		REG_A_BASE + 6,
-		REG_A_BASE + 3,
-		REG_A_BASE + 4,
-		REG_A_BASE + 5,
-		REG_A_BASE + 8,
-		REG_A_BASE + 9
-	};
-	unsigned int i;
+	static const unsigned int syscall_regs[MAX_ARGS] = { 6, 3, 4, 5, 8, 9 };
 
-	for (i = 0; i < n_args(tcp); ++i)
-		if (upeek(tcp, xtensaregs[i], &tcp->u_arg[i]) < 0)
-			return -1;
+	for (unsigned int i = 0; i < n_args(tcp); ++i)
+		tcp->u_arg[i] = xtensa_regs.a[syscall_regs[i]];
 	return 1;
 }
