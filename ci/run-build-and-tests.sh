562a51ced9f8607e4b6e0c140ba80f65cc0970ae
Dmitry V. Levin <ldv@altlinux.org>
2020-04-05 22:28:10 +0000

    travis: update coverage configuration
    * .travis.yml [$CHECK == coverage]: Change dist from trusty to bionic,
    change the compiler from gcc to gcc-9, change STACKTRACE from libunwind
    to libdw.
    * ci/run-build-and-tests.sh [$CHECK == coverage] (GCOV): New variable,
    pass its value to ./codecov.bash.

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -94,7 +94,12 @@ case "${CHECK-}" in
 		echo 'BEGIN OF TEST SUITE INFORMATION'
 		tail -n 99999 -- tests*/test-suite.log tests*/ksysent.gen.log
 		echo 'END OF TEST SUITE INFORMATION'
-		./codecov.bash -Z -a -abc
+		case "$CC" in
+			gcc*) GCOV="gcov${CC#gcc}" ;;
+			clang*) GCOV="llvm-cov${CC#clang} gcov" ;;
+			*) GCOV=gcov ;;
+		esac
+		./codecov.bash -Z -x "$GCOV" -a -abc
 		;;
 	valgrind)
 		make -k $j all VERBOSE=${VERBOSE-}
