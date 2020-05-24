d1c89cd624ecd2a4157e2d2e34914d022b00a2ab
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-05-24 12:41:37 +0200

    pidfd_getfd: fix decoder
    The decoder incorrectly assumed the first argument being a pid,
    but actually it is a pidfd.
    
    The patch also makes the decoder be able to decode the targetfd
    (-y and -yy options).
    
    * pidfd_getfd.c: Include "number_set.h".
    (SYS_FUNC(pidfd_getfd)): Rewrite using pidfd_get_pid.
    
    Fixes: v5.6~46 "Implement pidfd_getfd syscall decoding"
    
    Signed-off-by: Ákos Uzonyi <uzonyi.akos@gmail.com>

--- pidfd_getfd.c
+++ pidfd_getfd.c
@@ -8,16 +8,24 @@
  */
 
 #include "defs.h"
+#include "number_set.h"
 
 SYS_FUNC(pidfd_getfd)
 {
-	/* int pidfd */
-	printfd(tcp, (int) tcp->u_arg[0]);
-	/* int fd */
+	int pidfd = (int) tcp->u_arg[0];
+	int targetfd = (int) tcp->u_arg[1];
+	unsigned int flags = (unsigned int) tcp->u_arg[2];
+
+	printfd(tcp, pidfd);
 	tprints(", ");
-	printfd_pid_tracee_ns(tcp, (int) tcp->u_arg[0], (int) tcp->u_arg[1]);
-	/* unsigned int flags */
-	tprintf(", %#x", (unsigned int) tcp->u_arg[2]);
+
+	pid_t target_pid = pidfd_get_pid(tcp->pid, pidfd);
+	if (target_pid > 0)
+		printfd_pid(tcp, target_pid, targetfd);
+	else
+		tprintf("%d", targetfd);
+
+	tprintf(", %#x", flags);
 
 	return RVAL_DECODED | RVAL_FD;
 }
