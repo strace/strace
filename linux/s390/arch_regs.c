d25f16572a57764f7ccb9ec4315b4e57b1ca4b15
Dmitry V. Levin <ldv@altlinux.org>
2020-06-02 08:00:00 +0000

    s390: use a more appropriate regset type
    * linux/s390/arch_regs.c (s390_regset): Change the type from
    struct user_regs_struct to s390_regs, the latter is the subset of
    struct user_regs_struct used by the kernel, it's also the type
    used on s390x.

--- linux/s390/arch_regs.c
+++ linux/s390/arch_regs.c
@@ -6,7 +6,7 @@
  */
 
 /* PTRACE_GETREGSET on S390 is available since linux v2.6.27. */
-static struct user_regs_struct s390_regset;
+static s390_regs s390_regset;
 #define ARCH_REGS_FOR_GETREGSET s390_regset
 #define ARCH_PC_REG s390_regset.psw.addr
 #define ARCH_SP_REG s390_regset.gprs[15]
