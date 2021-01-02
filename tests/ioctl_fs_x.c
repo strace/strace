91447a51d6b55d3f579f6536d8ad85dbf8deeff2
Dmitry V. Levin <ldv@strace.io>
2021-01-02 08:00:00 +0000

    tests: check decoding of FS_IOC_FSGETXATTR and FS_IOC_FSSETXATTR
    * configure.ac (AC_CHECK_MEMBERS): Check for
    struct fsxattr.fsx_cowextsize in <linux/fs.h>.
    * tests/ioctl_fs_x-success.c: New file.
    * tests/ioctl_fs_x-success-Xabbrev.c: Likewise.
    * tests/ioctl_fs_x-success-Xraw.c: Likewise.
    * tests/ioctl_fs_x-success-Xverbose.c: Likewise.
    * tests/Makefile.am (check_PROGRAMS): Add ioctl_fs_x-success,
    ioctl_fs_x-success-Xabbrev, ioctl_fs_x-success-Xraw,
    and ioctl_fs_x-success-Xverbose.
    * tests/.gitignore: Likewise.
    * tests/gen_tests.in (ioctl_fs_x-success, ioctl_fs_x-success-Xabbrev,
    ioctl_fs_x-success-Xraw, ioctl_fs_x-success-Xverbose): New entries.

--- tests/ioctl_fs_x.c
+++ tests/ioctl_fs_x.c
@@ -1,7 +1,7 @@
 /*
  * Check decoding of linux/fs.h 'X' ioctl commands.
  *
- * Copyright (c) 2020 The strace developers.
+ * Copyright (c) 2020-2021 The strace developers.
  * All rights reserved.
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
@@ -24,6 +24,17 @@ do_ioctl(kernel_ulong_t cmd, kernel_ulong_t arg)
 	int rc = ioctl(-1, cmd, arg);
 	errstr = sprintrc(rc);
 
+# ifdef INJECT_RETVAL
+	if (rc != INJECT_RETVAL)
+		error_msg_and_fail("Return value [%d] does not match"
+				   " expectations [%d]", rc, INJECT_RETVAL);
+
+	static char inj_errstr[4096];
+
+	snprintf(inj_errstr, sizeof(inj_errstr), "%s (INJECTED)", errstr);
+	errstr = inj_errstr;
+# endif
+
 	return rc;
 }
 
@@ -33,10 +44,39 @@ do_ioctl_ptr(kernel_ulong_t cmd, const void *arg)
 	return do_ioctl(cmd, (uintptr_t) arg);
 }
 
+# ifdef INJECT_RETVAL
+static void
+skip_ioctls(int argc, const char *argv[])
+{
+	if (argc < 2)
+		error_msg_and_fail("Usage: %s NUM_SKIP", argv[0]);
+
+	unsigned long num_skip = strtoul(argv[1], NULL, 0);
+
+	for (size_t i = 0; i < num_skip; ++i) {
+		int rc = ioctl(-1, FIFREEZE, 0);
+
+		printf("ioctl(-1, %s) = %s%s\n",
+		       XLAT_STR(FIFREEZE), sprintrc(rc),
+		       rc == INJECT_RETVAL ? " (INJECTED)" : "");
+
+		if (rc == INJECT_RETVAL)
+			return;
+	}
+
+	error_msg_and_fail("Issued %lu ioctl syscalls but failed"
+			   " to detect an injected return code %d",
+			   num_skip, INJECT_RETVAL);
+}
+# endif /* INJECT_RETVAL */
 
 int
-main(void)
+main(int argc, const char *argv[])
 {
+# ifdef INJECT_RETVAL
+	skip_ioctls(argc, argv);
+# endif
+
 	static const struct {
 		uint32_t cmd;
 		const char *str;
@@ -44,12 +84,6 @@ main(void)
 	} simple_cmds[] = {
 		{ ARG_STR(FIFREEZE), false },
 		{ ARG_STR(FITHAW), false },
-# ifdef FS_IOC_FSGETXATTR
-		{ ARG_STR(FS_IOC_FSGETXATTR), true },
-# endif
-# ifdef FS_IOC_FSSETXATTR
-		{ ARG_STR(FS_IOC_FSSETXATTR), true },
-# endif
 		{ _IO('X', 0xff), "_IOC(_IOC_NONE, 0x58, 0xff, 0)", true },
 	};
 
@@ -80,12 +114,18 @@ main(void)
 		}
 	}
 
-# if defined FITRIM
+# if defined FITRIM || defined HAVE_STRUCT_FSXATTR_FSX_COWEXTSIZE
 	static const struct {
 		uint32_t cmd;
 		const char *str;
 	} null_arg_cmds[] = {
+#  ifdef FITRIM
 		{ ARG_STR(FITRIM) },
+#  endif
+#  ifdef HAVE_STRUCT_FSXATTR_FSX_COWEXTSIZE
+		{ ARG_STR(FS_IOC_FSSETXATTR) },
+		{ ARG_STR(FS_IOC_FSGETXATTR) },
+#  endif
 	};
 
 	for (size_t i = 0; i < ARRAY_SIZE(null_arg_cmds); ++i) {
@@ -93,7 +133,8 @@ main(void)
 		printf("ioctl(-1, " XLAT_FMT ", NULL) = %s\n",
 		       XLAT_SEL(null_arg_cmds[i].cmd, null_arg_cmds[i].str),
 		       errstr);
-# endif /* FITRIM */
+	}
+# endif /* FITRIM || HAVE_STRUCT_FSXATTR_FSX_COWEXTSIZE */
 
 # ifdef FITRIM
 	TAIL_ALLOC_OBJECT_CONST_PTR(struct fstrim_range, p_range);
@@ -112,6 +153,103 @@ main(void)
 	       (uintmax_t) p_range->len, (uintmax_t) p_range->minlen,
 	       errstr);
 # endif /* FITRIM */
+
+# ifdef HAVE_STRUCT_FSXATTR_FSX_COWEXTSIZE
+	/* FS_IOC_FSSETXATTR */
+	TAIL_ALLOC_OBJECT_CONST_PTR(struct fsxattr, p_fsxattr);
+
+	do_ioctl_ptr(FS_IOC_FSSETXATTR, (char *) p_fsxattr + 1);
+	printf("ioctl(-1, %s, %p) = %s\n",
+	       XLAT_STR(FS_IOC_FSSETXATTR), (char *) p_fsxattr + 1, errstr);
+
+#  define   VALID_FSX_XFLAGS 0x8001fffb
+#  define INVALID_FSX_XFLAGS 0x7ffe0004
+
+	p_fsxattr->fsx_xflags = VALID_FSX_XFLAGS;
+	p_fsxattr->fsx_extsize = 0xdeadbeefU;
+	p_fsxattr->fsx_projid = 0xcafef00dU;
+	p_fsxattr->fsx_cowextsize = 0xbabec0deU;
+
+	do_ioctl_ptr(FS_IOC_FSSETXATTR, p_fsxattr);
+	printf("ioctl(-1, %s, {fsx_xflags=%s, fsx_extsize=%u, fsx_projid=%#x"
+	       ", fsx_cowextsize=%u}) = %s\n",
+	       XLAT_STR(FS_IOC_FSSETXATTR),
+	       XLAT_KNOWN(VALID_FSX_XFLAGS,
+			  "FS_XFLAG_REALTIME|FS_XFLAG_PREALLOC|"
+			  "FS_XFLAG_IMMUTABLE|FS_XFLAG_APPEND|"
+			  "FS_XFLAG_SYNC|FS_XFLAG_NOATIME|"
+			  "FS_XFLAG_NODUMP|FS_XFLAG_RTINHERIT|"
+			  "FS_XFLAG_PROJINHERIT|FS_XFLAG_NOSYMLINKS|"
+			  "FS_XFLAG_EXTSIZE|FS_XFLAG_EXTSZINHERIT|"
+			  "FS_XFLAG_NODEFRAG|FS_XFLAG_FILESTREAM|"
+			  "FS_XFLAG_DAX|FS_XFLAG_COWEXTSIZE|FS_XFLAG_HASATTR"),
+	       p_fsxattr->fsx_extsize,
+	       p_fsxattr->fsx_projid,
+	       p_fsxattr->fsx_cowextsize,
+	       errstr);
+
+	p_fsxattr->fsx_xflags = ~p_fsxattr->fsx_xflags;
+
+	do_ioctl_ptr(FS_IOC_FSSETXATTR, p_fsxattr);
+	printf("ioctl(-1, %s, {fsx_xflags=%s, fsx_extsize=%u, fsx_projid=%#x"
+	       ", fsx_cowextsize=%u}) = %s\n",
+	       XLAT_STR(FS_IOC_FSSETXATTR),
+	       XLAT_UNKNOWN(INVALID_FSX_XFLAGS, "FS_XFLAG_???"),
+	       p_fsxattr->fsx_extsize,
+	       p_fsxattr->fsx_projid,
+	       p_fsxattr->fsx_cowextsize,
+	       errstr);
+
+	/* FS_IOC_FSGETXATTR */
+	do_ioctl_ptr(FS_IOC_FSGETXATTR, (char *) p_fsxattr + 1);
+	printf("ioctl(-1, %s, %p) = %s\n",
+	       XLAT_STR(FS_IOC_FSGETXATTR), (char *) p_fsxattr + 1, errstr);
+
+	p_fsxattr->fsx_xflags = VALID_FSX_XFLAGS;
+	p_fsxattr->fsx_nextents = 0xfacefeedU;
+
+	if (do_ioctl_ptr(FS_IOC_FSGETXATTR, p_fsxattr) < 0) {
+		printf("ioctl(-1, %s, %p) = %s\n",
+		       XLAT_STR(FS_IOC_FSGETXATTR), p_fsxattr, errstr);
+	} else {
+		printf("ioctl(-1, %s, {fsx_xflags=%s, fsx_extsize=%u"
+		       ", fsx_nextents=%u, fsx_projid=%#x"
+		       ", fsx_cowextsize=%u}) = %s\n",
+		       XLAT_STR(FS_IOC_FSGETXATTR),
+		       XLAT_KNOWN(VALID_FSX_XFLAGS,
+				  "FS_XFLAG_REALTIME|FS_XFLAG_PREALLOC|"
+				  "FS_XFLAG_IMMUTABLE|FS_XFLAG_APPEND|"
+				  "FS_XFLAG_SYNC|FS_XFLAG_NOATIME|"
+				  "FS_XFLAG_NODUMP|FS_XFLAG_RTINHERIT|"
+				  "FS_XFLAG_PROJINHERIT|FS_XFLAG_NOSYMLINKS|"
+				  "FS_XFLAG_EXTSIZE|FS_XFLAG_EXTSZINHERIT|"
+				  "FS_XFLAG_NODEFRAG|FS_XFLAG_FILESTREAM|"
+				  "FS_XFLAG_DAX|FS_XFLAG_COWEXTSIZE|FS_XFLAG_HASATTR"),
+		       p_fsxattr->fsx_extsize,
+		       p_fsxattr->fsx_nextents,
+		       p_fsxattr->fsx_projid,
+		       p_fsxattr->fsx_cowextsize,
+		       errstr);
+	}
+
+	p_fsxattr->fsx_xflags = ~p_fsxattr->fsx_xflags;
+
+	if (do_ioctl_ptr(FS_IOC_FSGETXATTR, p_fsxattr) < 0) {
+		printf("ioctl(-1, %s, %p) = %s\n",
+		       XLAT_STR(FS_IOC_FSGETXATTR), p_fsxattr, errstr);
+	} else {
+		printf("ioctl(-1, %s, {fsx_xflags=%s, fsx_extsize=%u"
+		       ", fsx_nextents=%u, fsx_projid=%#x"
+		       ", fsx_cowextsize=%u}) = %s\n",
+		       XLAT_STR(FS_IOC_FSGETXATTR),
+		       XLAT_UNKNOWN(INVALID_FSX_XFLAGS, "FS_XFLAG_???"),
+		       p_fsxattr->fsx_extsize,
+		       p_fsxattr->fsx_nextents,
+		       p_fsxattr->fsx_projid,
+		       p_fsxattr->fsx_cowextsize,
+		       errstr);
+	}
+# endif /* HAVE_STRUCT_FSXATTR_FSX_COWEXTSIZE */
 
 	puts("+++ exited with 0 +++");
 	return 0;
