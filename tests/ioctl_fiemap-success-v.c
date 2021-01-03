e520ea58d3d8ceb1818442de240b8405b3642052
Dmitry V. Levin <ldv@strace.io>
2021-01-03 08:00:00 +0000

    tests: extend FS_IOC_FIEMAP decoding checks
    * tests/ioctl_fiemap.c: Rewrite.
    * tests/ioctl_fiemap-Xabbrev.c: New file.
    * tests/ioctl_fiemap-Xraw.c: Likewise.
    * tests/ioctl_fiemap-Xverbose.c: Likewise.
    * tests/ioctl_fiemap-success.c: Likewise.
    * tests/ioctl_fiemap-success-Xabbrev.c: Likewise.
    * tests/ioctl_fiemap-success-Xraw.c: Likewise.
    * tests/ioctl_fiemap-success-Xverbose.c: Likewise.
    * tests/ioctl_fiemap-success-v.c: Likewise.
    * tests/ioctl_fiemap-success-v-Xabbrev.c: Likewise.
    * tests/ioctl_fiemap-success-v-Xraw.c: Likewise.
    * tests/ioctl_fiemap-success-v-Xverbose.c: Likewise.
    * tests/gen_tests.in (ioctl_fiemap-Xabbrev, ioctl_fiemap-Xraw,
    ioctl_fiemap-Xverbose, ioctl_fiemap-success,
    ioctl_fiemap-success-Xabbrev, ioctl_fiemap-success-Xraw,
    ioctl_fiemap-success-Xverbose, ioctl_fiemap-success-v,
    ioctl_fiemap-success-v-Xabbrev, ioctl_fiemap-success-v-Xraw,
    ioctl_fiemap-success-v-Xverbose): New entries.
    * tests/Makefile.am (check_PROGRAMS): Add ioctl_fiemap-success,
    ioctl_fiemap-success-Xabbrev, ioctl_fiemap-success-Xraw,
    ioctl_fiemap-success-Xverbose, ioctl_fiemap-success-v,
    ioctl_fiemap-success-v-Xabbrev, ioctl_fiemap-success-v-Xraw,
    and ioctl_fiemap-success-v-Xverbose.
    * tests/pure_executables.list: Add ioctl_fiemap-Xabbrev,
    ioctl_fiemap-Xraw, and ioctl_fiemap-Xverbose.
    * tests/.gitignore: Add ioctl_fiemap-Xabbrev, ioctl_fiemap-Xraw,
    ioctl_fiemap-Xverbose, ioctl_fiemap-success,
    ioctl_fiemap-success-Xabbrev, ioctl_fiemap-success-Xraw,
    ioctl_fiemap-success-Xverbose, ioctl_fiemap-success-v,
    ioctl_fiemap-success-v-Xabbrev, ioctl_fiemap-success-v-Xraw,
    and ioctl_fiemap-success-v-Xverbose.

---
+++ tests/ioctl_fiemap-success-v.c
@@ -0,0 +1,2 @@
+#define VERBOSE 1
+#include "ioctl_fiemap-success.c"
--- tests/ioctl_fiemap.c
+++ tests/ioctl_fiemap.c
@@ -12,37 +12,179 @@
 #ifdef HAVE_LINUX_FIEMAP_H
 
 # include <stdio.h>
+# include <stdlib.h>
 # include <sys/ioctl.h>
 # include <linux/types.h>
 # include <linux/fiemap.h>
 # include <linux/fs.h>
-# include "xlat.h"
-# include "xlat/fiemap_flags.h"
 
-static void
-test_fiemap(void)
+static const char *errstr;
+
+static int
+do_ioctl(kernel_ulong_t cmd, kernel_ulong_t arg)
 {
-	(void) tail_alloc(1);
-	TAIL_ALLOC_OBJECT_CONST_PTR(struct fiemap, args);
+	int rc = ioctl(-1, cmd, arg);
+	errstr = sprintrc(rc);
 
-	printf("ioctl(-1, FS_IOC_FIEMAP, {fm_start=%" PRI__u64
-	       ", fm_length=%" PRI__u64", fm_flags=",
-	       args->fm_start, args->fm_length);
-	printflags(fiemap_flags, args->fm_flags, "FIEMAP_FLAG_???");
-	printf(", fm_extent_count=%u})", args->fm_extent_count);
-	ioctl(-1, FS_IOC_FIEMAP, args);
-	printf(" = -1 EBADF (%m)\n");
+# ifdef INJECT_RETVAL
+	if (rc != INJECT_RETVAL)
+		error_msg_and_fail("Return value [%d] does not match"
+				   " expectations [%d]", rc, INJECT_RETVAL);
 
-	/* The live version of this test is in btrfs.c */
+	static char inj_errstr[4096];
+
+	snprintf(inj_errstr, sizeof(inj_errstr), "%s (INJECTED)", errstr);
+	errstr = inj_errstr;
+# endif
+
+	return rc;
 }
 
-int
-main(void)
+static int
+do_ioctl_ptr(kernel_ulong_t cmd, const void *arg)
 {
-	test_fiemap();
+	return do_ioctl(cmd, (uintptr_t) arg);
+}
+
+# ifdef INJECT_RETVAL
+static void
+skip_ioctls(int argc, const char *argv[])
+{
+	if (argc < 2)
+		error_msg_and_fail("Usage: %s NUM_SKIP", argv[0]);
+
+	unsigned long num_skip = strtoul(argv[1], NULL, 0);
+
+	for (size_t i = 0; i < num_skip; ++i) {
+		int rc = ioctl(-1, FS_IOC_FIEMAP, 0);
+
+		printf("ioctl(-1, %s, NULL) = %s%s\n",
+		       XLAT_STR(FS_IOC_FIEMAP), sprintrc(rc),
+		       rc == INJECT_RETVAL ? " (INJECTED)" : "");
+
+		if (rc == INJECT_RETVAL)
+			return;
+	}
+
+	error_msg_and_fail("Issued %lu ioctl syscalls but failed"
+			   " to detect an injected return code %d",
+			   num_skip, INJECT_RETVAL);
+}
+# endif /* INJECT_RETVAL */
+
+int
+main(int argc, const char *argv[])
+{
+# ifdef INJECT_RETVAL
+	skip_ioctls(argc, argv);
+# endif
+
+	TAIL_ALLOC_OBJECT_VAR_PTR(struct fiemap, fiemap);
+
+	do_ioctl_ptr(FS_IOC_FIEMAP, (char *) fiemap + 1);
+	printf("ioctl(-1, %s, %p) = %s\n",
+	       XLAT_STR(FS_IOC_FIEMAP), (char *) fiemap + 1, errstr);
+
+# define   VALID_FM_FLAGS        0x7
+# define INVALID_FM_FLAGS 0xfffffff8
+
+	fiemap->fm_start = (typeof(fiemap->fm_start)) 0xdeadbeefcafef00dULL;
+	fiemap->fm_length = (typeof(fiemap->fm_length)) 0xfacefeedbabec0deULL;
+	fiemap->fm_flags = VALID_FM_FLAGS;
+	fiemap->fm_mapped_extents = 0xbadc0ded;
+	fiemap->fm_extent_count = 0xdeadc0de;
+
+	int rc = do_ioctl_ptr(FS_IOC_FIEMAP, fiemap);
+	printf("ioctl(-1, %s, {fm_start=%ju, fm_length=%ju"
+	       ", fm_flags=%s, fm_extent_count=%u}",
+	       XLAT_STR(FS_IOC_FIEMAP),
+	       (uintmax_t) fiemap->fm_start, (uintmax_t) fiemap->fm_length,
+	       XLAT_KNOWN(VALID_FM_FLAGS,
+			  "FIEMAP_FLAG_SYNC|FIEMAP_FLAG_XATTR|"
+			  "FIEMAP_FLAG_CACHE"),
+	       fiemap->fm_extent_count);
+	if (rc < 0) {
+		printf(") = %s\n", errstr);
+	} else {
+		printf(" => {fm_flags=%s, fm_mapped_extents=%u, ",
+		       XLAT_KNOWN(VALID_FM_FLAGS,
+				  "FIEMAP_FLAG_SYNC|FIEMAP_FLAG_XATTR|"
+				  "FIEMAP_FLAG_CACHE"),
+		       fiemap->fm_mapped_extents);
+# if VERBOSE
+		printf("fm_extents=%p", fiemap + 1);
+# else
+		printf("...");
+# endif
+		printf("}) = %s\n", errstr);
+	}
+
+# define   VALID_FE_FLAGS     0x3f8f
+# define INVALID_FE_FLAGS 0xffffc070
+
+	fiemap = tail_alloc(sizeof(*fiemap) + 2 * sizeof(fiemap->fm_extents[0]));
+	fiemap->fm_start = (typeof(fiemap->fm_start)) 0xdeadbeefcafef00dULL;
+	fiemap->fm_length = (typeof(fiemap->fm_length)) 0xfacefeedbabec0deULL;
+	fiemap->fm_flags = ~VALID_FM_FLAGS;
+	fiemap->fm_mapped_extents = 2;
+	fiemap->fm_extent_count = 0xdeadc0de;
+
+	fiemap->fm_extents[0].fe_logical = 0xfacefed1deadbef1;
+	fiemap->fm_extents[0].fe_physical = 0xfacefed2deadbef2;
+	fiemap->fm_extents[0].fe_length = 0xfacefed3deadbef3;
+	fiemap->fm_extents[0].fe_flags = VALID_FE_FLAGS;
+
+	fiemap->fm_extents[1].fe_logical = 0xfacefed1deadbef4;
+	fiemap->fm_extents[1].fe_physical = 0xfacefed2deadbef5;
+	fiemap->fm_extents[1].fe_length = 0xfacefed3deadbef6;
+	fiemap->fm_extents[1].fe_flags = ~VALID_FE_FLAGS;
+
+	rc = do_ioctl_ptr(FS_IOC_FIEMAP, fiemap);
+	printf("ioctl(-1, %s, {fm_start=%ju, fm_length=%ju"
+	       ", fm_flags=%s, fm_extent_count=%u}",
+	       XLAT_STR(FS_IOC_FIEMAP),
+	       (uintmax_t) fiemap->fm_start, (uintmax_t) fiemap->fm_length,
+	       XLAT_UNKNOWN(INVALID_FM_FLAGS, "FIEMAP_FLAG_???"),
+	       fiemap->fm_extent_count);
+	if (rc < 0) {
+		printf(") = %s\n", errstr);
+	} else {
+		printf(" => {fm_flags=%s, fm_mapped_extents=%u, ",
+		       XLAT_UNKNOWN(INVALID_FM_FLAGS, "FIEMAP_FLAG_???"),
+		       fiemap->fm_mapped_extents);
+# if VERBOSE
+		printf("fm_extents=[{fe_logical=%ju, fe_physical=%ju"
+		       ", fe_length=%ju, fe_flags=%s}"
+		       ", {fe_logical=%ju, fe_physical=%ju"
+		       ", fe_length=%ju, fe_flags=%s}]",
+		       (uintmax_t) fiemap->fm_extents[0].fe_logical,
+		       (uintmax_t) fiemap->fm_extents[0].fe_physical,
+		       (uintmax_t) fiemap->fm_extents[0].fe_length,
+		       XLAT_KNOWN(VALID_FE_FLAGS,
+			          "FIEMAP_EXTENT_LAST|"
+				  "FIEMAP_EXTENT_UNKNOWN|"
+				  "FIEMAP_EXTENT_DELALLOC|"
+				  "FIEMAP_EXTENT_ENCODED|"
+				  "FIEMAP_EXTENT_DATA_ENCRYPTED|"
+				  "FIEMAP_EXTENT_NOT_ALIGNED|"
+				  "FIEMAP_EXTENT_DATA_INLINE|"
+				  "FIEMAP_EXTENT_DATA_TAIL|"
+				  "FIEMAP_EXTENT_UNWRITTEN|"
+				  "FIEMAP_EXTENT_MERGED|"
+				  "FIEMAP_EXTENT_SHARED"),
+		       (uintmax_t) fiemap->fm_extents[1].fe_logical,
+		       (uintmax_t) fiemap->fm_extents[1].fe_physical,
+		       (uintmax_t) fiemap->fm_extents[1].fe_length,
+		       XLAT_UNKNOWN(INVALID_FE_FLAGS, "FIEMAP_EXTENT_???"));
+# else
+		printf("...");
+# endif
+		printf("}) = %s\n", errstr);
+	}
+
+	/* The live version of this test is in btrfs.c */
 
 	puts("+++ exited with 0 +++");
-
 	return 0;
 }
 
