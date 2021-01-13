c5ee3c575af0ddd14e83bf684c78b86ca7d6f548
Dmitry V. Levin <ldv@strace.io>
2021-01-13 08:00:00 +0000

    tests: cleanup chroot test
    * tests/chroot.c: Assume that __NR_chroot is defined, use sprintrc.
    * tests/gen_tests.in (chroot): Update.

--- tests/chroot.c
+++ tests/chroot.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of chroot syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,26 +10,19 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_chroot
-
-# include <stdio.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int
 main(void)
 {
 	static const char sample[] = "chroot.sample";
 
-	long rc = syscall(__NR_chroot, sample);
-	printf("chroot(\"%s\") = %ld %s (%m)\n",
-	       sample, rc, errno2name());
+	printf("chroot(NULL) = %s\n",
+	       sprintrc(syscall(__NR_chroot, NULL)));
+	printf("chroot(\"%s\") = %s\n",
+	       sample, sprintrc(syscall(__NR_chroot, sample)));
 
 	puts("+++ exited with 0 +++");
 	return 0;
 }
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_chroot")
-
-#endif
