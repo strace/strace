b246a31720c7347c84f63d5a6d6634fdd10662e3
Kent Gibson <warthog618@gmail.com>
2021-01-23 09:56:43 +0800

    Add GPIO v2 ioctl decoding
    Add decoding of GPIO uAPI v2 ioctls added in Linux v5.10.
    
    Signed-off-by: Kent Gibson <warthog618@gmail.com>
    
    * gpio_ioctl.c: Include "xlat/gpio_v2_line_flags.h" and
    "xlat/gpio_v2_line_attr_ids.h".
    (print_gpio_v2_line_attribute_raw, print_gpio_v2_line_attribute,
    print_gpio_v2_line_config_attribute,
    print_gpio_v2_line_attr_array_member,
    print_gpio_v2_line_config_attr_array_member, print_gpio_v2_line_config,
    print_gpio_v2_line_info, print_gpio_v2_line_request,
    print_gpio_v2_line_get_values, print_gpio_v2_line_set_values,
    print_gpio_v2_line_set_config): New functions.
    (gpio_ioctl): Use them.
    * types/gpio.h (GPIO_V2_LINES_MAX, GPIO_V2_LINE_NUM_ATTRS_MAX): New
    macros.
    (struct_gpio_v2_line_values, struct_gpio_v2_line_attribute,
    struct_gpio_v2_line_config_attribute, struct_gpio_v2_line_config,
    struct_gpio_v2_line_request, struct_gpio_v2_line_info): New types.
    * clat/gpio_ioctl_cmds.in (GPIO_V2_GET_LINEINFO_IOCTL,
    GPIO_V2_GET_LINEINFO_WATCH_IOCTL, GPIO_V2_GET_LINE_IOCTL,
    GPIO_V2_LINE_SET_CONFIG_IOCTL, GPIO_V2_LINE_GET_VALUES_IOCTL,
    GPIO_V2_LINE_SET_VALUES_IOCT): New constants.
    * xlat/gpio_v2_line_attr_ids.in: New file.
    * xlat/gpio_v2_line_flags.in: Likewise.
    
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>

--- gpio_ioctl.c
+++ gpio_ioctl.c
@@ -200,6 +200,221 @@ print_gpiohandle_set_config(struct tcb *const tcp, const kernel_ulong_t arg)
 	return RVAL_IOCTL_DECODED;
 }
 
+#include "xlat/gpio_v2_line_flags.h"
+#include "xlat/gpio_v2_line_attr_ids.h"
+
+static void
+print_gpio_v2_line_attribute_raw(const struct_gpio_v2_line_attribute *attr,
+				 bool as_field)
+{
+	if (as_field)
+		tprints("attr={");
+	PRINT_FIELD_U("", *attr, id);
+	if (attr->padding)
+		PRINT_FIELD_X(", ", *attr, padding);
+	tprintf(", data=%#" PRIx64, attr->values);
+	if (as_field)
+		tprints("}");
+}
+
+static void
+print_gpio_v2_line_attribute(const struct_gpio_v2_line_attribute *attr,
+			     bool as_field)
+{
+	if (attr->padding) {
+		/* unexpected padding usage so decode fields raw */
+		print_gpio_v2_line_attribute_raw(attr, as_field);
+		return;
+	}
+	switch (attr->id) {
+	case GPIO_V2_LINE_ATTR_ID_FLAGS:
+		PRINT_FIELD_FLAGS("", *attr, flags, gpio_v2_line_flags,
+				  "GPIO_V2_LINE_FLAG_???");
+		break;
+	case GPIO_V2_LINE_ATTR_ID_OUTPUT_VALUES:
+		PRINT_FIELD_X("", *attr, values);
+		break;
+	case GPIO_V2_LINE_ATTR_ID_DEBOUNCE:
+		PRINT_FIELD_U("", *attr, debounce_period_us);
+		break;
+	default:
+		/* unknown id so decode fields raw */
+		print_gpio_v2_line_attribute_raw(attr, as_field);
+		break;
+	}
+}
+
+static void
+print_gpio_v2_line_config_attribute(const struct_gpio_v2_line_config_attribute *attr)
+{
+	tprints("{");
+	print_gpio_v2_line_attribute(&attr->attr, true);
+	PRINT_FIELD_X(", ", *attr, mask);
+	tprints("}");
+}
+
+static bool
+print_gpio_v2_line_attr_array_member(struct tcb *tcp, void *elem_buf,
+				     size_t elem_size, void *data)
+{
+	tprints("{");
+	print_gpio_v2_line_attribute(elem_buf, false);
+	tprints("}");
+
+	return true;
+}
+
+static bool
+print_gpio_v2_line_config_attr_array_member(struct tcb *tcp, void *elem_buf,
+					    size_t elem_size, void *data)
+{
+	print_gpio_v2_line_config_attribute(elem_buf);
+
+	return true;
+}
+
+static void
+print_gpio_v2_line_config(struct tcb *const tcp,
+			  const struct_gpio_v2_line_config *lc)
+{
+	PRINT_FIELD_FLAGS("{", *lc, flags, gpio_v2_line_flags,
+			  "GPIO_V2_LINE_FLAG_???");
+	PRINT_FIELD_U(", ", *lc, num_attrs);
+	if (!IS_ARRAY_ZERO(lc->padding))
+		PRINT_FIELD_X_ARRAY(", ", *lc, padding);
+	if (lc->num_attrs)
+		PRINT_FIELD_ARRAY_UPTO(", ", *lc, attrs, lc->num_attrs, tcp,
+				       print_gpio_v2_line_config_attr_array_member);
+	tprints("}");
+}
+
+static int
+print_gpio_v2_line_info(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct_gpio_v2_line_info li;
+
+	if (entering(tcp))
+		tprints(", ");
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprints(" => ");
+
+	if (umove_or_printaddr(tcp, arg, &li))
+		return RVAL_IOCTL_DECODED;
+
+	if (entering(tcp)) {
+		PRINT_FIELD_U("{", li, offset);
+		tprints("}");
+		return 0;
+	}
+
+	/* exiting */
+	PRINT_FIELD_CSTRING("{", li, name);
+	PRINT_FIELD_CSTRING(", ", li, consumer);
+	PRINT_FIELD_FLAGS(", ", li, flags, gpio_v2_line_flags, "GPIO_V2_LINE_FLAG_???");
+	PRINT_FIELD_U(", ", li, num_attrs);
+	if (li.num_attrs)
+		PRINT_FIELD_ARRAY_UPTO(", ", li, attrs, li.num_attrs, tcp,
+				       print_gpio_v2_line_attr_array_member);
+	if (!IS_ARRAY_ZERO(li.padding))
+		PRINT_FIELD_X_ARRAY(", ", li, padding);
+	tprints("}");
+
+	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_gpio_v2_line_request(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct_gpio_v2_line_request lr;
+
+	if (entering(tcp))
+		tprints(", ");
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprints(" => ");
+
+	if (umove_or_printaddr(tcp, arg, &lr))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		PRINT_FIELD_FD("{", lr, fd, tcp);
+		tprints("}");
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	PRINT_FIELD_U("{", lr, num_lines);
+	PRINT_FIELD_ARRAY_UPTO(", ", lr, offsets, lr.num_lines, tcp,
+			       print_uint32_array_member);
+	PRINT_FIELD_OBJ_TCB_PTR(", ", lr, config, tcp,
+				print_gpio_v2_line_config);
+	PRINT_FIELD_CSTRING(", ", lr, consumer);
+	if (lr.event_buffer_size)
+		PRINT_FIELD_U(", ", lr, event_buffer_size);
+	if (!IS_ARRAY_ZERO(lr.padding))
+		PRINT_FIELD_X_ARRAY(", ", lr, padding);
+	tprints("}");
+	return 0;
+}
+
+static int
+print_gpio_v2_line_get_values(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct_gpio_v2_line_values vals;
+
+	if (entering(tcp))
+		tprints(", ");
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprints(" => ");
+
+	if (umove_or_printaddr(tcp, arg, &vals))
+		return RVAL_IOCTL_DECODED;
+
+	if (entering(tcp)) {
+		PRINT_FIELD_X("{", vals, mask);
+		tprints("}");
+		return 0;
+	}
+
+	/* exiting */
+	PRINT_FIELD_X("{", vals, bits);
+	tprints("}");
+
+	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_gpio_v2_line_set_values(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct_gpio_v2_line_values vals;
+
+	tprints(", ");
+	if (!umove_or_printaddr(tcp, arg, &vals)) {
+		PRINT_FIELD_X("{", vals, bits);
+		PRINT_FIELD_X(", ", vals, mask);
+		tprints("}");
+	}
+
+	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_gpio_v2_line_set_config(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct_gpio_v2_line_config lc;
+
+	tprints(", ");
+	if (!umove_or_printaddr(tcp, arg, &lc))
+		print_gpio_v2_line_config(tcp, &lc);
+
+	return RVAL_IOCTL_DECODED;
+}
+
 int
 gpio_ioctl(struct tcb *const tcp, const unsigned int code,
 	   const kernel_ulong_t arg)
@@ -209,6 +424,17 @@ gpio_ioctl(struct tcb *const tcp, const unsigned int code,
 		return print_gpiochip_info(tcp, arg);
 	case GPIO_GET_LINEINFO_UNWATCH_IOCTL:
 		return print_gpioline_info_unwatch(tcp, arg);
+	case GPIO_V2_GET_LINEINFO_IOCTL:
+	case GPIO_V2_GET_LINEINFO_WATCH_IOCTL:
+		return print_gpio_v2_line_info(tcp, arg);
+	case GPIO_V2_GET_LINE_IOCTL:
+		return print_gpio_v2_line_request(tcp, arg);
+	case GPIO_V2_LINE_SET_CONFIG_IOCTL:
+		return print_gpio_v2_line_set_config(tcp, arg);
+	case GPIO_V2_LINE_GET_VALUES_IOCTL:
+		return print_gpio_v2_line_get_values(tcp, arg);
+	case GPIO_V2_LINE_SET_VALUES_IOCTL:
+		return print_gpio_v2_line_set_values(tcp, arg);
 	case GPIO_GET_LINEINFO_IOCTL:
 	case GPIO_GET_LINEINFO_WATCH_IOCTL:
 		return print_gpioline_info(tcp, arg);
--- types/gpio.h
+++ types/gpio.h
@@ -22,6 +22,14 @@
 #  define GPIOHANDLES_MAX 64
 # endif
 
+# ifndef GPIO_V2_LINES_MAX
+#  define GPIO_V2_LINES_MAX 64
+# endif
+
+# ifndef GPIO_V2_LINE_NUM_ATTRS_MAX
+#  define GPIO_V2_LINE_NUM_ATTRS_MAX 10
+# endif
+
 typedef struct {
 	char name[GPIO_MAX_NAME_SIZE];
 	char label[GPIO_MAX_NAME_SIZE];
@@ -61,5 +69,52 @@ typedef struct {
 	uint8_t default_values[GPIOHANDLES_MAX];
 	uint32_t padding[4];
 } struct_gpiohandle_config;
+
+typedef struct {
+	uint64_t bits;
+	uint64_t mask;
+} struct_gpio_v2_line_values;
+
+typedef struct {
+	uint32_t id;
+	uint32_t padding;
+	union {
+		uint32_t debounce_period_us;
+		uint64_t flags;
+		uint64_t values;
+	};
+} struct_gpio_v2_line_attribute;
+
+typedef struct {
+	struct_gpio_v2_line_attribute attr;
+	uint64_t mask;
+} struct_gpio_v2_line_config_attribute;
+
+typedef struct {
+	uint64_t flags;
+	uint32_t num_attrs;
+	uint32_t padding[5];
+	struct_gpio_v2_line_config_attribute attrs[GPIO_V2_LINE_NUM_ATTRS_MAX];
+} struct_gpio_v2_line_config;
+
+typedef struct {
+	uint32_t offsets[GPIO_V2_LINES_MAX];
+	char consumer[GPIO_MAX_NAME_SIZE];
+	struct_gpio_v2_line_config config;
+	uint32_t num_lines;
+	uint32_t event_buffer_size;
+	uint32_t padding[5];
+	int32_t fd;
+} struct_gpio_v2_line_request;
+
+typedef struct {
+	char name[GPIO_MAX_NAME_SIZE];
+	char consumer[GPIO_MAX_NAME_SIZE];
+	uint32_t offset;
+	uint32_t num_attrs;
+	uint64_t flags;
+	struct_gpio_v2_line_attribute attrs[GPIO_V2_LINE_NUM_ATTRS_MAX];
+	uint32_t padding[4];
+} struct_gpio_v2_line_info;
 
 #endif /* STRACE_TYPES_GPIO_H */
