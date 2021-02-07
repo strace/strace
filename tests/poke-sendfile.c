ec50e5235f8d86f2c7182ed855a7fa3e5b667fcf
Elvira Khabirova <lineprinter0@gmail.com>
2021-02-07 10:08:49 +0300

    tests: check poke injection functionality
    * tests/poke.test: New test.
    * tests/poke-ptrace.test: Likewise.
    * tests/poke-range.test: Likewise.
    * tests/poke-unaligned.test: Likewise.
    * tests/poke-range.expected: New file.
    * tests/poke-unaligned.expected: Likewise.
    * tests/poke.c: New file.
    * tests/poke-sendfile.c: Likewise.
    * tests/gen_tests.in (poke-sendfile): New entry.
    (trace_personality_*): Skip poke executable.
    * tests/pure_executables.list: Add poke and poke-sendfile.
    * tests/.gitignore: Likewise.
    (EXTRA_DIST): Add poke-range.expected and poke-unaligned.expected.
    (MISC_TESTS): Add poke.test, poke-ptrace.test, poke-range.test,
    and poke-unaligned.test.
    * tests/qual_inject-syntax.test: Invoke with -efu,
    add checks for inject=SET:poke_{enter,exit} expression syntax.
    
    Co-authored-by: Dmitry V. Levin <ldv@altlinux.org>

---
+++ tests/poke-sendfile.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "tests.h"
+#include "scno.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+int
+main(void)
+{
+	int sv[2];
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sv))
+		perror_msg_and_skip("socketpair");
+
+	unsigned int file_size = 0;
+	socklen_t optlen = sizeof(file_size);
+	if (getsockopt(sv[1], SOL_SOCKET, SO_SNDBUF, &file_size, &optlen))
+		perror_msg_and_fail("getsockopt");
+	file_size /= 4;
+
+	int reg_in = create_tmpfile(O_RDWR);
+	if (ftruncate(reg_in, file_size))
+		perror_msg_and_fail("ftruncate(%d, %u)", reg_in, file_size);
+
+	TAIL_ALLOC_OBJECT_CONST_PTR(uint64_t, p_off);
+	*p_off = 0xcafef00dfacefeedULL;
+
+#ifdef __NR_sendfile64
+	syscall(__NR_sendfile64, sv[1], reg_in, p_off, file_size);
+	static const char name[] = "sendfile64";
+	static const unsigned long long expected = -1ULL;
+#else
+	syscall(__NR_sendfile, sv[1], reg_in, p_off, file_size);
+	static const char name[] = "sendfile";
+	static const unsigned long long expected = (kernel_ulong_t) -1ULL;
+#endif
+
+	printf("%s(%d, %d, [0] => [%llu], %u) = %u\n",
+	       name, sv[1], reg_in, expected, file_size, file_size);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
---
+++ tests/poke.c
@@ -0,0 +1,107 @@
+/*
+ * Check poke injection.
+ *
+ * Copyright (c) 2021 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "tests.h"
+#include "scno.h"
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+	FILE *exp_err = NULL;
+
+	if (argc > 1) {
+		exp_err = fopen(argv[1], "w");
+		if (!exp_err)
+			perror_msg_and_fail("fopen: %s", argv[1]);
+		setvbuf(exp_err, NULL, _IONBF, 0);
+	}
+
+	pid_t pid = getpid();
+	char *const chdir_buf = tail_alloc(PATH_MAX);
+	memset(chdir_buf, '/', PATH_MAX);
+	char *const getcwd_buf = tail_alloc(PATH_MAX);
+
+	/*
+	 * regular poke on entering syscall
+	 */
+	char *p = chdir_buf;
+	printf("chdir(\"%.*s\") = %s\n",
+	       PATH_MAX - 1, p, sprintrc(chdir(p)));
+
+	/*
+	 * poke at inaccessible address
+	 */
+	p += PATH_MAX;
+	printf("chdir(%p) = %s\n", p, sprintrc(chdir(p)));
+	if (exp_err) {
+		fprintf(exp_err,
+			".*: Failed to tamper with process %d: couldn't poke\n",
+			pid);
+	}
+
+	/*
+	 * poke at unaligned address,
+	 * short read,
+	 * if process_vm_writev is used: short write.
+	 */
+	--p;
+	printf("chdir(%p) = %s\n", p, sprintrc(chdir(p)));
+	if (exp_err) {
+		fprintf(exp_err,
+			".*: short read \\(1 < [[:digit:]]+\\) @%p: .*\n", p);
+		if (*p != '/') {
+			fprintf(exp_err,
+				".*: pid:%d short write"
+				" \\(1 < [[:digit:]]+\\) @%p\n",
+				pid, p);
+		}
+	}
+
+	/*
+	 * poke at a properly aligned address,
+	 * short read,
+	 * if process_vm_writev is not used: short write,
+	 * if process_vm_writev is used: short write is likely.
+	 */
+	p -= 7;
+	printf("chdir(%p) = %s\n", p, sprintrc(chdir(p)));
+	if (exp_err) {
+		fprintf(exp_err,
+			".*: short read \\(8 < [[:digit:]]+\\) @%p: .*\n", p);
+		if (*p != '/') {
+			fprintf(exp_err,
+				".*: pid:%d short write"
+				" \\(8 < [[:digit:]]+\\) @%p(: .*)?\n",
+				pid, p);
+		}
+	}
+
+	/*
+	 * regular poke on exiting syscall
+	 */
+	p = getcwd_buf;
+	long res = syscall(__NR_getcwd, p, PATH_MAX);
+	if (res <= 0)
+		perror_msg_and_fail("getcwd");
+
+	printf("getcwd(");
+	print_quoted_string(p);
+	printf(", %u) = %ld\n", PATH_MAX, res);
+
+	if (exp_err)
+		fclose(exp_err);
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
