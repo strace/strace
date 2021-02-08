2b0944e48798c11797770c07ca90435f26f3506f
Dmitry V. Levin <ldv@strace.io>
2021-02-08 08:00:00 +0000

    Merge src/linux/arch_defs_.h into src/arch_defs.h
    * src/arch_defs.h: Do not include "linux/arch_defs_.h" but import its
    contents.
    * src/linux/arch_defs_.h: Turn into an empty placeholder file.

--- src/arch_defs.h
+++ src/arch_defs.h
@@ -1,15 +1,84 @@
 /*
  * Copyright (c) 2018 Dmitry V. Levin <ldv@strace.io>
+ * Copyright (c) 2018-2021 The strace developers.
  * All rights reserved.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
-/* Architecture-specific definitions.  */
 #ifndef STRACE_ARCH_DEFS_H
 # define STRACE_ARCH_DEFS_H
 
 # include "arch_defs_.h"
-# include "linux/arch_defs_.h"
+
+/* Fallbacks for architecture-specific definitions.  */
+
+# ifndef HAVE_ARCH_GETRVAL2
+#  define HAVE_ARCH_GETRVAL2 0
+# endif
+
+# ifndef HAVE_ARCH_OLD_MMAP
+#  define HAVE_ARCH_OLD_MMAP 0
+# endif
+
+# ifndef HAVE_ARCH_OLD_MMAP_PGOFF
+#  define HAVE_ARCH_OLD_MMAP_PGOFF 0
+# endif
+
+# ifndef HAVE_ARCH_OLD_SELECT
+#  define HAVE_ARCH_OLD_SELECT 0
+# endif
+
+# ifndef HAVE_ARCH_UID16_SYSCALLS
+#  define HAVE_ARCH_UID16_SYSCALLS 0
+# endif
+
+# ifndef DEFAULT_PERSONALITY
+#  define DEFAULT_PERSONALITY 0
+# endif
+
+# ifndef SUPPORTED_PERSONALITIES
+#  define SUPPORTED_PERSONALITIES 1
+# endif
+
+# ifndef HAVE_ARCH_DEDICATED_ERR_REG
+#  define HAVE_ARCH_DEDICATED_ERR_REG 0
+# endif
+
+# ifndef CAN_ARCH_BE_COMPAT_ON_64BIT_KERNEL
+#  define CAN_ARCH_BE_COMPAT_ON_64BIT_KERNEL 0
+# endif
+
+# ifndef ARCH_NEEDS_SET_ERROR_FOR_SCNO_TAMPERING
+#  define ARCH_NEEDS_SET_ERROR_FOR_SCNO_TAMPERING 0
+# endif
+
+# ifndef MIN_WORDSIZE
+#  if SUPPORTED_PERSONALITIES > 1
+#   define MIN_WORDSIZE 4
+#  else
+#   define MIN_WORDSIZE SIZEOF_LONG
+#  endif
+# endif
+
+# ifndef HAVE_ARCH_TIME32_SYSCALLS
+#  define HAVE_ARCH_TIME32_SYSCALLS (MIN_WORDSIZE == 4)
+# endif
+
+# ifndef HAVE_ARCH_OLD_TIME64_SYSCALLS
+#  define HAVE_ARCH_OLD_TIME64_SYSCALLS (SIZEOF_LONG == 8)
+# endif
+
+# ifndef MIN_KLONGSIZE
+#  if SUPPORTED_PERSONALITIES > 1
+#   define MIN_KLONGSIZE 4
+#  else
+#   define MIN_KLONGSIZE SIZEOF_KERNEL_LONG_T
+#  endif
+# endif
+
+# ifndef HAVE_ARCH_TIMESPEC32
+#  define HAVE_ARCH_TIMESPEC32 (MIN_KLONGSIZE == 4)
+# endif
 
 #endif /* !STRACE_ARCH_DEFS_H */
