3dabc8fb0628cfafc089af36aca4fe279af4b3f6
Dmitry V. Levin <ldv@strace.io>
2021-02-09 08:00:00 +0000

    xlat: update UFFD_* constants
    * src/xlat/uffd_flags.in (UFFD_USER_MODE_ONLY): New constant introduced
    by Linux kernel commit v5.11-rc1~170^2~21.
    * NEWS: Mention this change.
    * tests/userfaultfd.c: Check it.
    * tests/gen_tests.in (userfaultfd): Update -a option argument.

--- tests/userfaultfd.c
+++ tests/userfaultfd.c
@@ -15,12 +15,48 @@
 # include <stdio.h>
 # include <unistd.h>
 
+# define UFFD_USER_MODE_ONLY 1
+
+static const char *errstr;
+
+static long
+k_userfaultfd(const unsigned int flags)
+{
+	const kernel_ulong_t fill = (kernel_ulong_t) 0xdefaced00000000ULL;
+	const kernel_ulong_t bad = (kernel_ulong_t) 0xbadc0dedbadc0dedULL;
+	const kernel_ulong_t arg1 = fill | flags;
+	const long rc = syscall(__NR_userfaultfd, arg1, bad, bad, bad, bad, bad);
+	errstr = sprintrc(rc);
+	return rc;
+}
+
 int
 main(void)
 {
-	long rc = syscall(__NR_userfaultfd, 2 | O_NONBLOCK | O_CLOEXEC);
-	printf("userfaultfd(O_NONBLOCK|O_CLOEXEC|0x2) = %ld %s (%m)\n",
-	       rc, errno2name());
+	struct {
+		unsigned int val;
+		const char *str;
+	} flags[] = {
+		{ ARG_STR(0) },
+		{ ARG_STR(O_NONBLOCK) },
+		{ ARG_STR(O_CLOEXEC) },
+		{ ARG_STR(O_NONBLOCK|O_CLOEXEC) },
+		{ ARG_STR(UFFD_USER_MODE_ONLY) },
+		{ ARG_STR(UFFD_USER_MODE_ONLY|O_NONBLOCK) },
+		{ ARG_STR(UFFD_USER_MODE_ONLY|O_CLOEXEC) },
+		{ ARG_STR(UFFD_USER_MODE_ONLY|O_NONBLOCK|O_CLOEXEC) },
+	};
+
+	for (unsigned int i = 0; i < ARRAY_SIZE(flags); ++i) {
+		k_userfaultfd(flags[i].val);
+		printf("userfaultfd(%s) = %s\n", flags[i].str, errstr);
+	}
+
+	k_userfaultfd(-1U);
+	printf("userfaultfd(UFFD_USER_MODE_ONLY|O_NONBLOCK|O_CLOEXEC|%#x)"
+	       " = %s\n", (-1U - UFFD_USER_MODE_ONLY - O_NONBLOCK - O_CLOEXEC),
+	       errstr);
+
 	puts("+++ exited with 0 +++");
 	return 0;
 }
