7834365668d21fc5ea25e387a6a6f1e271f84298
Dmitry V. Levin <ldv@strace.io>
2021-02-23 08:00:00 +0000

    tests: switch to use bundled <linux/stat.h>
    * bundled/linux/include/uapi/linux/stat.h: New file, copied from
    headers_install'ed Linux kernel v5.11.
    * bundled/Makefile.am (EXTRA_DIST): Add it.
    * configure.ac (AC_CHECK_TYPES): Remove struct statx.
    * tests/statx.c [!HAVE_STRUCT_STATX]: Remove.

--- tests/statx.c
+++ tests/statx.c
@@ -8,43 +8,35 @@
 #include "tests.h"
 #include "scno.h"
 
-#if defined __NR_statx && defined HAVE_STRUCT_STATX
+#include <linux/stat.h>
+#include "xlat.h"
+#include "xlat/statx_masks.h"
+#include "xlat/statx_attrs.h"
+#include "xlat/at_statx_sync_types.h"
 
-# include <linux/stat.h>
-# include "xlat.h"
-# include "xlat/statx_masks.h"
-# include "xlat/statx_attrs.h"
-# include "xlat/at_statx_sync_types.h"
-
-# define IS_STATX 1
-# define TEST_SYSCALL_STR "statx"
-# define STRUCT_STAT struct statx
-# define STRUCT_STAT_STR "struct statx"
-# define STRUCT_STAT_IS_STAT64 0
+#define IS_STATX 1
+#define TEST_SYSCALL_STR "statx"
+#define STRUCT_STAT struct statx
+#define STRUCT_STAT_STR "struct statx"
+#define STRUCT_STAT_IS_STAT64 0
 
 static unsigned    TEST_SYSCALL_STATX_FLAGS     = AT_STATX_SYNC_AS_STAT;
 static const char *TEST_SYSCALL_STATX_FLAGS_STR = "AT_STATX_SYNC_AS_STAT";
 static unsigned    TEST_SYSCALL_STATX_MASK      = STATX_ALL;
 static const char *TEST_SYSCALL_STATX_MASK_STR  = "STATX_ALL";
 
-# define TEST_SYSCALL_INVOKE(sample, pst) \
+#define TEST_SYSCALL_INVOKE(sample, pst) \
 	syscall(__NR_statx, AT_FDCWD, sample, TEST_SYSCALL_STATX_FLAGS, \
 		TEST_SYSCALL_STATX_MASK, pst)
-# define PRINT_SYSCALL_HEADER(sample) \
+#define PRINT_SYSCALL_HEADER(sample) \
 	do { \
 		int saved_errno = errno; \
 		printf("%s(AT_FDCWD, \"%s\", %s, %s, ", \
 		       TEST_SYSCALL_STR, sample, TEST_SYSCALL_STATX_FLAGS_STR, \
 		       TEST_SYSCALL_STATX_MASK_STR)
-# define PRINT_SYSCALL_FOOTER(rc) \
+#define PRINT_SYSCALL_FOOTER(rc) \
 		errno = saved_errno; \
 		printf(") = %s\n", sprintrc(rc)); \
 	} while (0)
 
-# include "xstatx.c"
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_statx && HAVE_STRUCT_STATX")
-
-#endif
+#include "xstatx.c"
