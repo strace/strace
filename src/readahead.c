0a9d9f8bd9c919787e904f4815d2553efaa1a499
Dmitry V. Levin <ldv@strace.io>
2021-03-01 08:00:00 +0000

    readahead: use print_fields.h helpers
    * src/readahead.c (SYS_FUNC(readahead)): Use tprint_arg_next and
    PRINT_VAL_U.

--- src/readahead.c
+++ src/readahead.c
@@ -9,11 +9,16 @@
 
 SYS_FUNC(readahead)
 {
-	int argn;
-
+	/* fd */
 	printfd(tcp, tcp->u_arg[0]);
-	argn = printllval(tcp, ", %lld", 1);
-	tprintf(", %" PRI_klu, tcp->u_arg[argn]);
+	tprint_arg_next();
+
+	/* offset */
+	int argn = printllval(tcp, "%lld", 1);
+	tprint_arg_next();
+
+	/* count */
+	PRINT_VAL_U(tcp->u_arg[argn]);
 
 	return RVAL_DECODED;
 }
