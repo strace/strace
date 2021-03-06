90f5243bc15517500349e61b9ecd9f74214f165b
Dmitry V. Levin <ldv@strace.io>
2021-03-06 08:00:00 +0000

    fstatfs64: use print_fields.h helpers
    * src/fstatfs64.c (SYS_FUNC(fstatfs64)): Use tprint_arg_next and
    PRINT_VAL_U.

--- src/fstatfs64.c
+++ src/fstatfs64.c
@@ -10,9 +10,15 @@
 SYS_FUNC(fstatfs64)
 {
 	if (entering(tcp)) {
+		/* fd */
 		printfd(tcp, tcp->u_arg[0]);
-		tprintf(", %" PRI_klu ", ", tcp->u_arg[1]);
+		tprint_arg_next();
+
+		/* size */
+		PRINT_VAL_U(tcp->u_arg[1]);
+		tprint_arg_next();
 	} else {
+		/* buf */
 		print_struct_statfs64(tcp, tcp->u_arg[2], tcp->u_arg[1]);
 	}
 	return 0;
