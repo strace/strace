5fc97a3c9a94ed06ac12992b3555ce1642e60a2d
Dmitry V. Levin <ldv@strace.io>
2021-03-08 08:00:00 +0000

    getcwd: use print_fields.h helpers
    * src/getcwd.c (SYS_FUNC(getcwd)): Use tprint_arg_next and PRINT_VAL_U.

--- src/getcwd.c
+++ src/getcwd.c
@@ -10,11 +10,15 @@
 SYS_FUNC(getcwd)
 {
 	if (exiting(tcp)) {
+		/* buf */
 		if (syserror(tcp))
 			printaddr(tcp->u_arg[0]);
 		else
 			printpathn(tcp, tcp->u_arg[0], tcp->u_rval - 1);
-		tprintf(", %" PRI_klu, tcp->u_arg[1]);
+		tprint_arg_next();
+
+		/* size */
+		PRINT_VAL_U(tcp->u_arg[1]);
 	}
 	return 0;
 }
