f65297bd7f90786cdd0eb53368a33b6f6a0cec13
Dmitry V. Levin <ldv@strace.io>
2021-03-12 08:00:00 +0000

    mq: use print_fields.h helpers
    * src/mq.c (SYS_FUNC(mq_open), do_mq_timedsend, do_mq_timedreceive,
    SYS_FUNC(mq_notify), SYS_FUNC(mq_getsetattr)): Use tprint_arg_next
    and PRINT_VAL_U.

--- src/mq.c
+++ src/mq.c
@@ -12,15 +12,21 @@
 
 SYS_FUNC(mq_open)
 {
+	/* name */
 	printpath(tcp, tcp->u_arg[0]);
-	tprints(", ");
+	tprint_arg_next();
+
 	/* flags */
 	tprint_open_modes(tcp->u_arg[1]);
+
 	if (tcp->u_arg[1] & O_CREAT) {
+		tprint_arg_next();
+
 		/* mode */
-		tprints(", ");
 		print_numeric_umode_t(tcp->u_arg[2]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* attr */
 		printmqattr(tcp, tcp->u_arg[3], false);
 	}
 	return RVAL_DECODED | RVAL_FD;
@@ -29,12 +35,25 @@ SYS_FUNC(mq_open)
 static int
 do_mq_timedsend(struct tcb *const tcp, const print_obj_by_addr_fn print_ts)
 {
+	/* mqdes */
 	printfd(tcp, tcp->u_arg[0]);
-	tprints(", ");
+	tprint_arg_next();
+
+	/* msg_ptr */
 	printstrn(tcp, tcp->u_arg[1], tcp->u_arg[2]);
-	tprintf(", %" PRI_klu ", %u, ", tcp->u_arg[2],
-		(unsigned int) tcp->u_arg[3]);
+	tprint_arg_next();
+
+	/* msg_len */
+	PRINT_VAL_U(tcp->u_arg[2]);
+	tprint_arg_next();
+
+	/* msg_prio */
+	PRINT_VAL_U((unsigned int) tcp->u_arg[3]);
+	tprint_arg_next();
+
+	/* abs_timeout */
 	print_ts(tcp, tcp->u_arg[4]);
+
 	return RVAL_DECODED;
 }
 
@@ -54,22 +73,31 @@ static int
 do_mq_timedreceive(struct tcb *const tcp, const print_obj_by_addr_fn print_ts)
 {
 	if (entering(tcp)) {
+		/* mqdes */
 		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
+		tprint_arg_next();
 	} else {
+		/* msg_ptr */
 		if (syserror(tcp))
 			printaddr(tcp->u_arg[1]);
 		else
 			printstrn(tcp, tcp->u_arg[1], tcp->u_rval);
-		tprintf(", %" PRI_klu ", ", tcp->u_arg[2]);
+		tprint_arg_next();
+
+		/* msg_len */
+		PRINT_VAL_U(tcp->u_arg[2]);
+		tprint_arg_next();
+
 		printnum_int(tcp, tcp->u_arg[3], "%u");
-		tprints(", ");
+		tprint_arg_next();
+
 		/*
 		 * Since the timeout parameter is read by the kernel
 		 * on entering syscall, it has to be decoded the same way
 		 * whether the syscall has failed or not.
 		 */
 		temporarily_clear_syserror(tcp);
+		/* abs_timeout */
 		print_ts(tcp, tcp->u_arg[4]);
 		restore_cleared_syserror(tcp);
 	}
@@ -90,8 +118,11 @@ SYS_FUNC(mq_timedreceive_time64)
 
 SYS_FUNC(mq_notify)
 {
+	/* mqdes */
 	printfd(tcp, tcp->u_arg[0]);
-	tprints(", ");
+	tprint_arg_next();
+
+	/* sevp */
 	print_sigevent(tcp, tcp->u_arg[1]);
 	return RVAL_DECODED;
 }
@@ -99,11 +130,15 @@ SYS_FUNC(mq_notify)
 SYS_FUNC(mq_getsetattr)
 {
 	if (entering(tcp)) {
+		/* mqdes */
 		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* newattr */
 		printmqattr(tcp, tcp->u_arg[1], true);
-		tprints(", ");
+		tprint_arg_next();
 	} else {
+		/* oldattr */
 		printmqattr(tcp, tcp->u_arg[2], true);
 	}
 	return 0;
