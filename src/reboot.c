ad61ff9ca3144d15595e48d80d20bb86da86eca4
Dmitry V. Levin <ldv@strace.io>
2021-03-14 08:00:00 +0000

    reboot: use print_fields.h helpers
    * src/reboot.c (SYS_FUNC(reboot)): Use tprint_arg_next.

--- src/reboot.c
+++ src/reboot.c
@@ -17,18 +17,25 @@ SYS_FUNC(reboot)
 	const unsigned int magic2 = tcp->u_arg[1];
 	const unsigned int cmd = tcp->u_arg[2];
 
+	/* magic */
 	printxval(bootflags1, magic1, "LINUX_REBOOT_MAGIC_???");
-	tprints(", ");
+	tprint_arg_next();
+
+	/* magic2 */
 	printxval(bootflags2, magic2, "LINUX_REBOOT_MAGIC_???");
-	tprints(", ");
+	tprint_arg_next();
+
+	/* cmd */
 	printxval(bootflags3, cmd, "LINUX_REBOOT_CMD_???");
 	if (cmd == LINUX_REBOOT_CMD_RESTART2) {
-		tprints(", ");
+		tprint_arg_next();
+
 		/*
 		 * The size of kernel buffer is 256 bytes and
 		 * the last byte is always zero, at most 255 bytes
 		 * are copied from the user space.
 		 */
+		/* arg */
 		printstr_ex(tcp, tcp->u_arg[3], 255, QUOTE_0_TERMINATED);
 	}
 	return RVAL_DECODED;
