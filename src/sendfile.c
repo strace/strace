c30e3469bcc22d3352d47a837ebe282187c5ba53
Dmitry V. Levin <ldv@strace.io>
2021-03-15 08:00:00 +0000

    sendfile: use print_fields.h helpers
    * src/sendfile.c (SYS_FUNC(sendfile64), SYS_FUNC(sendfile)): Use
    tprint_arg_next and PRINT_VAL_U.

--- src/sendfile.c
+++ src/sendfile.c
@@ -11,20 +11,33 @@
 SYS_FUNC(sendfile64)
 {
 	if (entering(tcp)) {
+		/* out_fd */
 		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* in_fd */
 		printfd(tcp, tcp->u_arg[1]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* offset */
 		if (!printnum_int64(tcp, tcp->u_arg[2], "%" PRIu64)) {
-			tprintf(", %" PRI_klu, tcp->u_arg[3]);
+			tprint_arg_next();
+
+			/* count */
+			PRINT_VAL_U(tcp->u_arg[3]);
 			return RVAL_DECODED;
 		}
 	} else {
 		if (!syserror(tcp) && tcp->u_rval) {
 			tprint_value_changed();
+
+			/* offset */
 			printnum_int64(tcp, tcp->u_arg[2], "%" PRIu64);
 		}
-		tprintf(", %" PRI_klu, tcp->u_arg[3]);
+		tprint_arg_next();
+
+		/* count */
+		PRINT_VAL_U(tcp->u_arg[3]);
 	}
 
 	return 0;
@@ -33,21 +46,34 @@ SYS_FUNC(sendfile64)
 SYS_FUNC(sendfile)
 {
 	if (entering(tcp)) {
+		/* out_fd */
 		printfd(tcp, tcp->u_arg[0]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* in_fd */
 		printfd(tcp, tcp->u_arg[1]);
-		tprints(", ");
+		tprint_arg_next();
+
+		/* offset */
 		if (!printnum_ulong(tcp, tcp->u_arg[2])
 		    || !tcp->u_arg[3]) {
-			tprintf(", %" PRI_klu, tcp->u_arg[3]);
+			tprint_arg_next();
+
+			/* count */
+			PRINT_VAL_U(tcp->u_arg[3]);
 			return RVAL_DECODED;
 		}
 	} else {
 		if (!syserror(tcp) && tcp->u_rval) {
 			tprint_value_changed();
+
+			/* offset */
 			printnum_ulong(tcp, tcp->u_arg[2]);
 		}
-		tprintf(", %" PRI_klu, tcp->u_arg[3]);
+		tprint_arg_next();
+
+		/* count */
+		PRINT_VAL_U(tcp->u_arg[3]);
 	}
 
 	return 0;
