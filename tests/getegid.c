5b41b9decfdb61f47ea4d98c838731813663971f
Dmitry V. Levin <ldv@strace.io>
2021-06-02 08:23:00 +0000

    tests: assume __NR_getegid is defined
    getegid syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/getegid.c: Assume __NR_getegid is defined.

--- tests/getegid.c
+++ tests/getegid.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of getegid syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,10 +10,8 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_getegid
-
-# include <stdio.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int
 main(void)
@@ -19,9 +19,3 @@ main(void)
 	printf("getegid() = %s\n", sprintrc(syscall(__NR_getegid)));
 	return 0;
 }
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_getegid")
-
-#endif
