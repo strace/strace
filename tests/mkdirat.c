fb0964494e5f2e595a22ac1dc6ce67efd312ca94
Dmitry V. Levin <ldv@strace.io>
2021-06-02 08:42:00 +0000

    tests: assume __NR_mkdirat is defined
    mkdirat syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/mkdirat.c: Assume __NR_mkdirat is defined.

--- tests/mkdirat.c
+++ tests/mkdirat.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of mkdirat syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,16 +10,9 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_mkdirat
+#define TEST_SYSCALL_NR			__NR_mkdirat
+#define TEST_SYSCALL_STR		"mkdirat"
+#define TEST_SYSCALL_PREFIX_ARGS	(long int) 0xdeadbeefffffffffULL,
+#define TEST_SYSCALL_PREFIX_STR		"-1, "
 
-# define TEST_SYSCALL_NR		__NR_mkdirat
-# define TEST_SYSCALL_STR		"mkdirat"
-# define TEST_SYSCALL_PREFIX_ARGS	(long int) 0xdeadbeefffffffffULL,
-# define TEST_SYSCALL_PREFIX_STR	"-1, "
-# include "umode_t.c"
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_mkdirat")
-
-#endif
+#include "umode_t.c"
