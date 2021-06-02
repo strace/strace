981da9477ac09ff4ef84f142d299ef25ad1fae4c
Dmitry V. Levin <ldv@strace.io>
2021-06-02 08:40:00 +0000

    tests: assume __NR_memfd_create is defined
    memfd_create syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/memfd_create.c: Assume __NR_memfd_create is defined.

--- tests/memfd_create.c
+++ tests/memfd_create.c
@@ -11,12 +11,10 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_memfd_create
-
-# include <stdio.h>
-# include <stdint.h>
-# include <unistd.h>
-# include <linux/memfd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <linux/memfd.h>
 
 static const char *errstr;
 
@@ -40,64 +38,58 @@ main(void)
 	       (int) size, pattern, errstr);
 
 	kernel_ulong_t flags = (kernel_ulong_t) 0xfacefeed00000007ULL;
-# define flags1_str "MFD_CLOEXEC|MFD_ALLOW_SEALING|MFD_HUGETLB"
+#define flags1_str "MFD_CLOEXEC|MFD_ALLOW_SEALING|MFD_HUGETLB"
 
 	k_memfd_create((uintptr_t) pattern, flags);
-# if XLAT_VERBOSE
+#if XLAT_VERBOSE
 	printf("memfd_create(\"%.*s\"..., %s /* %s */) = %s\n",
 	       (int) size, pattern,
 	       "0x7", flags1_str, errstr);
-# else
+#else
 	printf("memfd_create(\"%.*s\"..., %s) = %s\n",
 	       (int) size, pattern,
-#  if XLAT_RAW
+# if XLAT_RAW
 	       "0x7",
-#  else
+# else
 	       flags1_str,
-#  endif
-	       errstr);
 # endif
+	       errstr);
+#endif
 
 	pattern[size - 1] = '\0';
 	flags = 30 << MFD_HUGE_SHIFT;
 	k_memfd_create((uintptr_t) pattern, flags);
-# if XLAT_RAW
+#if XLAT_RAW
 	printf("memfd_create(\"%s\", %#x) = %s\n",
 	       pattern, (unsigned int) flags, errstr);
-# elif XLAT_VERBOSE
+#elif XLAT_VERBOSE
 	printf("memfd_create(\"%s\", %#x /* %s */) = %s\n",
 	       pattern, (unsigned int) flags, "30<<MFD_HUGE_SHIFT", errstr);
-# else /* XLAT_ABBREV */
+#else /* XLAT_ABBREV */
 	printf("memfd_create(\"%s\", 30<<MFD_HUGE_SHIFT) = %s\n",
 	       pattern, errstr);
-# endif
+#endif
 
 	flags = (kernel_ulong_t) -1ULL;
 	k_memfd_create(0, flags);
 	flags = -1U & ~(7 | (MFD_HUGE_MASK << MFD_HUGE_SHIFT));
 
-# define memfd_create_fmt "%s|%#x|%u<<MFD_HUGE_SHIFT"
+#define memfd_create_fmt "%s|%#x|%u<<MFD_HUGE_SHIFT"
 
 	printf("memfd_create(NULL, "
-# if XLAT_RAW
+#if XLAT_RAW
 	       "0xffffffff) = %s\n",
-# else
-#  if XLAT_VERBOSE
+#else
+# if XLAT_VERBOSE
 	       "0xffffffff /* " memfd_create_fmt " */"
-#  else /* XLAT_ABBREV */
+# else /* XLAT_ABBREV */
 	       memfd_create_fmt
-#  endif
+# endif
 	       ") = %s\n",
 	       flags1_str, (unsigned int) flags, MFD_HUGE_MASK,
-# endif
+#endif
 	       errstr);
 
 	puts("+++ exited with 0 +++");
 	return 0;
 }
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_memfd_create")
-
-#endif
