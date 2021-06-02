7cf1474130e35e1f655ec847df31ab17ace8c549
Dmitry V. Levin <ldv@strace.io>
2021-06-02 09:17:00 +0000

    tests: assume __NR_setfsgid is defined
    setfsgid syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/setfsgid.c: Assume __NR_setfsgid is defined.

--- tests/setfsgid.c
+++ tests/setfsgid.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of setfsgid syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,23 +10,15 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_setfsgid
+#define SYSCALL_NR	__NR_setfsgid
+#define SYSCALL_NAME	"setfsgid"
 
-# define SYSCALL_NR	__NR_setfsgid
-# define SYSCALL_NAME	"setfsgid"
-
-# if defined __NR_setfsgid32 && __NR_setfsgid != __NR_setfsgid32
-#  define UGID_TYPE	short
-#  define GETUGID	syscall(__NR_getegid)
-# else
-#  define UGID_TYPE	int
-#  define GETUGID	getegid()
-# endif
-
-# include "setfsugid.c"
-
+#if defined __NR_setfsgid32 && __NR_setfsgid != __NR_setfsgid32
+# define UGID_TYPE	short
+# define GETUGID	syscall(__NR_getegid)
 #else
-
-SKIP_MAIN_UNDEFINED("__NR_setfsgid")
-
+# define UGID_TYPE	int
+# define GETUGID	getegid()
 #endif
+
+#include "setfsugid.c"
