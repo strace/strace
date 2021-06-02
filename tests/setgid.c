9ef6cb756bb631898b194d0933b5477c76c6aa95
Dmitry V. Levin <ldv@strace.io>
2021-06-02 09:19:00 +0000

    tests: assume __NR_setgid is defined
    setgid syscall entries are present in syscallent files for all supported
    architectures.
    
    * tests/setgid.c: Assume __NR_setgid is defined.

--- tests/setgid.c
+++ tests/setgid.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of setgid syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,25 +10,17 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_setgid
+#define SYSCALL_NR	__NR_setgid
+#define SYSCALL_NAME	"setgid"
 
-# define SYSCALL_NR	__NR_setgid
-# define SYSCALL_NAME	"setgid"
-
-# if defined __NR_setgid32 && __NR_setgid != __NR_setgid32
-#  define UGID_TYPE	short
-#  define GETUGID	syscall(__NR_getegid)
-#  define CHECK_OVERFLOWUGID(arg)	check_overflowgid(arg)
-# else
-#  define UGID_TYPE	int
-#  define GETUGID	getegid()
-#  define CHECK_OVERFLOWUGID(arg)
-# endif
+#if defined __NR_setgid32 && __NR_setgid != __NR_setgid32
+# define UGID_TYPE	short
+# define GETUGID	syscall(__NR_getegid)
+# define CHECK_OVERFLOWUGID(arg)	check_overflowgid(arg)
+#else
+# define UGID_TYPE	int
+# define GETUGID	getegid()
+# define CHECK_OVERFLOWUGID(arg)
+#endif
 
 # include "setugid.c"
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_setgid")
-
-#endif
