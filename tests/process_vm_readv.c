08c12a2bcd07f9bcd0a1c1238186c798178425ea
Dmitry V. Levin <ldv@strace.io>
2021-06-02 08:56:00 +0000

    tests: assume __NR_process_vm_readv is defined
    process_vm_readv syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/process_vm_readv.c: Assume __NR_process_vm_readv is defined.

--- tests/process_vm_readv.c
+++ tests/process_vm_readv.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of process_vm_readv syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -6,20 +8,11 @@
  */
 
 #include "tests.h"
-
 #include "scno.h"
 
-#ifdef __NR_process_vm_readv
+#define OP     process_vm_readv
+#define OP_NR  __NR_process_vm_readv
+#define OP_STR "process_vm_readv"
+#define OP_WR  0
 
-# define OP     process_vm_readv
-# define OP_NR  __NR_process_vm_readv
-# define OP_STR "process_vm_readv"
-# define OP_WR  0
-
-# include "process_vm_readv_writev.c"
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_process_vm_readv");
-
-#endif
+#include "process_vm_readv_writev.c"
