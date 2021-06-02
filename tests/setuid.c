b4fc730a3a3c3f7fe60fb797f71c91c4d6687b88
Dmitry V. Levin <ldv@strace.io>
2021-06-02 09:25:00 +0000

    tests: assume __NR_setuid is defined
    setuid syscall entries are present in syscallent files for all supported
    architectures.
    
    * tests/setuid.c: Assume __NR_setuid is defined.

--- tests/setuid.c
+++ tests/setuid.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of setuid syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,25 +10,17 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_setuid
+#define SYSCALL_NR	__NR_setuid
+#define SYSCALL_NAME	"setuid"
 
-# define SYSCALL_NR	__NR_setuid
-# define SYSCALL_NAME	"setuid"
-
-# if defined __NR_setuid32 && __NR_setuid != __NR_setuid32
-#  define UGID_TYPE	short
-#  define GETUGID	syscall(__NR_geteuid)
-#  define CHECK_OVERFLOWUGID(arg)	check_overflowuid(arg)
-# else
-#  define UGID_TYPE	int
-#  define GETUGID	geteuid()
-#  define CHECK_OVERFLOWUGID(arg)
-# endif
-
-# include "setugid.c"
-
+#if defined __NR_setuid32 && __NR_setuid != __NR_setuid32
+# define UGID_TYPE	short
+# define GETUGID	syscall(__NR_geteuid)
+# define CHECK_OVERFLOWUGID(arg)	check_overflowuid(arg)
 #else
-
-SKIP_MAIN_UNDEFINED("__NR_setuid")
-
+# define UGID_TYPE	int
+# define GETUGID	geteuid()
+# define CHECK_OVERFLOWUGID(arg)
 #endif
+
+#include "setugid.c"
