5d647742761c7515f656ec03c17b3b39f97edac6
Dmitry V. Levin <ldv@strace.io>
2021-06-02 09:36:00 +0000

    tests: assume __NR_uname is defined
    uname syscall entries are present in syscallent files for all supported
    architectures.
    
    * tests/uname.c: Assume __NR_uname is defined.

--- tests/uname.c
+++ tests/uname.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of uname syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,11 +10,9 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_uname
-
-# include <stdio.h>
-# include <sys/utsname.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <sys/utsname.h>
+#include <unistd.h>
 
 int main(int ac, char **av)
 {
@@ -32,19 +32,13 @@ int main(int ac, char **av)
 		print_quoted_string(uname->version);
 		printf(", machine=");
 		print_quoted_string(uname->machine);
-# ifdef HAVE_STRUCT_UTSNAME_DOMAINNAME
+#ifdef HAVE_STRUCT_UTSNAME_DOMAINNAME
 		printf(", domainname=");
 		print_quoted_string(uname->domainname);
-# endif
+#endif
 	}
 	printf("}) = %d\n", rc);
 
 	puts("+++ exited with 0 +++");
 	return 0;
 }
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_uname")
-
-#endif
