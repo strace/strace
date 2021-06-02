361d877158e020497ec3af70f3c3105240d64361
Dmitry V. Levin <ldv@strace.io>
2021-06-02 09:12:00 +0000

    tests: assume __NR_sched_yield is defined
    sched_yield syscall entries are present in syscallent files for all
    supported architectures.
    
    * tests/sched_yield.c: Assume __NR_sched_yield is defined.

--- tests/sched_yield.c
+++ tests/sched_yield.c
@@ -1,4 +1,6 @@
 /*
+ * Check decoding of sched_yield syscall.
+ *
  * Copyright (c) 2016-2019 The strace developers.
  * All rights reserved.
  *
@@ -8,10 +10,8 @@
 #include "tests.h"
 #include "scno.h"
 
-#ifdef __NR_sched_yield
-
-# include <stdio.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int
 main(void)
@@ -21,9 +21,3 @@ main(void)
 	puts("+++ exited with 0 +++");
 	return 0;
 }
-
-#else
-
-SKIP_MAIN_UNDEFINED("__NR_sched_yield")
-
-#endif
