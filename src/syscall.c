d74413095a3c4c063902d043cc25d422d9edac07
Dmitry V. Levin <ldv@strace.io>
2022-01-09 08:00:00 +0000

    Update copyright headers
    Headers updated automatically using maint/update_copyright_years.sh
    script.

--- src/syscall.c
+++ src/syscall.c
@@ -6,7 +6,7 @@
  * Copyright (c) 1999 IBM Deutschland Entwicklung GmbH, IBM Corporation
  *                     Linux for s390 port by D.J. Barrow
  *                    <barrow_dj@mail.yahoo.com,djbarrow@de.ibm.com>
- * Copyright (c) 1999-2021 The strace developers.
+ * Copyright (c) 1999-2022 The strace developers.
  * All rights reserved.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
