8e6552b13ca98b493cacbb3717b75d616b9261f2
Eugene Syromyatnikov <evgsyr@gmail.com>
2022-01-28 21:58:47 +0100

    lirc_ioctl: use xlat for features decoding;  Also, put the print the indirection-related syntax

--- src/lirc_ioctl.c
+++ src/lirc_ioctl.c
@@ -7,23 +7,7 @@
 #include "defs.h"
 #include <linux/lirc.h>
 
-static const struct features {
-	int flag;
-	const char *name;
-} features[] = {
-	{ LIRC_CAN_REC_SCANCODE, "LIRC_CAN_REC_SCANCODE" },
-	{ LIRC_CAN_REC_MODE2, "LIRC_CAN_REC_MODE2" },
-	{ LIRC_CAN_GET_REC_RESOLUTION, "LIRC_CAN_GET_REC_RESOLUTION" },
-	{ LIRC_CAN_SEND_PULSE, "LIRC_CAN_SEND_PULSE" },
-	{ LIRC_CAN_SET_TRANSMITTER_MASK, "LIRC_CAN_SET_TRANSMITTER_MASK" },
-	{ LIRC_CAN_SET_SEND_CARRIER, "LIRC_CAN_SET_SEND_CARRIER" },
-	{ LIRC_CAN_SET_SEND_DUTY_CYCLE, "LIRC_CAN_SET_SEND_DUTY_CYCLE" },
-	{ LIRC_CAN_SET_REC_CARRIER, "LIRC_CAN_SET_REC_CARRIER" },
-	{ LIRC_CAN_SET_REC_CARRIER_RANGE, "LIRC_CAN_SET_REC_CARRIER_RANGE" },
-	{ LIRC_CAN_USE_WIDEBAND_RECEIVER, "LIRC_CAN_USE_WIDEBAND_RECEIVER" },
-	{ LIRC_CAN_MEASURE_CARRIER, "LIRC_CAN_MEASURE_CARRIER" },
-	{ LIRC_CAN_SET_REC_TIMEOUT, "LIRC_CAN_SET_REC_TIMEOUT" },
-};
+#include "xlat/lirc_features.h"
 
 int
 lirc_ioctl(struct tcb *const tcp, const unsigned int code,
@@ -38,22 +22,13 @@ lirc_ioctl(struct tcb *const tcp, const unsigned int code,
 		return RVAL_IOCTL_DECODED;
 
 	switch (code) {
-	case LIRC_GET_FEATURES: {
-		bool needs_or = false;
-		unsigned long i;
-
-		tprintf(", features=");
-
-		for (i = 0; i < ARRAY_SIZE(features); i++) {
-			if (value & features[i].flag) {
-				if (needs_or)
-					tprintf("|");
-				tprintf("%s", features[i].name);
-				needs_or = true;
-			}
-		}
+	case LIRC_GET_FEATURES:
+		tprint_arg_next();
+		tprint_indirect_begin();
+		printflags(lirc_features, value, "LIRC_CAN_???");
+		tprint_indirect_end();
 		break;
-	}
+
 	case LIRC_GET_SEND_MODE:
 	case LIRC_GET_REC_MODE:
 	case LIRC_SET_SEND_MODE:
