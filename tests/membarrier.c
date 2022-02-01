dadd6b4a7c678a40840602b0be2410934f6f8d0f
Sven Schnelle <svens@linux.ibm.com>
2022-02-01 12:40:26 +0100

    tests/membarrier: add additional return value
    linux commit 809232619f5b ("sched/membarrier: Fix membarrier-rseq fence
    command missing from query bitmask") changed the returned value, so we
    need to adjust the strace test as well.
    
    Signed-off-by: Sven Schnelle <svens@linux.ibm.com>

--- tests/membarrier.c
+++ tests/membarrier.c
@@ -72,6 +72,14 @@ main(void)
 			       "MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_RSEQ";
 			break;
 
+		case 2|4|8|16|128|256:
+			text = "MEMBARRIER_CMD_GLOBAL_EXPEDITED|"
+			       "MEMBARRIER_CMD_REGISTER_GLOBAL_EXPEDITED|"
+			       "MEMBARRIER_CMD_PRIVATE_EXPEDITED|"
+			       "MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED|"
+			       "MEMBARRIER_CMD_PRIVATE_EXPEDITED_RSEQ|"
+			       "MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_RSEQ";
+			break;
 		default:
 			error_msg_and_fail("membarrier returned %#x, does"
 					   " the test have to be updated?", rc);
