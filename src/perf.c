e6caf76ddcbeeb405084e53866ef64c2554abf46
Sven Schnelle <svens@linux.ibm.com>
2023-03-09 13:52:54 +0100

    Add config3 field to struct perf_event_attr
    Linux kernel commit 09519ec3b19 ("perf: Add perf_event_attr::config3")
    aka v6.3-rc1~29^2~13 added a new config3 field to struct
    perf_event_attr.  Add this field and implement its decoding in strace,
    which also fixes compilation of tests because of an array out-of-bounds
    warning in tests/perf_event.c.
    
    Signed-off-by: Sven Schnelle <svens@linux.ibm.com>
    
    * bundled/linux/include/uapi/linux/perf_event.h: Update to
    headers_install'ed Linux kernel v6.3-rc1.
    * src/perf_event_struct.h (struct perf_event_attr): Add config3 field.
    * src/perf.c (print_perf_event_attr): Print config3 field.
    * tests/perf_event_open.c (print_event_attr): Check it.

--- src/perf.c
+++ src/perf.c
@@ -434,6 +434,10 @@ print_perf_event_attr(struct tcb *const tcp, const kernel_ulong_t addr)
 	tprint_struct_next();
 	PRINT_FIELD_X(*attr, sig_data);
 
+	STRACE_PERF_CHECK_FIELD(config3);
+	tprint_struct_next();
+	PRINT_FIELD_X(*attr, config3);
+
 print_perf_event_attr_out:
 	if ((attr->size && (attr->size > size)) ||
 	    (!attr->size && (size < PERF_ATTR_SIZE_VER0))) {
