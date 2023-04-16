267b19178e2d4a31fc54624ff99c5c9218134c81
Dmitry V. Levin <ldv@strace.io>
2023-04-16 22:23:53 +0000

    bundled: update linux UAPI headers to v6.3-rc7
    * bundled/linux/include/uapi/linux/btrfs.h: Update to headers_install'ed
    Linux kernel v6.3-rc7.
    * bundled/linux/include/uapi/linux/dcbnl.h: Likewise.
    * bundled/linux/include/uapi/linux/fcntl.h: Likewise.
    * bundled/linux/include/uapi/linux/if_bridge.h: Likewise.
    * bundled/linux/include/uapi/linux/if_link.h: Likewise.
    * bundled/linux/include/uapi/linux/in.h: Likewise.
    * bundled/linux/include/uapi/linux/io_uring.h: Likewise.
    * bundled/linux/include/uapi/linux/memfd.h: Likewise.
    * bundled/linux/include/uapi/linux/netfilter/nf_tables.h: Likewise.
    * bundled/linux/include/uapi/linux/netfilter/nfnetlink_cttimeout.h: Likewise.
    * bundled/linux/include/uapi/linux/prctl.h: Likewise.
    * bundled/linux/include/uapi/linux/rtnetlink.h: Likewise.
    * bundled/linux/include/uapi/linux/v4l2-controls.h: Likewise.
    * bundled/linux/include/uapi/linux/version.h: Likewise.
    * bundled/linux/include/uapi/linux/videodev2.h: Likewise.

--- bundled/linux/include/uapi/linux/version.h
+++ bundled/linux/include/uapi/linux/version.h
@@ -1,4 +1,4 @@
-#define LINUX_VERSION_CODE 393728
+#define LINUX_VERSION_CODE 393984
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
 #define LINUX_VERSION_MAJOR 6
 #define LINUX_VERSION_PATCHLEVEL 2
