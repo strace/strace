2b81d6795cee32fda740c729f511cb13742fdb41
Dmitry V. Levin <ldv@strace.io>
2023-09-17 21:40:24 +0000

    bundled: update linux UAPI headers to v6.6-rc2
    * bundled/linux/include/uapi/linux/blkzoned.h: Update to
    headers_install'ed Linux kernel v6.6-rc2.
    * bundled/linux/include/uapi/linux/btrfs_tree.h: Likewise.
    * bundled/linux/include/uapi/linux/if_link.h: Likewise.
    * bundled/linux/include/uapi/linux/if_xdp.h: Likewise.
    * bundled/linux/include/uapi/linux/io_uring.h: Likewise.
    * bundled/linux/include/uapi/linux/kexec.h: Likewise.
    * bundled/linux/include/uapi/linux/mount.h: Likewise.
    * bundled/linux/include/uapi/linux/netfilter/nf_tables.h: Likewise.
    * bundled/linux/include/uapi/linux/perf_event.h: Likewise.
    * bundled/linux/include/uapi/linux/pkt_sched.h: Likewise.
    * bundled/linux/include/uapi/linux/quota.h: Likewise.
    * bundled/linux/include/uapi/linux/seccomp.h: Likewise.
    * bundled/linux/include/uapi/linux/userfaultfd.h: Likewise.
    * bundled/linux/include/uapi/linux/version.h: Likewise.
    * bundled/linux/include/uapi/linux/videodev2.h: Likewise.

--- bundled/linux/include/uapi/linux/version.h
+++ bundled/linux/include/uapi/linux/version.h
@@ -1,4 +1,4 @@
-#define LINUX_VERSION_CODE 394496
+#define LINUX_VERSION_CODE 394752
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
 #define LINUX_VERSION_MAJOR 6
 #define LINUX_VERSION_PATCHLEVEL 2
