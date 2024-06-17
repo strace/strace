1d6aed90646da5396afb602bfb648bb7a3981230
Dmitry V. Levin <ldv@strace.io>
2024-06-17 08:00:00 +0000

    bundled: update linux UAPI headers to v6.10-rc4
    * include/uapi/linux/cryptouser.h: Update to headers_install'ed
    Linux kernel v6.10-rc4.
    * include/uapi/linux/fcntl.h: Likewise.
    * include/uapi/linux/if_link.h: Likewise.
    * include/uapi/linux/input-event-codes.h: Likewise.
    * include/uapi/linux/io_uring.h: Likewise.
    * include/uapi/linux/kexec.h: Likewise.
    * include/uapi/linux/landlock.h: Likewise.
    * include/uapi/linux/mptcp.h: Likewise.
    * include/uapi/linux/prctl.h: Likewise.
    * include/uapi/linux/stat.h: Likewise.
    * include/uapi/linux/stddef.h: Likewise.
    * include/uapi/linux/tcp.h: Likewise.
    * include/uapi/linux/tee.h: Likewise.
    * include/uapi/linux/version.h: Likewise.
    * include/uapi/linux/videodev2.h: Likewise.

--- bundled/linux/include/uapi/linux/if_link.h
+++ bundled/linux/include/uapi/linux/if_link.h
@@ -1464,6 +1464,8 @@ enum {
 	IFLA_GTP_ROLE,
 	IFLA_GTP_CREATE_SOCKETS,
 	IFLA_GTP_RESTART_COUNT,
+	IFLA_GTP_LOCAL,
+	IFLA_GTP_LOCAL6,
 	__IFLA_GTP_MAX,
 };
 #define IFLA_GTP_MAX (__IFLA_GTP_MAX - 1)
@@ -1769,6 +1771,7 @@ enum {
 	IFLA_HSR_PROTOCOL,		/* Indicate different protocol than
 					 * HSR. For example PRP.
 					 */
+	IFLA_HSR_INTERLINK,		/* HSR interlink network device */
 	__IFLA_HSR_MAX,
 };
 
