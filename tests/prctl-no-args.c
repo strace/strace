d29c9e53c61bbd8d1d489a6231fa23b8eeef6ff5
Eugene Syromyatnikov <evgsyr@gmail.com>
2024-07-08 10:58:00 +0200

    tests: expand prctl-no-args test a bit
    Amend the test with checking of prctl operations that don't have
    arguments with any decodable semantics, but perform argument checking
    for the sake of possible future expandability (thus prompting printing
    of those arguments by strace), as well as unknown operations (that have
    the same treatment).
    
    * prctl-no-args-Xabbrev.c: New file.
    * prctl-no-args-Xraw.c: Likewise.
    * prctl-no-args-Xverbose.c: Likewise.
    * prctl-no-args-success-Xabbrev.c: Likewise.
    * prctl-no-args-success-Xraw.c: Likewise.
    * prctl-no-args-success-Xverbose.c: Likewise.
    * prctl-no-args-success.c: Likewise.
    * .gitignore: Add prctl-no-args-Xabbrev, prctl-no-args-Xraw,
    prctl-no-args-Xverbose, prctl-no-args-success,
    prctl-no-args-success-Xabbrev, prctl-no-args-success-Xraw,
    and prctl-no-args-success-Xverbose.
    * Makefile.am (check_PROGRAMS): Add prctl-no-args-success,
    prctl-no-args-success-Xabbrev, prctl-no-args-success-Xraw,
    and prctl-no-args-success-Xverbose.
    * gen_tests.in (prctl-no-args-Xabbrev, prctl-no-args-Xraw,
    prctl-no-args-Xverbose, prctl-no-args-success,
    prctl-no-args-success-Xabbrev, prctl-no-args-success-Xraw,
    prctl-no-args-success-Xverbose): New tests.
    * pure_executables.list: Add prctl-no-args-Xabbrev, prctl-no-args-Xraw,
    and prctl-no-args-Xverbose.
    * prctl-no-args.c: Include <stdlib.h>.
    (INJ_STR): New macro.
    (main): Add argc and argv arguments.
    (main) <options> Change the type to static const struct strval32.
    (main) <options_checking, options_unknown>: New constants.
    (main) <rc>: Move the variable declaration outside the loop body.
    (main): Add the prctl inject boilerplate, checks for options that
    do not print arguments, and checks for options without decodable
    arguments that print them.

--- tests/prctl-no-args.c
+++ tests/prctl-no-args.c
@@ -1,8 +1,12 @@
 /*
  * Check decoding of prctl operations without arguments and return code parsing:
  * PR_GET_KEEPCAPS, PR_GET_SECCOMP, PR_GET_TIMERSLACK, PR_GET_TIMING,
- * PR_TASK_PERF_EVENTS_DISABLE, PR_TASK_PERF_EVENTS_ENABLE, and
- * PR_GET_TAGGED_ADDR_CTRL.
+ * PR_TASK_PERF_EVENTS_DISABLE, and PR_TASK_PERF_EVENTS_ENABLE.
+ * Also check decoding of prctl operations that check arguments they do not use
+ * for zero equality and don't have any presentable semantics for the arguments
+ * they use and the return code: PR_GET_NO_NEW_PRIVS, PR_GET_THP_DISABLE,
+ * PR_MPX_DISABLE_MANAGEMENT, PR_MPX_ENABLE_MANAGEMENT, PR_GET_IO_FLUSHER,
+ * and unknown operations.
  *
  * Copyright (c) 2016 Eugene Syromyatnikov <evgsyr@gmail.com>
  * Copyright (c) 2016-2021 The strace developers.
@@ -15,20 +19,24 @@
 #include "scno.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <linux/prctl.h>
 
+#ifdef INJECT_RETVAL
+# define INJ_STR " (INJECTED)"
+#else
+# define INJ_STR ""
+#endif
+
 int
-main(void)
+main(int argc, char *argv[])
 {
 	static const kernel_ulong_t bogus_op_bits =
 		(kernel_ulong_t) 0xbadc0ded00000000ULL;
 	static const kernel_ulong_t bogus_arg =
 		(kernel_ulong_t) 0xfacefeeddeadbeefULL;
-	static const struct {
-		kernel_ulong_t val;
-		const char *str;
-	} options[] = {
+	static const struct strval32 options[] = {
 		{  7, "PR_GET_KEEPCAPS" },
 		{ 13, "PR_GET_TIMING" },
 		{ 21, "PR_GET_SECCOMP" },
@@ -36,15 +44,157 @@ main(void)
 		{ 31, "PR_TASK_PERF_EVENTS_DISABLE" },
 		{ 32, "PR_TASK_PERF_EVENTS_ENABLE" },
 	};
+	static const struct strval32 options_checking[] = {
+		{ 39, "PR_GET_NO_NEW_PRIVS" },
+		{ 42, "PR_GET_THP_DISABLE" },
+#ifdef INJECT_RETVAL /* avoid calling non-getter prctls without -einject= */
+		{ 43, "PR_MPX_ENABLE_MANAGEMENT" },
+		{ 44, "PR_MPX_DISABLE_MANAGEMENT" },
+#endif /* INJECT_RETVAL */
+		{ 58, "PR_GET_IO_FLUSHER" },
+	};
+	static const unsigned int options_unknown[] = {
+		0, 17, 18, 48, 49, 74, 75, 76,
+		0x41555855, /* "AUXV" - 1 */
+		0x41555857, /* "AUXV" + 1 */
+		0x53564d40, /* "SVMA" - 1 */
+		0x53564d42, /* "SVMA" + 1 */
+		0x59616d60, /* "Yama" - 1 */
+		0x59616d62, /* "Yama" + 1 */
+		0xffffffff,
+	};
 
 	TAIL_ALLOC_OBJECT_CONST_PTR(unsigned int, ptr);
+	long rc;
 
 	prctl_marker();
 
-	for (unsigned int i = 0; i < ARRAY_SIZE(options); i++) {
-		long rc = syscall(__NR_prctl, options[i].val | bogus_op_bits,
-				  bogus_arg);
-		printf("prctl(%s) = %s\n", options[i].str, sprintrc(rc));
+#ifdef INJECT_RETVAL
+	unsigned long num_skip;
+	long inject_retval;
+	bool locked = false;
+
+	if (argc < 3)
+		error_msg_and_fail("Usage: %s NUM_SKIP INJECT_RETVAL", argv[0]);
+
+	num_skip = strtoul(argv[1], NULL, 0);
+	inject_retval = strtol(argv[2], NULL, 0);
+
+	for (size_t i = 0; i < num_skip; i++) {
+		if (prctl_marker() != inject_retval)
+			continue;
+
+		locked = true;
+		break;
+	}
+
+	if (!locked)
+		error_msg_and_fail("Have not locked on prctl(-1, -2, -3, -4"
+				   ", -5) returning %ld", inject_retval);
+#endif /* INJECT_RETVAL */
+
+	for (const struct strval32 *i = options;
+	     i < options + ARRAY_SIZE(options); i++) {
+		rc = syscall(__NR_prctl, i->val | bogus_op_bits);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, bogus_arg);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, bogus_arg);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, 0, bogus_arg);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, 0, 0, bogus_arg);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, bogus_arg, bogus_arg + 1,
+			    bogus_arg + 2, bogus_arg + 3);
+		printf("prctl(" XLAT_FMT ") = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+	}
+
+	for (const struct strval32 *i = options_checking;
+	     i < options_checking + ARRAY_SIZE(options_checking); i++) {
+		rc = syscall(__NR_prctl, i->val | bogus_op_bits, 0, 0, 0, 0);
+		printf("prctl(" XLAT_FMT ", 0, 0, 0, 0) = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, bogus_arg, 0, 0, 0);
+		printf("prctl(" XLAT_FMT ", %#llx, 0, 0, 0) = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), (unsigned long long) bogus_arg,
+		       sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, bogus_arg, 0, 0);
+		printf("prctl(" XLAT_FMT ", 0, %#llx, 0, 0) = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), (unsigned long long) bogus_arg,
+		       sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, 0, bogus_arg, 0);
+		printf("prctl(" XLAT_FMT ", 0, 0, %#llx, 0) = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), (unsigned long long) bogus_arg,
+		       sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, 0, 0, 0, bogus_arg);
+		printf("prctl(" XLAT_FMT ", 0, 0, 0, %#llx) = %s" INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str), (unsigned long long) bogus_arg,
+		       sprintrc(rc));
+
+		rc = syscall(__NR_prctl, i->val, bogus_arg, bogus_arg + 1,
+			     bogus_arg + 2, bogus_arg + 3);
+		printf("prctl(" XLAT_FMT ", %#llx, %#llx, %#llx, %#llx) = %s"
+		       INJ_STR "\n",
+		       XLAT_SEL(i->val, i->str),
+		       (unsigned long long) bogus_arg,
+		       (unsigned long long) bogus_arg + 1,
+		       (unsigned long long) bogus_arg + 2,
+		       (unsigned long long) bogus_arg + 3,
+		       sprintrc(rc));
+	}
+
+	for (const unsigned int *i = options_unknown;
+	     i < options_unknown + ARRAY_SIZE(options_unknown); i++) {
+		rc = syscall(__NR_prctl, *i | bogus_op_bits, 0, 0, 0, 0);
+		printf("prctl(%#x" NRAW(" /* PR_??? */") ", 0, 0, 0, 0) = %s"
+		       INJ_STR "\n",
+		       *i, sprintrc(rc));
+
+		rc = syscall(__NR_prctl, *i, bogus_arg, 0, 0, 0);
+		printf("prctl(%#x" NRAW(" /* PR_??? */")
+		       ", %#llx, 0, 0, 0) = %s" INJ_STR "\n",
+		       *i, (unsigned long long) bogus_arg, sprintrc(rc));
+
+		rc = syscall(__NR_prctl, *i, 0, bogus_arg, 0, 0);
+		printf("prctl(%#x" NRAW(" /* PR_??? */")
+		       ", 0, %#llx, 0, 0) = %s" INJ_STR "\n",
+		       *i, (unsigned long long) bogus_arg, sprintrc(rc));
+
+		rc = syscall(__NR_prctl, *i, 0, 0, bogus_arg, 0);
+		printf("prctl(%#x" NRAW(" /* PR_??? */")
+		       ", 0, 0, %#llx, 0) = %s" INJ_STR "\n",
+		       *i, (unsigned long long) bogus_arg, sprintrc(rc));
+
+		rc = syscall(__NR_prctl, *i, 0, 0, 0, bogus_arg);
+		printf("prctl(%#x" NRAW(" /* PR_??? */")
+		       ", 0, 0, 0, %#llx) = %s" INJ_STR "\n",
+		       *i, (unsigned long long) bogus_arg, sprintrc(rc));
+
+		rc = syscall(__NR_prctl, *i, bogus_arg, bogus_arg + 1,
+			     bogus_arg + 2, bogus_arg + 3);
+		printf("prctl(%#x" NRAW(" /* PR_??? */")
+		       ", %#llx, %#llx, %#llx, %#llx) = %s" INJ_STR "\n",
+		       *i, (unsigned long long) bogus_arg,
+		       (unsigned long long) bogus_arg + 1,
+		       (unsigned long long) bogus_arg + 2,
+		       (unsigned long long) bogus_arg + 3,
+		       sprintrc(rc));
 	}
 
 	puts("+++ exited with 0 +++");
