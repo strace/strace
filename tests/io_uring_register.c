2013f5a1a43d6525edbf7aaf783a5d5369b5e9a8
Dmitry V. Levin <ldv@strace.io>
2024-07-29 08:00:00 +0000

    xlat: update IORING_* constants
    * bundled/linux/include/uapi/linux/io_uring.h: Update to
    headers_install'ed Linux kernel v6.11-rc1.
    * src/xlat/uring_ops.in (IORING_OP_BIND): New constant introduced
    by Linux kernel commit v6.11-rc1~232^2~17.
    (IORING_OP_LISTEN): New constant introduced by Linux kernel commit
    v6.11-rc1~232^2~16.
    * tests/io_uring_register.c (main): Update expected output.
    * NEWS: Mention this change.

--- tests/io_uring_register.c
+++ tests/io_uring_register.c
@@ -373,12 +373,12 @@ main(void)
 	probe->ops[0].flags = 0;
 	probe->ops[0].resv2 = 0xbeefface;
 
-	probe->ops[1].op = 55;
+	probe->ops[1].op = 57;
 	probe->ops[1].resv = 0;
 	probe->ops[1].flags = IO_URING_OP_SUPPORTED;
 	probe->ops[1].resv2 = 0xdeadc0de;
 
-	probe->ops[2].op = 56;
+	probe->ops[2].op = 58;
 	probe->ops[2].resv = 0xaf;
 	probe->ops[2].flags = 0xbeef;
 	probe->ops[2].resv2 = 0;
@@ -393,32 +393,32 @@ main(void)
 	       ", ops_len=%hhu, resv2=[0, %#x, 0], ops=["
 	       "{op=" XLAT_FMT_U ", resv=0xde, flags=0, resv2=0xbeefface}, "
 	       "{op=" XLAT_FMT_U ", flags=" XLAT_FMT ", resv2=0xdeadc0de}, "
-	       "{op=56" NRAW(" /* IORING_OP_??? */") ", resv=0xaf, flags="
+	       "{op=58" NRAW(" /* IORING_OP_??? */") ", resv=0xaf, flags="
 	       XLAT_FMT "}, {op=254" NRAW(" /* IORING_OP_??? */")
 	       ", flags=0xc0de" NRAW(" /* IO_URING_OP_??? */") "}]}"
 #if RETVAL_INJECTED
 	       " => {last_op=" XLAT_FMT_U ", ops_len=%hhu, resv2=[0, %#x, 0], "
 	       "ops=[{op=" XLAT_FMT_U ", resv=0xde, flags=0, resv2=0xbeefface}"
 	       ", {op=" XLAT_FMT_U ", flags=" XLAT_FMT ", resv2=0xdeadc0de}"
-	       ", {op=56" NRAW(" /* IORING_OP_??? */") ", resv=0xaf, flags="
+	       ", {op=58" NRAW(" /* IORING_OP_??? */") ", resv=0xaf, flags="
 	       XLAT_FMT "}, {op=254" NRAW(" /* IORING_OP_??? */")
 	       ", flags=0xc0de" NRAW(" /* IO_URING_OP_??? */") "}, ...]}"
 #endif
 	       ", 4) = %s\n",
 	       fd_null, path_null, XLAT_ARGS(IORING_REGISTER_PROBE),
 	       XLAT_ARGS(IORING_OP_EPOLL_CTL), probe->ops_len, probe->resv2[1],
-	       XLAT_ARGS(IORING_OP_NOP), XLAT_ARGS(IORING_OP_FTRUNCATE),
+	       XLAT_ARGS(IORING_OP_NOP), XLAT_ARGS(IORING_OP_LISTEN),
 	       XLAT_ARGS(IO_URING_OP_SUPPORTED),
 	       XLAT_ARGS(IO_URING_OP_SUPPORTED|0xbeee),
 #if RETVAL_INJECTED
 	       XLAT_ARGS(IORING_OP_EPOLL_CTL), probe->ops_len, probe->resv2[1],
-	       XLAT_ARGS(IORING_OP_NOP), XLAT_ARGS(IORING_OP_FTRUNCATE),
+	       XLAT_ARGS(IORING_OP_NOP), XLAT_ARGS(IORING_OP_LISTEN),
 	       XLAT_ARGS(IO_URING_OP_SUPPORTED),
 	       XLAT_ARGS(IO_URING_OP_SUPPORTED|0xbeee),
 #endif
 	       errstr);
 
-	probe->last_op = 56;
+	probe->last_op = 58;
 	probe->resv2[1] = 0;
 	fill_memory_ex(probe->ops, sizeof(probe->ops[0]) * (DEFAULT_STRLEN + 1),
 		    0x40, 0x80);
@@ -427,7 +427,7 @@ main(void)
 	printf("io_uring_register(%u<%s>, " XLAT_FMT,
 	       fd_null, path_null, XLAT_ARGS(IORING_REGISTER_PROBE));
 	for (size_t c = 0; c < 1 + RETVAL_INJECTED; c++) {
-		printf("%s{last_op=56" NRAW(" /* IORING_OP_??? */")
+		printf("%s{last_op=58" NRAW(" /* IORING_OP_??? */")
 		       ", ops_len=%hhu, ops=[",
 		       c ? " => " : ", ", probe->ops_len);
 		for (size_t i = 0; i < DEFAULT_STRLEN; i++) {
@@ -498,9 +498,9 @@ main(void)
 		{ ARG_STR(IORING_RESTRICTION_SQE_OP), true,
 		  "sqe_op=", ARG_STR(IORING_OP_NOP), true },
 		{ ARG_STR(IORING_RESTRICTION_SQE_OP), true,
-		  "sqe_op=", ARG_STR(IORING_OP_FTRUNCATE), true },
+		  "sqe_op=", ARG_STR(IORING_OP_LISTEN), true },
 		{ ARG_STR(IORING_RESTRICTION_SQE_OP), true,
-		  "sqe_op=", 56, " /* IORING_OP_??? */", false },
+		  "sqe_op=", 58, " /* IORING_OP_??? */", false },
 		{ ARG_STR(IORING_RESTRICTION_SQE_OP), true,
 		  "sqe_op=", 255, " /* IORING_OP_??? */", false },
 		{ ARG_STR(IORING_RESTRICTION_SQE_FLAGS_ALLOWED), true,
@@ -1251,7 +1251,7 @@ main(void)
 			       (intmax_t) sync_cancel_reg->timeout.tv_nsec);
 			if (j & 0x20)
 				printf(", opcode=" XLAT_FMT,
-				       XLAT_ARGS(IORING_OP_FTRUNCATE));
+				       XLAT_ARGS(IORING_OP_LISTEN));
 			else
 				printf(", opcode=%#x"
 				       NRAW(" /* IORING_OP_??? */"),
