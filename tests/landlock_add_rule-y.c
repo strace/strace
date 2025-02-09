70fc467728802df7d5dd40989f64601d9aed9c13
Dmitry V. Levin <ldv@strace.io>
2025-02-09 08:00:00 +0000

    tests/landlock_add_rule-y: skip if /proc is unavailable
    * tests/landlock_add_rule-y.c (SKIP_IF_PROC_IS_UNAVAILABLE): New macro.

--- tests/landlock_add_rule-y.c
+++ tests/landlock_add_rule-y.c
@@ -3,5 +3,6 @@
 #define RULESET_FD_STR "7</dev/zero>"
 #define PARENT_FD 8
 #define PARENT_FD_STR "8</dev/full>"
+#define SKIP_IF_PROC_IS_UNAVAILABLE skip_if_unavailable("/proc/self/fd/")
 
 #include "landlock_add_rule.c"
