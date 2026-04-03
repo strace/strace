0e8cb819f55c2778abc815807b39fcb4382d3c02
Barnabás Pőcze <pobrn@protonmail.com>
2026-04-03 22:24:51 +0200

    v4l2: decode `VIDIOC_QUERYMENU`
    Decode the `VIDIOC_QUERYMENU` ioctl, which is used for both string and integer
    menu controls. Unfortunately without querying the type of the control it is
    not possible to know if it is a string or integer, so print both.
    
    Integer menus:
    
      ioctl(3, VIDIOC_QUERYMENU, {id=V4L2_CTRL_CLASS_USER+0xf907, index=2} => {name="\2", value=2}) = 0 <0.000025>
    
    String menus:
    
      ioctl(3, VIDIOC_QUERYMENU, {id=V4L2_CTRL_CLASS_USER+0xf904, index=1} => {name="Menu Item 1", value=7310548498887107917}) = 0 <0.000039>
    
    * src/v4l2.c: Decode `VIDIOC_QUERYMENU`.
    * tests/ioctl_v4l2.c: Remove `VIDIOC_QUERYMENU` from the list of
       unsupported ioctls, and add some `VIDIOC_EXPBUF` tests.

--- tests/ioctl_v4l2.c
+++ tests/ioctl_v4l2.c
@@ -502,7 +502,6 @@ main(void)
 		{ ARG_STR(VIDIOC_OVERLAY) },
 		{ ARG_STR(VIDIOC_G_AUDIO) },
 		{ ARG_STR(VIDIOC_S_AUDIO) },
-		{ ARG_STR(VIDIOC_QUERYMENU) },
 		{ ARG_STR(VIDIOC_G_EDID) },
 		{ ARG_STR(VIDIOC_S_EDID) },
 		{ ARG_STR(VIDIOC_G_OUTPUT) },
@@ -1085,6 +1084,26 @@ main(void)
 	ioctl(-1, VIDIOC_QUERYCTRL, p_queryctrl);
 	printf("ioctl(-1, %s, {id=" XLAT_FMT "})" RVAL_EBADF,
 	       XLAT_STR(VIDIOC_QUERYCTRL), XLAT_ARGS(V4L2_CID_SATURATION));
+
+	/* VIDIOC_QUERYMENU */
+	ioctl(-1, VIDIOC_QUERYMENU, 0);
+	printf("ioctl(-1, %s, NULL)" RVAL_EBADF,
+	       XLAT_STR(VIDIOC_QUERYMENU));
+
+	TAIL_ALLOC_OBJECT_CONST_PTR(struct v4l2_querymenu, p_v4l2_querymenu);
+
+	p_v4l2_querymenu->id = V4L2_CID_EXPOSURE_AUTO;
+	p_v4l2_querymenu->index = 42;
+
+	ioctl(-1, VIDIOC_QUERYMENU, p_v4l2_querymenu);
+	printf("ioctl(-1, %s, {"
+	       "id=" NABBR("%#x") VERB(" /* ") NRAW("V4L2_CID_EXPOSURE_AUTO") VERB(" */") ", "
+	       "index=%u"
+	       "})" RVAL_EBADF,
+	       XLAT_STR(VIDIOC_QUERYMENU),
+	       NABBR(p_v4l2_querymenu->id,)
+	       p_v4l2_querymenu->index
+	);
 
 	/* VIDIOC_G_INPUT */
 	ioctl(-1, VIDIOC_G_INPUT, 0);
