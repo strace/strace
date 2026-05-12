56662c4443b212fb097bd32ffb8e67c20ad0236e
Dmitry V. Levin <ldv@strace.io>
2026-05-12 08:00:00 +0000

    tests: robustify delay test
    * tests/delay.c: Refactor the test to take into account that some delays
    are longer than expected dur to high load.  If the measured delay
    exceeds expectations, do not abort the test but give it another try
    instead.

--- tests/delay.c
+++ tests/delay.c
@@ -8,6 +8,7 @@
  */
 
 #include "tests.h"
+#include <errno.h>
 #include <inttypes.h>
 #include <limits.h>
 #include <stdio.h>
@@ -32,23 +33,45 @@ usecs_from_ts(const struct timespec *const ts)
     return (int64_t) ts->tv_sec * 1000000 + ts->tv_nsec / 1000;
 }
 
-static void
-check_(const int64_t got, const bool ge, const int64_t orig, const int nproc,
-       const int exitcode)
+static bool
+check_expected(const int64_t got, const bool ge, const int64_t exp_delay,
+	       const int nproc)
 {
-	const int64_t thresh = (orig * (ge ? nproc - 1 : nproc + 1)) / nproc;
+	const int64_t threshold =
+		exp_delay * (ge ? (nproc - 1) : (nproc + 1)) / nproc;
 
-	if (ge ? got >= thresh : got <= thresh)
-		return;
+	if (ge ? (got >= threshold) : (got <= threshold))
+		return true;
 
-	fprintf(stderr, "Got delay of %" PRId64 ", %s than threshold value of "
-			"%" PRId64 " (expected nominal delay value is %" PRId64
-			")\n", got, ge ? "less" : "more", thresh, orig);
+	fprintf(stderr,
+		"Got delay of %" PRId64 ", %s than threshold value of"
+		" %" PRId64 " (expected nominal delay value is %" PRId64 ")\n",
+		got, ge ? "less" : "more", threshold, exp_delay);
 
-	_exit(exitcode);
+	return false;
 }
 
-static void
+static int
+check_expectations(const int64_t got, const int64_t exp_delay, const int nproc,
+		   const int rc_short, const int rc_long)
+{
+	if (!check_expected(got, true, exp_delay, nproc))
+		return rc_short;
+	if (!check_expected(got, false, exp_delay, nproc))
+		return rc_long;
+	return 0;
+}
+
+enum {
+	BAD_OTHER = 2,
+	DELAY_ENTER_TOO_SHORT = 4,
+	DELAY_EXIT_TOO_SHORT = 8,
+	DELAY_ENTER_TOO_LONG = 16,
+	DELAY_EXIT_TOO_LONG = 32,
+	MASK_DELAY_TOO_LONG = DELAY_ENTER_TOO_LONG | DELAY_EXIT_TOO_LONG,
+};
+
+static int
 check_delay(const kernel_old_timeval_t *const tv0,
 	    const struct timespec *const ts,
 	    const kernel_old_timeval_t *const tv1,
@@ -59,39 +82,81 @@ check_delay(const kernel_old_timeval_t *const tv0,
 	const int64_t us0 = usecs_from_tv(tv0);
 	const int64_t us  = usecs_from_ts(ts);
 	const int64_t us1 = usecs_from_tv(tv1);
+	int rc = 0;
 
-	check_(us - us0, true,  delay_exit,  nproc, 1);
-	check_(us - us0, false, delay_exit,  nproc, 2);
-	check_(us1 - us, true,  delay_enter, nproc, 3);
-	check_(us1 - us, false, delay_enter, nproc, 4);
+	rc |= check_expectations(us - us0, delay_exit, nproc,
+				 DELAY_EXIT_TOO_SHORT, DELAY_EXIT_TOO_LONG);
+	rc |= check_expectations(us1 - us, delay_enter, nproc,
+				 DELAY_ENTER_TOO_SHORT, DELAY_ENTER_TOO_LONG);
+
+	return rc;
 }
 
 static void
-run(const int nproc, const int delay_enter, const int delay_exit)
+do_child(const int nproc, const int delay_enter, const int delay_exit)
 {
 	kernel_old_timeval_t prev = { 0, 0 }, now;
+	struct timespec ts;
+	int rc = 0;
 
 	for (int i = 0; i < nproc; ++i, prev = now) {
-		struct timespec ts;
-
-		if (i && clock_gettime(CLOCK_REALTIME, &ts))
-			perror_msg_and_fail("clock_gettime");
-
 		if (syscall(__NR_gettimeofday, &now, NULL))
 			perror_msg_and_fail("gettimeofday");
 
-		if (!i)
-			continue;
+		if (i)
+			rc |= check_delay(&prev, &ts, &now, nproc,
+					  delay_enter, delay_exit);
 
-		check_delay(&prev, &ts, &now, nproc, delay_enter, delay_exit);
+		if (clock_gettime(CLOCK_REALTIME, &ts))
+			perror_msg_and_fail("clock_gettime");
 	}
 
-	_exit(0);
+	_exit(rc);
+}
+
+static int
+run_checks(const int nproc, const int delay_enter, const int delay_exit)
+{
+	int rc = 0;
+	pid_t pid;
+
+	for (int i = 0; i < nproc; ++i) {
+		pid = fork();
+
+		if (pid < 0)
+			perror_msg_and_fail("fork");
+		if (pid)
+			usleep(MAX(delay_enter, delay_exit) / nproc);
+		else
+			do_child(nproc, delay_enter, delay_exit);
+	}
+
+	int status;
+	while ((pid = wait(&status)) > 0) {
+		if (status == 0)
+			continue;
+		if (WIFEXITED(status)) {
+			int es = WEXITSTATUS(status);
+
+			fprintf(stderr, "pid %d exit status %d", pid, es);
+			rc |= es;
+		} else {
+			fprintf(stderr, "pid %d wait status %#x", pid, status);
+			rc |= BAD_OTHER;
+		}
+	}
+
+	if (errno != ECHILD)
+		perror_msg_and_fail("wait");
+
+	return rc;
 }
 
 int
 main(int ac, char *av[])
 {
+	int rc;
+
 	if (ac != 4)
 		error_msg_and_fail("usage: delay <nproc> <delay_enter> <delay_exit>");
 
@@ -108,19 +173,16 @@ main(int ac, char *av[])
 		perror_msg_and_fail("invalid delay_exit: %s", av[3]);
 
 	for (int i = 0; i < nproc; ++i) {
-		pid_t pid = fork();
-
-		if (pid)
-			usleep(MAX(delay_enter, delay_exit) / nproc);
-		else
-			run(nproc, delay_enter, delay_exit);
+		rc = run_checks(nproc, delay_enter, delay_exit);
+		if (rc == 0) {
+			if (i)
+				fprintf(stderr, "attempt #%d passed\n", i + 1);
+			break;
+		}
+		if (rc & ~MASK_DELAY_TOO_LONG)
+			break;
+		fprintf(stderr, "attempt #%d failed\n", i + 1);
 	}
 
-	int status;
-	while (wait(&status) > 0) {
-		if (status)
-			perror_msg_and_fail("wait status %d", status);
-	}
-
-	return 0;
+	return rc;
 }
