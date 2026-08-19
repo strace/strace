3e4d70a5e497dc924fba50766f1dc31081c7a59b
Dmitry V. Levin <ldv@strace.io>
2026-08-19 09:00:00 +0000

    Assume linux/time_types.h provides all necessary types
    Since linux/time_types.h is bundled, there is no need to check
    whether it provides the types we use.
    
    * configure.ac (AC_CHECK_TYPES): Remove struct __kernel_timespec and
    struct __kernel_sock_timeval.
    * src/kernel_time_types.h: Include <linux/time_types.h> unconditionally,
    remove fallback definitions of struct __kernel_timespec and
    struct __kernel_sock_timeval.

--- src/kernel_time_types.h
+++ src/kernel_time_types.h
@@ -9,23 +9,6 @@
 # define STRACE_KERNEL_TIME_TYPES_H
 
 # include "kernel_timespec.h"
-
-# if defined HAVE_STRUCT___KERNEL_SOCK_TIMEVAL   \
-  || defined HAVE_STRUCT___KERNEL_TIMESPEC
-#  include <linux/time_types.h>
-# else
-#  include <stdint.h>
-# endif
-
-# ifndef HAVE_STRUCT___KERNEL_SOCK_TIMEVAL
-struct __kernel_sock_timeval {
-	int64_t tv_sec;
-	int64_t tv_usec;
-};
-# endif
-
-# ifndef HAVE_STRUCT___KERNEL_TIMESPEC
-#  define __kernel_timespec kernel_timespec64_t
-# endif
+# include <linux/time_types.h>
 
 #endif /* !STRACE_KERNEL_TIME_TYPES_H */
