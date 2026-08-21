55c2b33f5e59ac208a1b411f19be72bc38e789d3
Thomas Weißschuh (Schneider Electric) <thomas.weissschuh@linutronix.de>
2026-08-21 11:36:23 +0200

    tests: create dedicated clock id to name decoding test
    Currently the decoding of numeric clock ids into clock names is tested
    as part of the time related system calls. These are currently not
    handling different translation formats.
    
    More types of clocks are about to be supported which will require more
    tests and tests for different formats. Handling those as part of the
    existing tests would require a lot of churn and unnecessary duplication.
    
    Create a new testcase only for the clockid decoding.
    
    * tests/printclockname.c: New file.
    * tests/printclockname-Xabbrev.c: Likewise.
    * tests/printclockname-Xraw.c: Likewise.
    * tests/printclockname-Xverbose.c: Likewise.
    * tests/.gitignore: Add printclockname-Xabbrev, printclockname-Xraw
      and printclockname-Xabbrev.
    * tests/gen_tests.in: Likewise.
    * tests/pure_executables.list: Likewise.
    * tests/Makefile.am: Add printclockname.c.
    
    Signed-off-by: Thomas Weißschuh (Schneider Electric) <thomas.weissschuh@linutronix.de>

---
+++ tests/printclockname.c
@@ -0,0 +1,60 @@
+/*
+ * This file is part of clock_xettime* strace tests.
+ *
+ * Copyright (c) 2015-2016 Dmitry V. Levin <ldv@strace.io>
+ * Copyright (c) 2015-2020 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <linux/time.h>
+#include <linux/unistd.h>
+
+#include "tests.h"
+
+int
+main(void)
+{
+	syscall(__NR_clock_getres, CLOCK_REALTIME, NULL);
+#if XLAT_RAW
+	printf("clock_getres(0, NULL)                   = 0\n");
+#elif XLAT_VERBOSE
+	printf("clock_getres(0 /* CLOCK_REALTIME */, NULL) = 0\n");
+#else
+	printf("clock_getres(CLOCK_REALTIME, NULL)      = 0\n");
+#endif
+
+	syscall(__NR_clock_getres, CLOCK_MONOTONIC, NULL);
+#if XLAT_RAW
+	printf("clock_getres(0x1, NULL)                 = 0\n");
+#elif XLAT_VERBOSE
+	printf("clock_getres(0x1 /* CLOCK_MONOTONIC */, NULL) = 0\n");
+#else
+	printf("clock_getres(CLOCK_MONOTONIC, NULL)     = 0\n");
+#endif
+
+	syscall(__NR_clock_getres, CLOCK_PROCESS_CPUTIME_ID, NULL);
+#if XLAT_RAW
+	printf("clock_getres(0x2, NULL)                 = 0\n");
+#elif XLAT_VERBOSE
+	printf("clock_getres(0x2 /* CLOCK_PROCESS_CPUTIME_ID */, NULL) = 0\n");
+#else
+	printf("clock_getres(CLOCK_PROCESS_CPUTIME_ID, NULL) = 0\n");
+#endif
+
+	syscall(__NR_clock_getres, CLOCK_THREAD_CPUTIME_ID, NULL);
+#if XLAT_RAW
+	printf("clock_getres(0x3, NULL)                 = 0\n");
+#elif XLAT_VERBOSE
+	printf("clock_getres(0x3 /* CLOCK_THREAD_CPUTIME_ID */, NULL) = 0\n");
+#else
+	printf("clock_getres(CLOCK_THREAD_CPUTIME_ID, NULL) = 0\n");
+#endif
+
+	puts("+++ exited with 0 +++");
+	return 0;
+}
